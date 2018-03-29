6a56cedf761641e0ef8083f96ef50c0fa535b14d
Merlin Beutlberger <m.beutlberger@sap.com>
2018-03-29 14:54:52 +0200

    [cli] Initial commit

--- packages/cli/lib/cli/commands/init.js
+++ packages/cli/lib/cli/commands/init.js
@@ -1,32 +1,50 @@
 // Init
-import baseMiddleware from "../middlewares/base.js";
-
-const initCommand = {
+let init = {
 	command: "init",
-	describe: "Initialize the UI5 CLI configuration for an application or library project.",
-	middlewares: [baseMiddleware]
+	describe: "Initialize the UI5 Build and Development Tooling configuration for an application or library project.",
+	middlewares: [require("../middlewares/base.js")]
 };
 
-initCommand.handler = async function() {
-	const {exists} = await import("../../utils/fsHelper.js");
-	const {default: init} = await import("../../init/init.js");
-	const {default: path} = await import("node:path");
-	const {writeFile} = await import("node:fs/promises");
-	const {default: jsYaml} = await import("js-yaml");
+init.handler = async function(argv) {
+	const init = require("../../init/init");
+	const {promisify} = require("util");
+	const path = require("path");
+	const fs = require("fs");
+	const stat = promisify(fs.stat);
+	const writeFile = promisify(fs.writeFile);
+	const safeDumpYaml = require("js-yaml").safeDump;
 
-	const yamlPath = path.resolve("./ui5.yaml");
-	if (await exists(yamlPath)) {
-		throw new Error("Initialization not possible: ui5.yaml already exists");
+	async function exists(filePath) {
+		try {
+			await stat(filePath);
+			return true;
+		} catch (err) {
+			// "File or directory does not exist"
+			if (err.code === "ENOENT") {
+				return false;
+			} else {
+				throw err;
+			}
+		}
 	}
 
-	const projectConfig = await init();
-	const yaml = jsYaml.dump(projectConfig, {quotingType: `"`});
+	try {
+		const yamlPath = path.resolve("./ui5.yaml");
+		if (await exists(yamlPath)) {
+			throw new Error("Initialization not possible: ui5.yaml already exists");
+		}
 
-	await writeFile(yamlPath, yaml);
-	process.stdout.write(`Wrote ui5.yaml to ${yamlPath}:`);
-	process.stdout.write("\n");
-	process.stdout.write(yaml);
-	process.stdout.write("\n");
+		const projectConfig = await init();
+		const yaml = safeDumpYaml(projectConfig);
+
+		await writeFile(yamlPath, yaml);
+
+		console.log(`Wrote ui5.yaml to ${yamlPath}:\n`);
+		console.log(yaml);
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
 };
 
-export default initCommand;
+module.exports = init;
