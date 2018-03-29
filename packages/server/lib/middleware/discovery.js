a5994b6fd2e39a19abbc551929d25cba12c3b80d
Merlin Beutlberger <m.beutlberger@sap.com>
2018-03-29 14:54:52 +0200

    [server] Initial commit

--- packages/server/lib/middleware/discovery.js
+++ packages/server/lib/middleware/discovery.js
@@ -2,34 +2,19 @@ const librariesPattern = /([A-Z0-9._%+-/]+)\/[A-Z0-9._]*\.library$/i;
 const testPagesPattern = /(([A-Z0-9._%+-]+\/)+([A-Z_0-9-\\.]+)\.(html|htm))$/i;
 const urlPattern = /\/(app_pages|all_libs|all_tests)(?:[?#].*)?$/;
 
-/**
- * Creates and returns the middleware to discover project files.
- *
- * List project files with URL (needed exclusively by the OpenUI5 testsuite):
- * <ul>
- * <li>/discovery/app_pages: get application pages</li>
- * <li>/discovery/all_libs: list all libraries</li>
- * <li>/discovery/all_tests: list all tests</li>
- * </ul>
- *
- * @module @ui5/server/middleware/discovery
- * @param {object} parameters Parameters
- * @param {@ui5/server/internal/MiddlewareManager.middlewareResources} parameters.resources Parameters
- * @returns {Function} Returns a server middleware closure.
- */
-function createMiddleware({resources}) {
+function createMiddleware({resourceCollections}) {
 	return function discoveryMiddleware(req, res, next) {
-		const parts = urlPattern.exec(req.url);
-		const type = parts && parts[1];
+		var parts = urlPattern.exec(req.url);
+		var type = parts && parts[1];
 		if (!type) {
 			next();
 			return;
 		}
 
-		const response = [];
+		var response = [];
 
 		function sendResponse() {
-			const responseData = {};
+			var responseData = {};
 			response.sort((a, b) => {
 				if (type === "app_pages" || type === "all_libs") {
 					return a.entry.localeCompare(b.entry);
@@ -45,9 +30,9 @@ function createMiddleware({resources}) {
 		}
 
 		if (type === "app_pages") {
-			resources.rootProject.byGlob("/**/*.{html,htm}").then(function(resources) {
+			resourceCollections.source.byGlob("/**/*.{html,htm}").then(function(resources) {
 				resources.forEach(function(resource) {
-					const relPath = resource.getPath().substr(1); // cut off leading "/"
+					let relPath = resource.getPath().substr(1); // cut off leading "/"
 					response.push({
 						entry: relPath
 					});
@@ -55,12 +40,12 @@ function createMiddleware({resources}) {
 				sendResponse();
 			});
 		} else if (type === "all_libs") {
-			resources.all.byGlob([
+			resourceCollections.combo.byGlob([
 				"/resources/**/*.library"
 			]).then(function(resources) {
 				resources.forEach(function(resource) {
-					const relPath = resource.getPath().substr(11); // cut off leading "/resources/"
-					const match = librariesPattern.exec(relPath);
+					let relPath = resource.getPath().substr(11); // cut off leading "/resources/"
+					var match = librariesPattern.exec(relPath);
 					if (match) {
 						response.push({
 							entry: match[1]
@@ -71,34 +56,32 @@ function createMiddleware({resources}) {
 			});
 		} else if (type === "all_tests") {
 			Promise.all([
-				resources.all.byGlob("/resources/**/*.library"),
-				resources.all.byGlob("/test-resources/**/*.{html,htm}")
+				resourceCollections.combo.byGlob("/resources/**/*.library"),
+				resourceCollections.combo.byGlob("/test-resources/**/*.{html,htm}")
 			]).then(function(results) {
-				const libraryResources = results[0];
-				const testPageResources = results[1];
-				const libs = Object.create(null);
+				let libraryResources = results[0];
+				let testPageResources = results[1];
+				var libs = {};
 
 				libraryResources.forEach(function(resource) {
-					const relPath = resource.getPath().substr(11); // cut off leading "/resources/"
-					const match = librariesPattern.exec(relPath);
+					let relPath = resource.getPath().substr(11); // cut off leading "/resources/"
+					var match = librariesPattern.exec(relPath);
 					if (match) {
-						const lib = match[1];
-						libs[lib + "/"] = lib.replace(/\//g, ".");
+						var lib = match[1];
+						libs[lib] = lib.replace(/\//g, ".");
 					}
 				});
 
-				const libPrefixes = Object.keys(libs).sort().reverse();
 				testPageResources.forEach(function(resource) {
-					const relPath = resource.getPath().substr(16); // cut off leading "/test-resources/"
+					let relPath = resource.getPath().substr(16); // cut off leading "/test-resources/"
 					if (testPagesPattern.test(relPath)) {
-						libPrefixes.some(function(lib) {
-							if (relPath.startsWith(lib)) {
+						Object.keys(libs).forEach(function(lib) {
+							if (relPath.indexOf(lib) === 0) {
 								response.push({
 									lib: libs[lib],
-									name: relPath.substr(lib.length),
+									name: relPath.substr(lib.length + 1),
 									url: "../" + relPath
 								});
-								return true; // abort loop
 							}
 						});
 					}
@@ -107,9 +90,9 @@ function createMiddleware({resources}) {
 				sendResponse();
 			});
 		} else {
-			next(new Error(`Unknown discovery type "${type}"`));
+			next(new Error(`Unkown discovery type "${type}"`));
 		}
 	};
 }
 
-export default createMiddleware;
+module.exports = createMiddleware;
