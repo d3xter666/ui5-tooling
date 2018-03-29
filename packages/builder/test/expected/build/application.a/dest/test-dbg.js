be7a51928323a0be611e35b2f81e01de5585281e
Merlin Beutlberger <m.beutlberger@sap.com>
2018-03-29 14:54:52 +0200

    [builder] Initial commit

--- packages/builder/test/expected/build/application.a/dest/test-dbg.js
+++ packages/builder/test/expected/build/application.a/dest/test-dbg.js
@@ -1,9 +1,5 @@
-sap.ui.define([
-    "library/d/some"
-], function(someObject) {
-	function test(paramA) {
-		var variableA = paramA;
-		console.log(variableA);
-	}
-	test();
-});
+function test(paramA) {
+	var variableA = paramA;
+	console.log(variableA);
+}
+test();
