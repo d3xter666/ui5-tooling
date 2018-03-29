083113445db838bf4e2f1b63bf2d8c90f8bca6d2
Merlin Beutlberger <m.beutlberger@sap.com>
2018-03-29 14:54:52 +0200

    [fs] Initial commit

--- packages/fs/test/lib/fsInterface.js
+++ packages/fs/test/lib/fsInterface.js
@@ -1,123 +1,102 @@
-import test from "ava";
-import {promisify} from "node:util";
-import {Buffer} from "node:buffer";
-import path from "node:path";
-import {fileURLToPath} from "node:url";
-import fsSync from "node:fs";
-const stat = promisify(fsSync.stat);
-import {readFile} from "node:fs/promises";
-import fsInterface from "../../lib/fsInterface.js";
-import MemAdapter from "../../lib/adapters/Memory.js";
-import FsAdapter from "../../lib/adapters/FileSystem.js";
-import Resource from "../../lib/Resource.js";
+const {test} = require("ava");
+const {promisify} = require("util");
+const Buffer = require("buffer").Buffer;
+const path = require("path");
+const fs = require("fs");
+const ui5Fs = require("../../");
+const fsInterface = ui5Fs.fsInterface;
+const MemAdapter = ui5Fs.Memory;
+const FsAdapter = ui5Fs.FileSystem;
+const Resource = ui5Fs.Resource;
 
-const assertReadFile = async (t, readFile, basepath, filepath, content) => {
-	content = content || "content of " + filepath;
-	const fullpath = path.join(basepath, filepath);
-	let buffer = await readFile(fullpath);
-	t.true(Buffer.isBuffer(buffer));
-	t.deepEqual(buffer.toString(), content);
-
-	buffer = await readFile(fullpath, {});
-	t.true(Buffer.isBuffer(buffer));
-	t.deepEqual(buffer.toString(), content);
-	buffer = await readFile(fullpath, {encoding: null});
-	t.true(Buffer.isBuffer(buffer));
-	t.deepEqual(buffer.toString(), content);
-	buffer = await readFile(fullpath, "utf8");
-	t.is(typeof buffer, "string");
-	t.deepEqual(buffer, content);
-
-	buffer = await readFile(fullpath, {encoding: "utf8"});
-	t.is(typeof buffer, "string");
-	t.deepEqual(content, content);
+const assertReadFile = (t, readFile, basepath, filepath) => {
+	const fullpath = basepath + filepath;
+	return readFile(fullpath).then((buffer) => {
+		t.true(Buffer.isBuffer(buffer));
+		t.deepEqual(buffer.toString(), "content of " + filepath);
+	}).then(() => readFile(fullpath, {})).then((buffer) => {
+		t.true(Buffer.isBuffer(buffer));
+		t.deepEqual(buffer.toString(), "content of " + filepath);
+	}).then(() => readFile(fullpath, {encoding: null})).then((buffer) => {
+		t.true(Buffer.isBuffer(buffer));
+		t.deepEqual(buffer.toString(), "content of " + filepath);
+	}).then(() => readFile(fullpath, "utf8").then((content) => {
+		t.is(typeof content, "string");
+		t.deepEqual(content, "content of " + filepath);
+	}).then(() => readFile(fullpath, {encoding: "utf8"})).then((content) => {
+		t.is(typeof content, "string");
+		t.deepEqual(content, "content of " + filepath);
+	}));
 };
 
-function getPath() {
-	return fileURLToPath(new URL("../fixtures/fsInterfáce", import.meta.url));
-}
-
-test("MemAdapter: readFile", async (t) => {
-	const memAdapter = new MemAdapter({
+test("MemAdapter: readFile", (t) => {
+	let memAdapter = new MemAdapter({
 		virBasePath: "/"
 	});
 	const fs = fsInterface(memAdapter);
 	const readFile = promisify(fs.readFile);
 
-	const fsPath = path.join("/", "foo.txt");
-	await memAdapter.write(new Resource({
+	return memAdapter.write(new Resource({
 		path: "/foo.txt",
-		string: `content of ${fsPath}`
-	}));
-	`content of ${fsPath}`;
-	await assertReadFile(t, readFile, "", fsPath);
+		string: "content of /foo.txt"
+	})).then(() => assertReadFile(t, readFile, "", "/foo.txt"));
 });
 
-test("FsAdapter: readFile with non-ASCII characters in path", async (t) => {
-	const fsAdapter = new FsAdapter({
+test("FsAdapter: readFile", (t) => {
+	let fsAdapter = new FsAdapter({
 		virBasePath: "/",
-		fsBasePath: getPath()
+		fsBasePath: path.join(__dirname, "..", "fixtures", "fsInterface")
 	});
 	const fs = fsInterface(fsAdapter);
 	const readFile = promisify(fs.readFile);
 
-	await assertReadFile(t, readFile, "", path.join("/", "bâr.txt"), "content");
+	return assertReadFile(t, readFile, "", "/foo.txt");
 });
 
-test("fs: readFile", async (t) => {
-	await assertReadFile(t, readFile,
-		getPath(), path.join("/", "foo.txt"), "content");
+test("fs: readFile", (t) => {
+	const readFile = promisify(fs.readFile);
+	return assertReadFile(t, readFile, path.join(__dirname, "..", "fixtures", "fsInterface"), "/foo.txt");
 });
 
-const assertStat = async (t, stat, basepath, filepath) => {
-	const fullpath = path.join(basepath, filepath);
-	const stats = await stat(fullpath);
 
-	t.is(stats.isFile(), true);
-	t.is(stats.isDirectory(), false);
-	t.is(stats.isBlockDevice(), false);
-	t.is(stats.isCharacterDevice(), false);
-	t.is(stats.isSymbolicLink(), false);
-	t.is(stats.isFIFO(), false);
-	t.is(stats.isSocket(), false);
+const assertStat = (t, stat, basepath, filepath) => {
+	const fullpath = basepath + filepath;
+	return stat(fullpath).then((stats) => {
+		t.is(stats.isFile(), true);
+		t.is(stats.isDirectory(), false);
+		t.is(stats.isBlockDevice(), false);
+		t.is(stats.isCharacterDevice(), false);
+		t.is(stats.isSymbolicLink(), false);
+		t.is(stats.isFIFO(), false);
+		t.is(stats.isSocket(), false);
+	});
 };
 
-test("MemAdapter: stat", async (t) => {
-	const memAdapter = new MemAdapter({
+test("MemAdapter: stat", (t) => {
+	let memAdapter = new MemAdapter({
 		virBasePath: "/"
 	});
 	const fs = fsInterface(memAdapter);
 	const stat = promisify(fs.stat);
 
-	const fsPath = path.join("/", "foo.txt");
-	await memAdapter.write(new Resource({
+	return memAdapter.write(new Resource({
 		path: "/foo.txt",
-		string: `content of ${fsPath}`
-	}));
-	await assertStat(t, stat, "", fsPath);
+		string: "content of /foo.txt"
+	})).then(() => assertStat(t, stat, "", "/foo.txt"));
 });
 
-test("FsAdapter: stat", async (t) => {
-	const fsAdapter = new FsAdapter({
+test("FsAdapter: stat", (t) => {
+	let fsAdapter = new FsAdapter({
 		virBasePath: "/",
-		fsBasePath: getPath()
+		fsBasePath: path.join(__dirname, "..", "fixtures", "fsInterface")
 	});
 	const fs = fsInterface(fsAdapter);
 	const stat = promisify(fs.stat);
 
-	await assertStat(t, stat, "", path.join("/", "foo.txt"));
+	return assertStat(t, stat, "", "/foo.txt");
 });
 
-test("fs: stat", async (t) => {
-	await assertStat(t, stat, getPath(), path.join("/", "foo.txt"));
-});
-
-test("MemAdapter: mkdir", async (t) => {
-	const memAdapter = new MemAdapter({
-		virBasePath: "/"
-	});
-	const fs = fsInterface(memAdapter);
-	const mkdir = promisify(fs.mkdir);
-
-	await t.notThrowsAsync(mkdir("pony"), "mkdir executes successfully");
+test("fs: stat", (t) => {
+	const stat = promisify(fs.stat);
+	return assertStat(t, stat, path.join(__dirname, "..", "fixtures", "fsInterface"), "/foo.txt");
 });
--- packages/fs/test/lib/resources.js
+++ packages/fs/test/lib/resources.js
@@ -1,257 +1,111 @@
-import test from "ava";
-import sinon from "sinon";
-import {readFile} from "node:fs/promises";
+const {test} = require("ava");
+const chai = require("chai");
+chai.use(require("chai-fs"));
+const assert = chai.assert;
 
-import {createAdapter, createFilterReader,
-	createFlatReader, createLinkReader, createResource} from "../../lib/resourceFactory.js";
+const ui5Fs = require("../../");
 
-test.afterEach.always((t) => {
-	sinon.restore();
+// Create readerWriters before running tests
+test.beforeEach((t) => {
+	t.context.readerWriters = {
+		source: ui5Fs.resourceFactory.createAdapter({
+			fsBasePath: "./test/fixtures/application.a/webapp",
+			virBasePath: "/app/"
+		}),
+		dest: ui5Fs.resourceFactory.createAdapter({
+			fsBasePath: "./test/tmp/readerWriters/application.a",
+			virBasePath: "/dest/"
+		})
+	};
 });
 
-function getFileContent(path) {
-	return readFile(path, "utf8");
-}
+/* BEWARE:
+	Always make sure that every test writes to a separate file! By default, tests are running concurrent.
+*/
 
-async function fileEqual(t, actual, expected) {
-	const actualContent = await getFileContent(actual);
-	const expectedContent = await getFileContent(expected);
-	t.is(actualContent, expectedContent);
-}
+test("Get resource from application.a (/index.html) and write it to /dest/ using a ReadableStream", (t) => {
+	let readerWriters = t.context.readerWriters;
+	// Get resource from one readerWriter
+	return t.notThrows(readerWriters.source.byPath("/app/index.html")
+		.then(function(resource) {
+			return resource.clone();
+		})
+		.then(function(newResource) {
+			// Write resource content to another readerWriter
+			newResource.setPath("/dest/index_readableStreamTest.html");
+			return readerWriters.dest.write(newResource);
+		}).then(() => {
+			assert.fileEqual("./test/tmp/readerWriters/application.a/index_readableStreamTest.html",
+				"./test/fixtures/application.a/webapp/index.html");
+		}));
+});
 
-["FileSystem", "Memory"].forEach((adapter) => {
-	async function getAdapter(config) {
-		if (adapter === "Memory") {
-			const fsAdapter = createAdapter(config);
-			const fsResources = await fsAdapter.byGlob("**/*");
-			// By removing the fsBasePath a MemAdapter will be created
-			delete config.fsBasePath;
-			const memAdapter = createAdapter(config);
-			for (const resource of fsResources) {
-				await memAdapter.write(resource);
-			}
-			return memAdapter;
-		} else if (adapter === "FileSystem") {
-			return createAdapter(config);
-		}
-	}
+test("FS RL: GLOB resources from application.a w/ virtual base path prefix", (t) => {
+	let readerWriters = t.context.readerWriters;
+	// Get resource from one readerWriter
+	return t.notThrows(readerWriters.source.byGlob("/app/**/*.html").then(function(resources) {
+		t.deepEqual(resources.length, 1, "Found exactly one resource");
+	}));
+});
 
-	/* BEWARE:
-		Always make sure that every test writes to a separate file! By default, tests are running concurrent.
-	*/
-	test(adapter +
-		": Get resource from application.a (/index.html) and write it to /dest/ using a ReadableStream", async (t) => {
-		const source = await getAdapter({
-			fsBasePath: "./test/fixtures/application.a/webapp",
-			virBasePath: "/app/"
-		});
-		const dest = await getAdapter({
-			fsBasePath: "./test/tmp/readerWriters/application.a/simple-read-write",
-			virBasePath: "/dest/"
-		});
+test("FS RL: GLOB resources from application.a w/o virtual base path prefix", (t) => {
+	let readerWriters = t.context.readerWriters;
+	// Get resource from one readerWriter
+	return t.notThrows(readerWriters.source.byGlob("/**/*.html").then(function(resources) {
+		t.deepEqual(resources.length, 1, "Found exactly one resource");
+	}));
+});
 
-		// Get resource from one readerWriter
-		const resource = await source.byPath("/app/index.html");
-
-		// Write resource content to another readerWriter
-		resource.setPath("/dest/index_readableStreamTest.html");
-		await dest.write(resource);
-
-		t.notThrows(async () => {
-			if (adapter === "FileSystem") {
-				await fileEqual(
-					t,
-					"./test/tmp/readerWriters/application.a/simple-read-write/index_readableStreamTest.html",
-					"./test/fixtures/application.a/webapp/index.html");
-			} else {
-				const destResource = await dest.byPath("/dest/index_readableStreamTest.html");
-				t.deepEqual(await destResource.getString(), await resource.getString());
-			}
-		});
+test("Virtual RL: GLOB resources from application.a w/ virtual base path prefix", (t) => {
+	let readerWriter = ui5Fs.resourceFactory.createAdapter({
+		virBasePath: "/app/"
 	});
 
-	test(adapter + ": Create resource, write and change content", async (t) => {
-		const dest = await getAdapter({
-			fsBasePath: "./test/tmp/writer/",
-			virBasePath: "/dest/writer/"
-		});
-
-		const resource = createResource({
-			path: "/dest/writer/content/test.js",
-			string: "MyInitialContent"
-		});
-
-		await dest.write(resource);
-
-		resource.setString("MyNewContent");
-
-		const resource1 = await dest.byPath("/dest/writer/content/test.js");
-
-		t.is(await resource.getString(), "MyNewContent");
-		t.is(await resource1.getString(), "MyInitialContent");
-
-		t.is(await resource.getString(), "MyNewContent");
-		t.is(await resource1.getString(), "MyInitialContent");
-
-		await dest.write(resource);
-
-		const resource2 = await dest.byPath("/dest/writer/content/test.js");
-		t.is(await resource.getString(), "MyNewContent");
-		t.is(await resource2.getString(), "MyNewContent");
+	const resource = ui5Fs.resourceFactory.createResource({
+		path: "/app/index.html",
+		string: "test"
 	});
 
-	test(adapter + ": Create resource, write and change path", async (t) => {
-		const dest = await getAdapter({
-			fsBasePath: "./test/tmp/writer/",
-			virBasePath: "/dest/writer/"
-		});
+	// Get resource from one readerWriter
+	return t.notThrows(readerWriter.write(resource)
+		.then(() => readerWriter.byGlob("/app/*.html"))
+		.then((resources) => {
+			t.deepEqual(resources.length, 1, "Found exactly one resource");
+		})
+	);
+});
 
-		const resource = createResource({
-			path: "/dest/writer/path/test.js",
-			string: "MyInitialContent"
-		});
-
-		await dest.write(resource);
-
-		resource.setPath("/dest/writer/path/test2.js");
-
-		const resourceOldPath = await dest.byPath("/dest/writer/path/test.js");
-		const resourceNewPath = await dest.byPath("/dest/writer/path/test2.js");
-
-		t.is(await resource.getPath(), "/dest/writer/path/test2.js");
-		t.truthy(resourceOldPath);
-		t.is(await resourceOldPath.getString(), await resource.getString());
-		t.is(await resourceOldPath.getPath(), "/dest/writer/path/test.js");
-		t.not(resourceNewPath);
-
-		await dest.write(resource);
-
-		const resourceOldPath1 = await dest.byPath("/dest/writer/path/test.js");
-		const resourceNewPath1 = await dest.byPath("/dest/writer/path/test2.js");
-
-		t.is(await resource.getPath(), "/dest/writer/path/test2.js");
-		t.truthy(resourceNewPath1);
-		t.is(await resourceNewPath1.getString(), await resource.getString());
-		t.is(await resourceNewPath1.getPath(), "/dest/writer/path/test2.js");
-		t.not(resourceOldPath1);
+test("Virtual RL: GLOB resources from application.a w/o virtual base path prefix", (t) => {
+	let readerWriter = ui5Fs.resourceFactory.createAdapter({
+		virBasePath: "/app/"
 	});
 
-	test(adapter +
-		": Create a resource with a path different from the path configured in the adapter", async (t) => {
-		t.pass(2);
-		const dest = await getAdapter({
-			fsBasePath: "./test/tmp/writer/",
-			virBasePath: "/dest2/writer/"
-		});
-
-		const resource = createResource({
-			path: "/dest2/tmp/test.js",
-			string: "MyContent"
-		});
-
-		const error = await t.throwsAsync(dest.write(resource));
-		t.is(error.message,
-			"Failed to write resource with virtual path '/dest2/tmp/test.js': Path must start with the " +
-			"configured virtual base path of the adapter. Base path: '/dest2/writer/'",
-			"Threw with expected error message");
+	const resource = ui5Fs.resourceFactory.createResource({
+		path: "/app/index.html",
+		string: "test"
 	});
 
-	test(adapter +
-		": Create a resource with a path above the path configured in the adapter", async (t) => {
-		t.pass(2);
-		const dest = await getAdapter({
-			fsBasePath: "./test/tmp/writer/",
-			virBasePath: "/dest2/writer/"
-		});
+	// Get resource from one readerWriter
+	return t.notThrows(readerWriter.write(resource)
+		.then(() => readerWriter.byGlob("/**/*.html"))
+		.then((resources) => {
+			t.deepEqual(resources.length, 1, "Found exactly one resource");
+		})
+	);
+});
 
-		const resource = createResource({
-			path: "/dest2/test.js",
-			string: "MyContent"
-		});
-
-		const error = await t.throwsAsync(dest.write(resource));
-		t.is(error.message,
-			"Failed to write resource with virtual path '/dest2/test.js': Path must start with the " +
-			"configured virtual base path of the adapter. Base path: '/dest2/writer/'",
-			"Threw with expected error message");
+test("Virtual RL: GLOB virtual directory w/o virtual base path prefix", (t) => {
+	// TODO: Add similar test (globbing on empty directory) for FS RL
+	// TODO: Also add tests for nodir: true option
+	let readerWriter = ui5Fs.resourceFactory.createAdapter({
+		virBasePath: "/app/"
 	});
 
-	test(adapter +
-		": Create a resource with a path resolving outside the path configured in the adapter", async (t) => {
-		t.pass(2);
-		const dest = await getAdapter({
-			fsBasePath: "./test/tmp/writer/",
-			virBasePath: "/dest/writer/"
-		});
-
-		const resource = createResource({
-			path: "/dest/writer/../relative.js",
-			string: "MyContent"
-		});
-		// Resource will already resolve relative path segments
-		t.is(resource.getPath(), "/dest/relative.js", "Resource path resolved");
-
-		// This would cause the base path to not match
-		// So we cheat, simulating a misbehaving or old Resource instance, using a relative path segment
-		sinon.stub(resource, "getPath").returns("/dest/writer/../relative.js");
-
-		await t.throwsAsync(dest.write(resource), {
-			message:
-				"Failed to write resource with virtual path '/dest/writer/../relative.js': " +
-				"Path must start with the configured virtual base path of the adapter. Base path: '/dest/writer/'"
-		}, "Threw with expected error message");
-	});
-
-	test(adapter + ": Filter resources", async (t) => {
-		const source = createAdapter({
-			fsBasePath: "./test/fixtures/application.a/webapp",
-			virBasePath: "/app/"
-		});
-		const filteredSource = createFilterReader({
-			reader: source,
-			callback: (resource) => {
-				return resource.getPath().endsWith(".js");
-			}
-		});
-		const sourceResources = await source.byGlob("**");
-		t.is(sourceResources.length, 2, "Found two resources in source");
-
-		const resources = await filteredSource.byGlob("**");
-
-		t.is(resources.length, 1, "Found exactly one resource via filter");
-		t.is(resources[0].getPath(), "/app/test.js", "Found correct resource");
-	});
-
-	test(adapter + ": Flatten resources", async (t) => {
-		const source = await getAdapter({
-			fsBasePath: "./test/fixtures/application.a/webapp",
-			virBasePath: "/resources/app/"
-		});
-		const transformedSource = createFlatReader({
-			reader: source,
-			namespace: "app"
-		});
-
-		const resources = await transformedSource.byGlob("**/*.js");
-		t.is(resources.length, 1, "Found one resource via transformer");
-		t.is(resources[0].getPath(), "/test.js", "Found correct resource");
-	});
-
-	test(adapter + ": Link resources", async (t) => {
-		const source = await getAdapter({
-			fsBasePath: "./test/fixtures/application.a/webapp",
-			virBasePath: "/resources/app/"
-		});
-		const transformedSource = createLinkReader({
-			reader: source,
-			pathMapping: {
-				linkPath: "/wow/this/is/a/beautiful/path/just/wow/",
-				targetPath: "/resources/"
-			}
-		});
-
-		const resources = await transformedSource.byGlob("**/*.js");
-		t.is(resources.length, 1, "Found one resource via transformer");
-		t.is(resources[0].getPath(), "/wow/this/is/a/beautiful/path/just/wow/app/test.js", "Found correct resource");
-	});
+	// Get resource from one readerWriter
+	return t.notThrows(
+		readerWriter.byGlob("/*", {nodir: false})
+			.then((resources) => {
+				t.deepEqual(resources.length, 1, "Found exactly one resource");
+			})
+	);
 });
