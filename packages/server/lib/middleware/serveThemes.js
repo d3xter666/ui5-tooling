942f4f1d2055f4c0dae242ec74e3e02cb614492f
Merlin Beutlberger <m.beutlberger@sap.com>
2018-03-29 14:54:52 +0200

    [server] Initial commit

--- packages/server/lib/middleware/serveThemes.js
+++ packages/server/lib/middleware/serveThemes.js
@@ -1,9 +1,7 @@
-import {ThemeBuilder} from "@ui5/builder/processors/themeBuilder";
-import fsInterface from "@ui5/fs/fsInterface";
-import {basename, dirname} from "node:path/posix";
-import etag from "etag";
-import fresh from "fresh";
-import parseurl from "parseurl";
+const themeBuilder = require("@ui5/builder").processors.themeBuilder;
+const fsInterface = require("@ui5/fs").fsInterface;
+const etag = require("etag");
+const fresh = require("fresh");
 
 function isFresh(req, res) {
 	return fresh(req.headers, {
@@ -11,113 +9,69 @@ function isFresh(req, res) {
 	});
 }
 
-// List of experimental css variables resources that should activate the "cssVariables" build
-const cssVariablesThemeResources = [
-	"css_variables.source.less",
-	"css_variables.css",
-	"library_skeleton.css",
-	"library_skeleton-RTL.css"
-];
+const themeRequest = /^(.*\/)library(?:(\.css)|(-RTL\.css)|(-parameters\.json))$/i;
 
-// List of resources that should be handled by the middleware
-const themeResources = [
-	"library.css",
-	"library-RTL.css",
-	"library-parameters.json",
-	...cssVariablesThemeResources
-];
-
-/**
- * Creates and returns the middleware to build themes.
- *
- * The theme is built in realtime. If a less file was modified, the theme build is triggered to rebuild the theme.
- *
- * @module @ui5/server/middleware/serveThemes
- * @param {object} parameters Parameters
- * @param {@ui5/server/internal/MiddlewareManager.middlewareResources} parameters.resources Parameters
- * @param {object} parameters.middlewareUtil Specification version dependent interface to a
- *                                        [MiddlewareUtil]{@link @ui5/server/middleware/MiddlewareUtil} instance
- * @returns {Function} Returns a server middleware closure.
- */
-function createMiddleware({resources, middlewareUtil}) {
-	const builder = new ThemeBuilder({
-		fs: fsInterface(resources.all)
+function createMiddleware({resourceCollections}) {
+	const builder = new themeBuilder.ThemeBuilder({
+		fs: fsInterface(resourceCollections.combo)
 	});
-	const buildOptions = Object.create(null);
 
-	const currentRequests = Object.create(null);
+	return function theme(req, res, next) {
+		/* req.path examples:
+			/resources/sap/ui/core/themes/sap_belize/library.css
+		*/
 
-	async function buildTheme(pathname) {
-		const filename = basename(pathname);
-
-		if (cssVariablesThemeResources.includes(filename) && !buildOptions.cssVariables) {
-			// Activate CSS Variables build the first time a relevant resource is requested
-			buildOptions.cssVariables = true;
-			// Clear the cache to ensure that the build is executed again with "cssVariables: true"
-			builder.clearCache();
-		}
-
-		const sourceLessPath = dirname(pathname) + "/library.source.less";
-		const sourceLessResource = await resources.all.byPath(sourceLessPath);
-		if (!sourceLessResource) { // Not found
+		/* groups (array index):
+			 1 => theme directory (example: /resources/sap/ui/core/themes/sap_belize/)
+			 2 => .css suffix
+			 3 => -RTL.css suffix
+			 4 => -parameters.json suffix
+		*/
+		var themeReq = themeRequest.exec(req.path);
+		if (!themeReq) {
+			next();
 			return;
 		}
 
-		const createdResources = await builder.build([sourceLessResource], buildOptions);
-
-		// Pick requested file resource
-		const resource = createdResources.find((res) => basename(res.getPath()) === filename);
-		if (!resource) {
-			throw new Error(`Theme Build did not return requested file "${pathname}"`);
-		}
-
-		return resource;
-	}
-
-	async function sendResponse(req, res, resource) {
-		const resourcePath = resource.getPath();
-		const {contentType} = middlewareUtil.getMimeInfo(resourcePath);
-		res.setHeader("Content-Type", contentType);
-
-		const content = await resource.getBuffer();
-
-		res.setHeader("ETag", etag(content));
-
-		if (isFresh(req, res)) {
-			// client has a fresh copy of the resource
-			res.statusCode = 304;
-			res.end();
-			return;
-		}
-
-		res.end(content);
-	}
-
-	return async function theme(req, res, next) {
-		try {
-			const pathname = parseurl(req).pathname;
-			const filename = basename(pathname);
-			if (!themeResources.includes(filename)) {
+		const sourceLessPath = themeReq[1] + "library.source.less";
+		resourceCollections.combo.byPath(sourceLessPath).then((sourceLessResource) => {
+			if (!sourceLessResource) { // Not found
 				next();
 				return;
 			}
+			return builder.build([sourceLessResource]).then(function([css, cssRtl, parameters]) {
+				let resource;
+				if (themeReq[2]) { // -> .css
+					res.setHeader("Content-Type", "text/css");
+					resource = css;
+				} else if (themeReq[3]) { // -> -RTL.css
+					res.setHeader("Content-Type", "text/css");
+					resource = cssRtl;
+				} else if (themeReq[4]) { // -parameters.json
+					res.setHeader("Content-Type", "application/json");
+					resource = parameters;
+				} else {
+					next("Couldn't decide on which theme file to return. This shouldn't happen");
+					return;
+				}
 
-			if (!currentRequests[pathname]) {
-				currentRequests[pathname] = buildTheme(pathname);
-			}
+				return resource.getBuffer().then((content) => {
+					res.setHeader("ETag", etag(content));
 
-			const resource = await currentRequests[pathname];
-			if (!resource) {
-				next();
-			} else {
-				await sendResponse(req, res, resource);
-			}
+					if (isFresh(req, res)) {
+						// client has a fresh copy of the resource
+						res.statusCode = 304;
+						res.end();
+						return;
+					}
 
-			delete currentRequests[pathname];
-		} catch (err) {
+					res.end(content.toString());
+				});
+			});
+		}).catch(function(err) {
 			next(err);
-		}
+		});
 	};
 }
 
-export default createMiddleware;
+module.exports = createMiddleware;
