8dca8afd46ed3973a745d41f8dd32d2c8434d00d
Tommy Vinh Lam <tommy.vinh.lam@sap.com>
2018-04-17 17:15:16 +0200

    [builder][FEATURE] Add ability to configure component preloads and custom bundles
    - lbt: Ignore missing manifest.json errors
    - moduleBundler: Add parameter for custom bundle options
    - Use /resources as base for all bundle globs

--- packages/builder/test/expected/build/application.h/dest/sectionsA/customBundle.js
+++ packages/builder/test/expected/build/application.h/dest/sectionsA/customBundle.js
@@ -1,4 +1,8 @@
-//@ui5-bundle application/h/sectionsA/customBundle.js
-sap.ui.predefine("application/h/sectionsA/section1", ["sap/m/Button"],function(n){console.log("Section 1 included")});
-sap.ui.predefine("application/h/sectionsA/section3", ["sap/m/Button"],function(n){console.log("Section 3 included")});
-//# sourceMappingURL=customBundle.js.map
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"application/h/sectionsA/section1.js":function(){sap.ui.define(["sap/m/Button"],function(n){console.log("Section 1 included")});
+},
+	"application/h/sectionsA/section3.js":function(){sap.ui.define(["sap/m/Button"],function(n){console.log("Section 3 included")});
+}
+}});
