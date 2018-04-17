71221634da417e494edb693663b065437c63760c
Tommy Vinh Lam <tommy.vinh.lam@sap.com>
2018-04-17 17:15:16 +0200

    [builder][FEATURE] Add ability to configure component preloads and custom bundles
    - lbt: Ignore missing manifest.json errors
    - moduleBundler: Add parameter for custom bundle options
    - Use /resources as base for all bundle globs

--- packages/builder/test/expected/build/application.g/dest/subcomponentA/Component-preload.js
+++ packages/builder/test/expected/build/application.g/dest/subcomponentA/Component-preload.js
@@ -1,6 +1,9 @@
-//@ui5-bundle application/g/subcomponentA/Component-preload.js
-sap.ui.predefine("application/g/subcomponentA/Component", ["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.subcomponentA.Component",{metadata:{manifest:"json"}})});
-sap.ui.require.preload({
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"application/g/subcomponentA/Component-dbg.js":function(){sap.ui.define(["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.subcomponentA.Component",{metadata:{manifest:"json"}})});
+},
+	"application/g/subcomponentA/Component.js":function(){sap.ui.define(["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.subcomponentA.Component",{metadata:{manifest:"json"}})});
+},
 	"application/g/subcomponentA/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"application.g.subcomponentA","type":"application","applicationVersion":{"version":"1.2.2"},"embeds":["embedded"],"title":"{{title}}"}}'
-});
-//# sourceMappingURL=Component-preload.js.map
+}});
