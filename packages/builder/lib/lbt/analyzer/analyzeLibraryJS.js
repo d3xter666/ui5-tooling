61a603f4b6f3beee5c7d69d5690d917005258712
Frank Weigel <frank.weigel@sap.com>
2018-06-29 18:18:22 +0200

    [builder][INTERNAL] Create and integrate a new task 'generateLibraryManifest' (https://github.com/SAP/ui5-builder/pull/26)
    The task creates a manifest.json file for libraries that don't have one.
    It collects the necessary information from the project structure
    (supported themes, versions of dependencies), from the .library file
    (name, dependencies, supported themes, thirdparty components, i18n
    information...) and from an initLibrary call inside the library.js file
    (implemented controls, elements, types and interfaces, noLibraryCSS
    flag).
    
    An existing manifest.json won't be modified or overwritten.
    
    - refactor test scenario 'library.h' so that the sub-components reside
      in the namespace of the library
    - add new test scenario 'library.i' to test manifest creation with 
      data from .library and library.js file
    - add the newly generated manifest.json to the expected content of 
      the other library test scenarios
    - enhance all library test scenarios with a dependency to a fake 
      'sap.ui.core' lib. For UI5 libs, this dependency is mandatory and 
      manifest generation needs it to determine the UI5 version
    - re-classify log message for missing .library file as 'verbose'

--- packages/builder/lib/lbt/analyzer/analyzeLibraryJS.js
+++ packages/builder/lib/lbt/analyzer/analyzeLibraryJS.js
@@ -1,25 +1,17 @@
-
-import {parseJS, Syntax, VisitorKeys} from "../utils/parseUtils.js";
-import {getPropertyKey, isMethodCall, isIdentifier, getStringArray} from "../utils/ASTUtils.js";
-import SapUiDefineCall from "../calls/SapUiDefine.js";
-import {getLogger} from "@ui5/logger";
-const log = getLogger("lbt:analyzer:LibraryJS");
+"use strict";
+const esprima = require("esprima");
+const {Syntax} = esprima;
+const {getPropertyKey, isMethodCall, isIdentifier, getStringArray} = require("../utils/ASTUtils");
+const VisitorKeys = require("estraverse").VisitorKeys;
 
 const CALL__SAP_UI_GETCORE = ["sap", "ui", "getCore"];
-const CALL_SAP_UI_DEFINE = ["sap", "ui", "define"];
-const CALL_AMD_DEFINE = ["define"];
-const AMD__LIB_INIT_RESOURCES = [
-	// Dependency definition, init library method name
-	["sap/ui/core/Core.js", "initLibrary"],
-	["sap/ui/core/Lib.js", "init"]
-];
 
 /*
  * Static Code Analyzer that extracts library information from the sap.ui.getCore().initLibrary()
  * call in a library.js module.
  */
 async function analyze(resource) {
-	const libInfo = {
+	let libInfo = {
 		noLibraryCSS: false,
 		types: [],
 		controls: [],
@@ -27,58 +19,35 @@ async function analyze(resource) {
 		interfaces: []
 	};
 
-	const code = await resource.getBuffer();
-	const rootNode = parseJS(code);
-	let analyzedDefineCall = null;
-	visit( rootNode );
-
 	function visit(node) {
-		if ( node.type === Syntax.CallExpression ) {
-			// Define call would always be at the top of the tree, so it'd
-			// be the first significant thing that's been found.
-			analyzedDefineCall = analyzedDefineCall || analyzeSapUiDefineCalls(node);
+		if ( node.type == Syntax.CallExpression
+				&& node.callee.type === Syntax.MemberExpression
+				&& isMethodCall(node.callee.object, CALL__SAP_UI_GETCORE)
+				&& isIdentifier(node.callee.property, "initLibrary")
+				&& node.arguments.length === 1
+				&& node.arguments[0].type === Syntax.ObjectExpression ) {
+			node.arguments[0].properties.forEach( (prop) => {
+				let key = getPropertyKey(prop);
+				let value = prop.value;
+				if ( key === "noLibraryCSS"
+						&& (value.type === Syntax.Literal && typeof value.value === "boolean") ) {
+					libInfo.noLibraryCSS = value.value;
+				} else if ( key === "types" && value.type == Syntax.ArrayExpression ) {
+					libInfo.types = getStringArray(value, true);
+				} else if ( key === "interfaces" && value.type == Syntax.ArrayExpression ) {
+					libInfo.interfaces = getStringArray(value, true);
+				} else if ( key === "controls" && value.type == Syntax.ArrayExpression ) {
+					libInfo.controls = getStringArray(value, true);
+				} else if ( key === "elements" && value.type == Syntax.ArrayExpression ) {
+					libInfo.elements = getStringArray(value, true);
+				}
+			});
 
-			if (isInitLibraryCall(node)) {
-				node.arguments[0].properties.forEach( (prop) => {
-					if (prop.type === Syntax.SpreadElement) {
-						// SpreadElements are currently not supported
-						return;
-					}
-
-					const key = getPropertyKey(prop);
-					const value = prop.value;
-
-					if ( key === "noLibraryCSS" &&
-							(value.type === Syntax.Literal && typeof value.value === "boolean") ) {
-						libInfo.noLibraryCSS = value.value;
-					} else if ( key === "types" && value.type == Syntax.ArrayExpression ) {
-						libInfo.types = getStringArray(value, true);
-					} else if ( key === "interfaces" && value.type == Syntax.ArrayExpression ) {
-						libInfo.interfaces = getStringArray(value, true);
-					} else if ( key === "controls" && value.type == Syntax.ArrayExpression ) {
-						libInfo.controls = getStringArray(value, true);
-					} else if ( key === "elements" && value.type == Syntax.ArrayExpression ) {
-						libInfo.elements = getStringArray(value, true);
-					} else if ( ["designtime", "dependencies", "extensions", "name", "version"].includes(key) ) {
-						// do nothing, for all other supported properties
-					} else if ( key === "apiVersion" &&
-						(value.type === Syntax.Literal && typeof value.value === "number") ) {
-						// just a validation
-					} else {
-						log.error(
-							"Unexpected property '" + key +
-							"' or wrong type for '" + key +
-							"' for a library initialization call in '" + resource.getPath() + "'"
-						);
-					}
-				});
-
-				return true; // abort, we're done
-			}
+			return true; // abort, we're done
 		}
 
-		for ( const key of VisitorKeys[node.type] ) {
-			const child = node[key];
+		for ( let key of VisitorKeys[node.type] ) {
+			let child = node[key];
 			if ( Array.isArray(child) ) {
 				if ( child.some(visit) ) {
 					return true;
@@ -93,64 +62,8 @@ async function analyze(resource) {
 		return false;
 	}
 
-	/**
-	 * Finds and extracts dependencies from sap.ui.define call
-	 *
-	 * @param {Node} node
-	 * @returns {SapUiDefineCall}
-	 */
-	function analyzeSapUiDefineCalls(node) {
-		// Analyze sap.ui.define calls
-		if ( [CALL_AMD_DEFINE, CALL_SAP_UI_DEFINE].some(
-			(defCall) => isMethodCall(node, defCall)) ) {
-			const defCall = new SapUiDefineCall(node, resource.getName());
-
-			return AMD__LIB_INIT_RESOURCES
-				// Resolve dependency variable names.
-				.map((dependency) => [defCall.findImportName(dependency[0]), dependency[1]])
-				// Remove potential dependencies that are not actually present.
-				.filter((dependency) => dependency[0]);
-		}
-
-		return null;
-	}
-
-	/**
-	 * Determines whether the node is an initLibrary call.
-	 *
-	 * @param {Node} node
-	 * @returns {boolean}
-	 */
-	function isInitLibraryCall(node) {
-		// sap.ui.getCore()
-		if (
-			node.type == Syntax.CallExpression &&
-			node.callee.type === Syntax.MemberExpression &&
-			isMethodCall(node.callee.object, CALL__SAP_UI_GETCORE) &&
-			isIdentifier(node.callee.property, "initLibrary") &&
-			node.arguments.length === 1 &&
-			node.arguments[0].type === Syntax.ObjectExpression
-		) {
-			return true;
-		}
-
-		// Find a CallExpression that is initLibrary
-		// i.e. node.callee.object === "Core" && node.callee.property === "initLibrary"
-		if (
-			node.type === Syntax.CallExpression &&
-			node.callee.type === Syntax.MemberExpression &&
-			analyzedDefineCall &&
-			analyzedDefineCall.some((potentialDependency) =>
-				isMethodCall(node, potentialDependency)
-			) &&
-			node.arguments.length === 1 &&
-			node.arguments[0].type === Syntax.ObjectExpression
-		) {
-			return true;
-		}
-
-		return false;
-	}
+	let code = await resource.getBuffer();
+	visit( esprima.parse(code) );
 
 	return libInfo;
 }
@@ -170,12 +83,12 @@ async function analyze(resource) {
  *
  * Note: only the first initLibrary() call that is found with a DFS on the AST, will be evaluated.
  *
- * @param {@ui5/fs/Resource} resource library.js resource whose content should be analyzed
+ * @param {Resource} resource library.js resource whose content should be analyzed
  * @returns {Promise<object>} A Promise on the extract info object
  */
-export default function(resource) {
+module.exports = function(resource) {
 	if ( resource == null ) {
 		return Promise.resolve({});
 	}
 	return analyze(resource);
-}
+};
