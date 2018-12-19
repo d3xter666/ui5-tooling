5ec543ff3c4df7ada1da68b20e036b7f50070fa5
Domi <dominik.garrecht@googlemail.com>
2018-12-19 18:07:03 +0100

    [fs] Increase test coverage for fs module (https://github.com/SAP/ui5-fs/pull/56)
    * Increase test coverage for ReaderCollection, DuplexCollection, ReaderCollectionPrioritized, Resource, traceSummary, resourceFactory, AbstractReaderWriter, AbstractReader
    
    * Apply Matthias' code review comments

--- packages/fs/test/lib/AbstractReaderWriter.js
+++ packages/fs/test/lib/AbstractReaderWriter.js
@@ -1,25 +1,10 @@
-import test from "ava";
-import AbstractReaderWriter from "../../lib/AbstractReaderWriter.js";
+const {test} = require("ava");
+const AbstractReaderWriter = require("../../lib/AbstractReaderWriter");
 
 test("AbstractReaderWriter: constructor throws an error", (t) => {
-	t.throws(() => {
+	const error = t.throws(() => {
 		new AbstractReaderWriter();
-	}, {
-		instanceOf: TypeError,
-		message: "Class 'AbstractReaderWriter' is abstract"
-	});
+	}, TypeError);
+
+	t.is(error.message, "Class 'AbstractReaderWriter' is abstract");
 });
-
-test("Incomplete AbstractReaderWriter subclass: Abstract functions throw error", (t) => {
-	class Dummy extends AbstractReaderWriter {}
-
-	const instance = new Dummy();
-
-	t.throws(() => {
-		instance._write();
-	}, {
-		instanceOf: Error,
-		message: "Not implemented"
-	});
-});
-
