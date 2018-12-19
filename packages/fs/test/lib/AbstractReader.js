99d7b29cf178525a6d93093ee6c39f3f76795dbb
Domi <dominik.garrecht@googlemail.com>
2018-12-19 18:07:03 +0100

    [fs] Increase test coverage for fs module (https://github.com/SAP/ui5-fs/pull/56)
    * Increase test coverage for ReaderCollection, DuplexCollection, ReaderCollectionPrioritized, Resource, traceSummary, resourceFactory, AbstractReaderWriter, AbstractReader
    
    * Apply Matthias' code review comments

--- packages/fs/test/lib/AbstractReader.js
+++ packages/fs/test/lib/AbstractReader.js
@@ -1,37 +1,10 @@
-import test from "ava";
-import AbstractReader from "../../lib/AbstractReader.js";
+const {test} = require("ava");
+const AbstractReader = require("../../lib/AbstractReader");
 
 test("AbstractReader: constructor throws an error", (t) => {
-	t.throws(() => {
+	const error = t.throws(() => {
 		new AbstractReader();
-	}, {
-		instanceOf: TypeError,
-		message: "Class 'AbstractReader' is abstract"
-	});
-});
+	}, TypeError);
 
-test("Incomplete AbstractReader subclass: Abstract functions throw error", (t) => {
-	class Dummy extends AbstractReader {}
-
-	const instance = new Dummy();
-	t.throws(() => {
-		instance._byGlob();
-	}, {
-		instanceOf: Error,
-		message: "Function '_byGlob' is not implemented"
-	});
-
-	t.throws(() => {
-		instance._runGlob();
-	}, {
-		instanceOf: Error,
-		message: "Function '_runGlob' is not implemented"
-	});
-
-	t.throws(() => {
-		instance._byPath();
-	}, {
-		instanceOf: Error,
-		message: "Function '_byPath' is not implemented"
-	});
+	t.is(error.message, "Class 'AbstractReader' is abstract");
 });
