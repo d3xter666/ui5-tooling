e58cc810a872f7c5a284726291a89e5e18b02760
Domi <dominik.garrecht@googlemail.com>
2018-12-19 18:07:03 +0100

    [fs] Increase test coverage for fs module (https://github.com/SAP/ui5-fs/pull/56)
    * Increase test coverage for ReaderCollection, DuplexCollection, ReaderCollectionPrioritized, Resource, traceSummary, resourceFactory, AbstractReaderWriter, AbstractReader
    
    * Apply Matthias' code review comments

--- packages/fs/test/lib/DuplexCollection.js
+++ packages/fs/test/lib/DuplexCollection.js
@@ -1,8 +1,8 @@
-import test from "ava";
-import sinon from "sinon";
-import DuplexCollection from "../../lib/DuplexCollection.js";
-import ReaderCollectionPrioritized from "../../lib/ReaderCollectionPrioritized.js";
-import Resource from "../../lib/Resource.js";
+const {test} = require("ava");
+const sinon = require("sinon");
+const DuplexCollection = require("../../lib/DuplexCollection");
+const ReaderCollectionPrioritized = require("../../lib/ReaderCollectionPrioritized");
+const Resource = require("../../lib/Resource");
 
 test("DuplexCollection: constructor", (t) => {
 	const duplexCollection = new DuplexCollection({
@@ -14,7 +14,7 @@ test("DuplexCollection: constructor", (t) => {
 	t.deepEqual(duplexCollection._reader, {}, "reader assigned");
 	t.deepEqual(duplexCollection._writer, {}, "writer assigned");
 	t.true(duplexCollection._combo instanceof ReaderCollectionPrioritized, "prioritized reader collection created");
-	t.is(duplexCollection._combo.getName(), "myCollection - ReaderCollectionPrioritized", "name assigned");
+	t.deepEqual(duplexCollection._combo._name, "myCollection", "name assigned");
 	t.deepEqual(duplexCollection._combo._readers, [{}, {}], "reader and writer assigned to readers");
 });
 
@@ -27,11 +27,11 @@ test("DuplexCollection: constructor with setting default name of an empty string
 	t.deepEqual(duplexCollection._reader, {}, "reader assigned");
 	t.deepEqual(duplexCollection._writer, {}, "writer assigned");
 	t.true(duplexCollection._combo instanceof ReaderCollectionPrioritized, "prioritized reader collection created");
-	t.is(duplexCollection._combo.getName(), " - ReaderCollectionPrioritized", "name assigned");
+	t.deepEqual(duplexCollection._combo._name, "", "name assigned");
 	t.deepEqual(duplexCollection._combo._readers, [{}, {}], "reader and writer assigned to readers");
 });
 
-test("DuplexCollection: _byGlob w/o finding a resource", async (t) => {
+test("DuplexCollection: _byGlob w/o finding a resource", (t) => {
 	t.plan(3);
 
 	const abstractReader = {
@@ -47,20 +47,21 @@ test("DuplexCollection: _byGlob w/o finding a resource", async (t) => {
 	};
 	const comboSpy = sinon.spy(duplexCollection._combo, "_byGlob");
 
-	const resources = await duplexCollection._byGlob("anyPattern", {someOption: true}, trace);
-
-	t.true(Array.isArray(resources), "Found resources are returned as an array");
-	t.true(resources.length === 0, "No resources found");
-	t.true(comboSpy.calledWithExactly("anyPattern", {someOption: true}, trace),
-		"Delegated globbing task correctly to readers");
+	return duplexCollection._byGlob("anyPattern", {someOption: true}, trace)
+		.then(function(resources) {
+			t.true(Array.isArray(resources), "Found resources are returned as an array");
+			t.true(resources.length === 0, "No resources found");
+			t.true(comboSpy.calledWithExactly("anyPattern", {someOption: true}, trace),
+				"Delegated globbing task correctly to readers");
+		});
 });
 
-test("DuplexCollection: _byGlob", async (t) => {
+test("DuplexCollection: _byGlob", (t) => {
 	t.plan(5);
 
 	const resource = new Resource({
-		path: "/my/path",
-		buffer: Buffer.from("content")
+		path: "my/path",
+		buffer: new Buffer("content")
 	});
 	const abstractReader = {
 		_byGlob: sinon.stub().returns(Promise.resolve([resource]))
@@ -74,23 +75,76 @@ test("DuplexCollection: _byGlob", async (t) => {
 		collection: sinon.spy()
 	};
 	const comboSpy = sinon.spy(duplexCollection._combo, "_byGlob");
-	const resources = await duplexCollection._byGlob("anyPattern", {someOption: true}, trace);
-	const resourceContent = await resource.getString();
 
-	t.true(Array.isArray(resources), "Found resources are returned as an array");
-	t.true(resources.length === 1, "Resource found");
-	t.is(resource.getPath(), "/my/path", "Resource has expected path");
-	t.true(comboSpy.calledWithExactly("anyPattern", {someOption: true}, trace),
-		"Delegated globbing task correctly to readers");
-	t.is(resourceContent, "content", "Resource has expected content");
+	return duplexCollection._byGlob("anyPattern", {someOption: true}, trace)
+		.then(function(resources) {
+			t.true(Array.isArray(resources), "Found resources are returned as an array");
+			t.true(resources.length === 1, "Resource found");
+			t.deepEqual(resource.getPath(), "my/path", "Resource has expected path");
+			t.true(comboSpy.calledWithExactly("anyPattern", {someOption: true}, trace),
+				"Delegated globbing task correctly to readers");
+			return resource.getString().then(function(content) {
+				t.deepEqual(content, "content", "Resource has expected content");
+			});
+		});
 });
 
-test("DuplexCollection: _byPath with reader finding a resource", async (t) => {
+test("DuplexCollection: _byGlobSource w/o found resources", (t) => {
+	t.plan(3);
+
+	const abstractReader = {
+		byGlob: sinon.stub().returns(Promise.resolve([]))
+	};
+	const duplexCollection = new DuplexCollection({
+		name: "myCollection",
+		reader: abstractReader,
+		writer: abstractReader
+	});
+
+	return duplexCollection.byGlobSource("anyPattern", {someOption: true})
+		.then(function(resources) {
+			t.true(Array.isArray(resources), "Found resources are returned as an array");
+			t.true(resources.length === 0, "No resources found");
+			t.true(abstractReader.byGlob.calledWithExactly("anyPattern", {someOption: true}),
+				"Delegated globbing task correctly to readers");
+		});
+});
+
+test("DuplexCollection: _byGlobSource with default options and a reader finding a resource", (t) => {
+	t.plan(3);
+
+	const resource = new Resource({
+		path: "my/path",
+		buffer: new Buffer("content")
+	});
+	const abstractReader = {
+		byGlob: sinon.stub().returns(Promise.resolve([resource]))
+	};
+	const abstractWriter = {
+		byPath: sinon.stub().returns(Promise.resolve())
+	};
+	const duplexCollection = new DuplexCollection({
+		name: "myCollection",
+		reader: abstractReader,
+		writer: abstractWriter
+	});
+
+	return duplexCollection.byGlobSource("anyPattern")
+		.then(function(resources) {
+			t.true(Array.isArray(resources), "Found resources are returned as an array");
+			t.true(abstractReader.byGlob.calledWithExactly("anyPattern", {}),
+				"Delegated globbing task correctly to readers");
+			t.true(abstractWriter.byPath.calledWithExactly("my/path"),
+				"byPath called on writer");
+		});
+});
+
+test("DuplexCollection: _byPath with reader finding a resource", (t) => {
 	t.plan(4);
 
 	const resource = new Resource({
-		path: "/path",
-		buffer: Buffer.from("content")
+		path: "path",
+		buffer: new Buffer("content")
 	});
 	const pushCollectionSpy = sinon.spy(resource, "pushCollection");
 	const abstractReader = {
@@ -105,17 +159,20 @@ test("DuplexCollection: _byPath with reader finding a resource", async (t) => {
 		writer: abstractReader
 	});
 	const comboSpy = sinon.spy(duplexCollection._combo, "_byPath");
-	const readResource = await duplexCollection._byPath("anyVirtualPath", {someOption: true}, trace);
-	const readResourceContent = await readResource.getString();
 
-	t.true(comboSpy.calledWithExactly("anyVirtualPath", {someOption: true}, trace),
-		"Delegated globbing task correctly to readers");
-	t.true(pushCollectionSpy.called, "pushCollection called on resource");
-	t.is(readResource.getPath(), "/path", "Resource has expected path");
-	t.is(readResourceContent, "content", "Resource has expected content");
+	return duplexCollection._byPath("anyVirtualPath", {someOption: true}, trace)
+		.then(function(resource) {
+			t.true(comboSpy.calledWithExactly("anyVirtualPath", {someOption: true}, trace),
+				"Delegated globbing task correctly to readers");
+			t.true(pushCollectionSpy.called, "pushCollection called on resource");
+			t.deepEqual(resource.getPath(), "path", "Resource has expected path");
+			return resource.getString().then(function(content) {
+				t.deepEqual(content, "content", "Resource has expected content");
+			});
+		});
 });
 
-test("DuplexCollection: _byPath with two readers both finding no resource", async (t) => {
+test("DuplexCollection: _byPath with two readers both finding no resource", (t) => {
 	t.plan(3);
 
 	const abstractReaderOne = {
@@ -132,52 +189,33 @@ test("DuplexCollection: _byPath with two readers both finding no resource", asyn
 		reader: abstractReaderOne,
 		writer: abstractReaderTwo
 	});
-	const readResource = await duplexCollection._byPath("anyVirtualPath", {someOption: true}, trace);
 
-	t.true(abstractReaderOne._byPath.calledWithExactly("anyVirtualPath", {someOption: true}, trace),
-		"Delegated globbing task correctly to reader one");
-	t.true(abstractReaderTwo._byPath.calledWithExactly("anyVirtualPath", {someOption: true}, trace),
-		"Delegated globbing task correctly to reader two");
-	t.falsy(readResource, "No resource found");
+	return duplexCollection._byPath("anyVirtualPath", {someOption: true}, trace)
+		.then(function(resource) {
+			t.true(abstractReaderOne._byPath.calledWithExactly("anyVirtualPath", {someOption: true}, trace),
+				"Delegated globbing task correctly to reader one");
+			t.true(abstractReaderTwo._byPath.calledWithExactly("anyVirtualPath", {someOption: true}, trace),
+				"Delegated globbing task correctly to reader two");
+			t.falsy(resource, "No resource found");
+		});
 });
 
-test("DuplexCollection: _write successful", async (t) => {
+test("DuplexCollection: _write successful", (t) => {
 	t.plan(1);
 
 	const resource = new Resource({
-		path: "/my/path",
-		buffer: Buffer.from("content")
+		path: "my/path",
+		buffer: new Buffer("content")
 	});
 	const duplexCollection = new DuplexCollection({
 		name: "myCollection",
-		reader: {},
 		writer: {
 			write: sinon.stub().returns(Promise.resolve())
 		}
 	});
-	await duplexCollection._write(resource);
 
-	t.pass("write on writer called");
-});
-
-test("DuplexCollection: Throws for empty reader", (t) => {
-	t.throws(() => {
-		new DuplexCollection({
-			name: "myReader",
-			writer: {}
+	return duplexCollection._write(resource)
+		.then(function(resource) {
+			t.pass("write on writer called");
 		});
-	}, {
-		message: "Cannot create DuplexCollection myReader: No reader provided"
-	});
-});
-
-test("DuplexCollection: Throws for empty writer", (t) => {
-	t.throws(() => {
-		new DuplexCollection({
-			name: "myReader",
-			reader: {}
-		});
-	}, {
-		message: "Cannot create DuplexCollection myReader: No writer provided"
-	});
 });
