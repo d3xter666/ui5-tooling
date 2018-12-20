77120e82b817ce896d4de76a4f69c83634d9de1f
Sven Bender <susouke@gmail.com>
2018-12-20 16:05:24 +0100

    [server][INTERNAL] improve test coverage (https://github.com/SAP/ui5-server/pull/88)
    Added more tests to improve test coverage.

--- packages/server/test/lib/server/acceptRemoteConnections.js
+++ packages/server/test/lib/server/acceptRemoteConnections.js
@@ -1,28 +1,30 @@
-import test from "ava";
-import supertest from "supertest";
-import {serve} from "../../../lib/server.js";
-import {graphFromPackageDependencies} from "@ui5/project/graph";
+const {test} = require("ava");
+const supertest = require("supertest");
+const ui5Server = require("../../../");
+const server = ui5Server.server;
+const normalizer = require("@ui5/project").normalizer;
 
 let request;
-let server;
+let serve;
 
 // Start server before running tests
-test.before(async () => {
-	const graph = await graphFromPackageDependencies({
+test.before(() => {
+	return normalizer.generateProjectTree({
 		cwd: "./test/fixtures/application.a"
+	}).then((tree) => {
+		return server.serve(tree, {
+			port: 3334,
+			acceptRemoteConnections: true
+		}).then((serveResult) => {
+			request = supertest("http://127.0.0.1:3334");
+			serve = serveResult;
+		});
 	});
-
-	server = await serve(graph, {
-		port: 3334,
-		acceptRemoteConnections: true
-	});
-
-	request = supertest("http://127.0.0.1:3334");
 });
 
 test.after(() => {
 	return new Promise((resolve, reject) => {
-		server.close((error) => {
+		serve.close((error) => {
 			if (error) {
 				reject(error);
 			} else {
@@ -32,12 +34,13 @@ test.after(() => {
 	});
 });
 
-test("Get resource from application.a (/index.html) with enabled remote connection", async (t) => {
-	const res = await request.get("/index.html");
-	if (res.error) {
-		t.fail(res.error.text);
-	}
-	t.is(res.statusCode, 200, "Correct HTTP status code");
-	t.regex(res.headers["content-type"], /html/, "Correct content type");
-	t.regex(res.text, /<title>Application A<\/title>/, "Correct response");
+test("Get resource from application.a (/index.html) with enabled remote connection", (t) => {
+	return request.get("/index.html").then((res) => {
+		if (res.error) {
+			t.fail(res.error.text);
+		}
+		t.deepEqual(res.statusCode, 200, "Correct HTTP status code");
+		t.regex(res.headers["content-type"], /html/, "Correct content type");
+		t.regex(res.text, /<title>Application A<\/title>/, "Correct response");
+	});
 });
