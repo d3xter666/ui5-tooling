2ea1f544dde99afcc7e62d8b8dd3213fb2250468
Tobias Sorn <tobias.sorn@web.de>
2019-01-08 13:55:16 +0100

    [builder][INTERNAL] Add unit tests (https://github.com/SAP/ui5-builder/pull/120)

--- packages/builder/test/expected/build/library.d/preload/resources/library/d/library-preload.js
+++ packages/builder/test/expected/build/library.d/preload/resources/library/d/library-preload.js
@@ -1,41 +1,9 @@
-//@ui5-bundle library/d/library-preload.js
-/*
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"library/d/some.js":function(){/*!
  * ${copyright}
  */
-
-sap.ui.predefine("library/d/library", [
-	"sap/ui/core/Lib",
-	"sap/ui/core/library"
-], function (Library) {
-	"use strict";
-
-	return Library.init({
-		name: "library.d",
-		version: "${version}",
-		dependencies: [
-			"sap.ui.core"
-		],
-		types: [],
-		interfaces: [],
-		controls: [],
-		elements: [],
-		noLibraryCSS: true
-	});
-});
-/*!
- * ${copyright}
- */
-
-sap.ui.predefine("library/d/some", ["./library"], (_library) => {
-	var someNonUglifiedVariable = "World";
-	console.log('Hello ' + someNonUglifiedVariable);
-});
-sap.ui.require.preload({
-	"library/d/empty.js":function(){
-/*!
- * ${copyright}
- */
-},
-	"library/d/legacy.js":'/*!\n * ${copyright}\n */\nvar topLevelVar = "Old World";\nconsole.log(\'Hello \' + topLevelVar);\n'
-});
-//# sourceMappingURL=library-preload.js.map
+console.log("HelloWorld");
+}
+}});
