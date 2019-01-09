07aeee74fa3380308d2768d6b7f441395880301c
Matthias Oßwald <matz3@users.noreply.github.com>
2019-01-09 17:38:32 +0100

    [builder][FEATURE] Add transformation of apps index.html in self-contained build (https://github.com/SAP/ui5-builder/pull/137)
    BREAKING CHANGE:
    When running a self-contained build on an application project, the
    index.html will be transformed by adopting the UI5 bootstrap script tag
    to load the custom bundle file instead.

--- packages/builder/lib/processors/bootstrapHtmlTransformer.js
+++ packages/builder/lib/processors/bootstrapHtmlTransformer.js
@@ -1,41 +1,31 @@
-import {getLogger} from "@ui5/logger";
-const log = getLogger("builder:processors:bootstrapHtmlTransformer");
-import * as cheerio from "cheerio";
-
-/**
- * @module @ui5/builder/processors/bootstrapHtmlTransformer
- */
+const log = require("@ui5/logger").getLogger("builder:processors:bootstrapHtmlTransformer");
+const cheerio = require("cheerio");
 
 /**
  * Transforms the UI5 bootstrap of a HTML resource files.
  *
- * @public
- * @function default
- * @static
- *
- * @param {object} parameters Parameters
- * @param {@ui5/fs/Resource[]} parameters.resources List of resources to be processed
- * @param {object} parameters.options Options
- * @param {string} parameters.options.src Bootstrap "src" that should be used
- * @returns {Promise<@ui5/fs/Resource[]>} Promise resolving with the cloned resources
+ * @module builder/processors/bootstrapHtmlTransformer
+ * @param {Object} parameters Parameters
+ * @param {module:@ui5/fs.Resource[]} parameters.resources List of resources to be processed
+ * @param {Object} [parameters.options] Options
+ * @param {string} [parameters.options.src] Bootstrap "src" that should be used
+ * @returns {Promise<module:@ui5/fs.Resource[]>} Promise resolving with the cloned resources
  */
-export default function({resources, options: {src}}) {
+module.exports = function({resources, options}) {
 	async function processResource(resource) {
 		const content = await resource.getString();
 		const $ = cheerio.load(content);
 		const bootstrapScript = $("script#sap-ui-bootstrap");
 		if (bootstrapScript.length === 1) {
-			bootstrapScript.attr("src", src);
+			bootstrapScript.attr("src", options.src);
 			resource.setString($.html());
 		} else if (bootstrapScript.length > 1) {
-			log.warn("Skipping bootstrap transformation. " +
-				"Found multiple bootstrap script tags with id=sap-ui-bootstrap.");
+			log.warn("Skipping bootstrap transformation. Found multiple bootstrap script tags with id=sap-ui-bootstrap.");
 		} else {
-			log.warn("Skipping bootstrap transformation. " +
-				"Could not find bootstrap script tag with id=sap-ui-bootstrap.");
+			log.warn("Skipping bootstrap transformation. Could not find bootstrap script tag with id=sap-ui-bootstrap.");
 		}
 		return resource;
 	}
 
 	return Promise.all(resources.map(processResource));
-}
+};
