7096575d7110fb73e4b13739b5bb702b62983a84
Merlin Beutlberger <m.beutlberger@sap.com>
2019-01-29 20:38:47 +0100

    [fs][INTERNAL] Memory Adapter: Fix directory globbing

--- packages/fs/test/lib/adapters/FileSystem.js
+++ packages/fs/test/lib/adapters/FileSystem.js
@@ -1,13 +1,63 @@
-import test from "ava";
+const {test} = require("ava");
+const {resourceFactory} = require("../../../");
 
-import FileSystem from "../../../lib/adapters/FileSystem.js";
+test("GLOB resources from application.a w/ virtual base path prefix", async (t) => {
+	const readerWriter = resourceFactory.createAdapter({
+		fsBasePath: "./test/fixtures/application.a/webapp",
+		virBasePath: "/app/"
+	});
 
-test.serial("Missing parameter: fsBasePath", (t) => {
-	t.throws(() => {
-		new FileSystem({
-			virBasePath: "/"
-		});
-	}, {
-		message: "Unable to create adapter: Missing parameter 'fsBasePath'"
-	}, "Threw with expected error message");
+	const resources = await readerWriter.byGlob("/app/**/*.html");
+	t.deepEqual(resources.length, 1, "Found exactly one resource");
+});
+
+test("GLOB resources from application.a w/o virtual base path prefix", async (t) => {
+	const readerWriter = resourceFactory.createAdapter({
+		fsBasePath: "./test/fixtures/application.a/webapp",
+		virBasePath: "/app/"
+	});
+
+	const resources = await readerWriter.byGlob("/**/*.html");
+	t.deepEqual(resources.length, 1, "Found exactly one resource");
+});
+
+
+test("GLOB virtual directory w/o virtual base path prefix", async (t) => {
+	const readerWriter = resourceFactory.createAdapter({
+		fsBasePath: "./test/fixtures/application.a/webapp",
+		virBasePath: "/app/"
+	});
+
+	const resources = await readerWriter.byGlob("/*", {nodir: false});
+	t.deepEqual(resources.length, 1, "Found exactly one resource");
+});
+
+test("GLOB virtual directory w/ virtual base path prefix", async (t) => {
+	const readerWriter = resourceFactory.createAdapter({
+		fsBasePath: "./test/fixtures/application.a/webapp",
+		virBasePath: "/app/one/two/"
+	});
+
+	const resources = await readerWriter.byGlob("/app/*", {nodir: false});
+	t.deepEqual(resources.length, 1, "Found exactly one resource");
+});
+
+test("GLOB virtual directory w/o virtual base path prefix and nodir: true", async (t) => {
+	const readerWriter = resourceFactory.createAdapter({
+		fsBasePath: "./test/fixtures/application.a/webapp",
+		virBasePath: "/app/"
+	});
+
+	const resources = await readerWriter.byGlob("/*", {nodir: true});
+	t.deepEqual(resources.length, 0, "Found no resources");
+});
+
+test("GLOB virtual directory w/ virtual base path prefix and nodir: true", async (t) => {
+	const readerWriter = resourceFactory.createAdapter({
+		fsBasePath: "./test/fixtures/application.a/webapp",
+		virBasePath: "/app/one/two/"
+	});
+
+	const resources = await readerWriter.byGlob("/app/*", {nodir: true});
+	t.deepEqual(resources.length, 0, "Found no resources");
 });
