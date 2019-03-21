63a6bc32d16dbbcb3cca707c57823a40af86923e
Frank Weigel <frank.weigel@sap.com>
2019-03-21 15:59:02 +0100

    [builder][FEATURE] Add JSDoc build functionalities (https://github.com/SAP/ui5-builder/pull/42)
    New tasks:
    - generateJsdoc
    - executeJsdocSdkTransformation
    - generateApiIndex
    
    New processors:
    - jsdocGenerator
    - sdkTransformer
    - apiIndexGenerator
    
    Can be tested by executing `npm run build-sdk` in the OpenUI5 testsuite project

--- packages/builder/test/lib/tasks/jsdoc/generateApiIndex.js
+++ packages/builder/test/lib/tasks/jsdoc/generateApiIndex.js
@@ -1,31 +1,33 @@
-import test from "ava";
-import sinonGlobal from "sinon";
-import esmock from "esmock";
+const {test} = require("ava");
+const sinon = require("sinon");
+const ui5Fs = require("@ui5/fs");
 
-test.beforeEach(async (t) => {
-	const sinon = t.context.sinon = sinonGlobal.createSandbox();
+const mock = require("mock-require");
 
-	t.context.ReaderCollectionPrioritizedStubClass = sinon.stub();
-	t.context.fsInterfaceStub = sinon.stub().returns("custom fs");
-
-	t.context.apiIndexGeneratorStub = sinon.stub().resolves(["resource A", "resource B"]);
-
-	t.context.generateApiIndex = await esmock("../../../../lib/tasks/jsdoc/generateApiIndex.js", {
-		"@ui5/fs/ReaderCollectionPrioritized": t.context.ReaderCollectionPrioritizedStubClass,
-		"@ui5/fs/fsInterface": t.context.fsInterfaceStub,
-		"../../../../lib/processors/jsdoc/apiIndexGenerator": t.context.apiIndexGeneratorStub
-	});
-});
+const generateApiIndex = require("../../../../lib/tasks/jsdoc/generateApiIndex");
 
 test.afterEach.always((t) => {
-	t.context.sinon.restore();
+	sinon.restore();
 });
 
 test.serial("generateApiIndex", async (t) => {
-	const {
-		sinon, generateApiIndex, apiIndexGeneratorStub,
-		ReaderCollectionPrioritizedStubClass, fsInterfaceStub
-	} = t.context;
+	const apiIndexGeneratorStub = sinon.stub().resolves(["resource A", "resource B"]);
+	const fsInterfaceStub = sinon.stub(ui5Fs, "fsInterface").returns("custom fs");
+	mock("../../../../lib/processors/jsdoc/apiIndexGenerator", apiIndexGeneratorStub);
+
+	class ReaderCollectionPrioritizedStubClass {
+		constructor(parameters) {
+			t.deepEqual(parameters, {
+				name: "generateApiIndex - workspace + dependencies: some.project",
+				readers: [workspace, dependencies]
+			}, "ReaderCollectionPrioritized got called with the correct arguments");
+		}
+	}
+	const readerCollectionPrioritizedStub = ReaderCollectionPrioritizedStubClass;
+	const readerCollectionPrioritizedOrig = ui5Fs.ReaderCollectionPrioritized;
+	ui5Fs.ReaderCollectionPrioritized = readerCollectionPrioritizedStub;
+	const generateApiIndex = mock.reRequire("../../../../lib/tasks/jsdoc/generateApiIndex");
+	ui5Fs.ReaderCollectionPrioritized = readerCollectionPrioritizedOrig;
 
 	const writeStub = sinon.stub().resolves();
 	const workspace = {
@@ -40,18 +42,11 @@ test.serial("generateApiIndex", async (t) => {
 		}
 	});
 
-	t.is(ReaderCollectionPrioritizedStubClass.callCount, 1);
-	t.true(ReaderCollectionPrioritizedStubClass.calledWithNew());
-	t.deepEqual(ReaderCollectionPrioritizedStubClass.getCall(0).args, [{
-		name: "generateApiIndex - workspace + dependencies: some.project",
-		readers: [workspace, dependencies]
-	}], "ReaderCollectionPrioritized got called with the correct arguments");
-
-	t.is(fsInterfaceStub.callCount, 1, "fsInterface got called once");
+	t.deepEqual(fsInterfaceStub.callCount, 1, "fsInterface got called once");
 	t.true(fsInterfaceStub.getCall(0).args[0] instanceof ReaderCollectionPrioritizedStubClass,
 		"fsInterface got called with an instance of ReaderCollectionPrioritizedStubClass");
 
-	t.is(apiIndexGeneratorStub.callCount, 1, "apiIndexGenerator processor got called once");
+	t.deepEqual(apiIndexGeneratorStub.callCount, 1, "apiIndexGenerator processor got called once");
 	t.deepEqual(apiIndexGeneratorStub.getCall(0).args[0], {
 		versionInfoPath: "/resources/sap-ui-version.json",
 		testResourcesRootPath: "/test-resources",
@@ -62,14 +57,18 @@ test.serial("generateApiIndex", async (t) => {
 		fs: "custom fs"
 	}, "apiIndexGenerator got called with correct arguments");
 
-	t.is(writeStub.callCount, 2, "Write got called twice");
-	t.is(writeStub.getCall(0).args[0], "resource A", "Write got called with correct arguments");
-	t.is(writeStub.getCall(1).args[0], "resource B", "Write got called with correct arguments");
+	t.deepEqual(writeStub.callCount, 2, "Write got called twice");
+	t.deepEqual(writeStub.getCall(0).args[0], "resource A", "Write got called with correct arguments");
+	t.deepEqual(writeStub.getCall(1).args[0], "resource B", "Write got called with correct arguments");
+
+	mock.stop("../../../../lib/processors/jsdoc/apiIndexGenerator");
+
+	sinon.restore();
+	mock.reRequire("../../../../lib/tasks/jsdoc/generateApiIndex");
 });
 
 test("generateApiIndex with missing parameters", async (t) => {
-	const {generateApiIndex} = t.context;
-	await t.throwsAsync(generateApiIndex(), {
-		instanceOf: TypeError
-	}, "TypeError thrown");
+	const error = await t.throws(generateApiIndex());
+	t.deepEqual(error.message, "[generateApiIndex]: One or more mandatory options not provided",
+		"Correct error message thrown");
 });
