2129ba2f40d994f759f7dcaac90d5df5d373ceed
Frank Weigel <frank.weigel@sap.com>
2019-03-21 15:59:02 +0100

    [builder][FEATURE] Add JSDoc build functionalities (https://github.com/SAP/ui5-builder/pull/42)
    New tasks:
    - generateJsdoc
    - executeJsdocSdkTransformation
    - generateApiIndex
    
    New processors:
    - jsdocGenerator
    - sdkTransformer
    - apiIndexGenerator
    
    Can be tested by executing `npm run build-sdk` in the OpenUI5 testsuite project

--- packages/builder/lib/processors/jsdoc/jsdocGenerator.js
+++ packages/builder/lib/processors/jsdoc/jsdocGenerator.js
@@ -1,52 +1,42 @@
-import {spawn} from "node:child_process";
-import fs from "graceful-fs";
-import path from "node:path";
-import {promisify} from "node:util";
+const spawn = require("child_process").spawn;
+const fs = require("graceful-fs");
+const path = require("path");
+const {promisify} = require("util");
 const writeFile = promisify(fs.writeFile);
-import {createAdapter} from "@ui5/fs/resourceFactory";
-import {fileURLToPath} from "node:url";
-
-/**
- * @public
- * @module @ui5/builder/processors/jsdoc/jsdocGenerator
- */
+const {resourceFactory} = require("@ui5/fs");
 
 /**
  * JSDoc generator
  *
  * @public
- * @function default
- * @static
- *
- * @param {object} parameters Parameters
+ * @alias module:@ui5/builder.processors.jsdocGenerator
+ * @param {Object} parameters Parameters
  * @param {string} parameters.sourcePath Path of the source files to be processed
  * @param {string} parameters.targetPath Path to write any output files
  * @param {string} parameters.tmpPath Path to write temporary and debug files
- * @param {object} parameters.options Options
+ * @param {Object} parameters.options Options
  * @param {string} parameters.options.projectName Project name
  * @param {string} parameters.options.namespace Namespace to build (e.g. <code>some/project/name</code>)
  * @param {string} parameters.options.version Project version
  * @param {Array} [parameters.options.variants=["apijson"]] JSDoc variants to be built
- * @returns {Promise<@ui5/fs/Resource[]>} Promise resolving with newly created resources
+ * @returns {Promise<module:@ui5/fs.Resource[]>} Promise resolving with newly created resources
  */
-export default async function jsdocGenerator(
-	{sourcePath, targetPath, tmpPath, options: {projectName, namespace, version, variants}} = {}
-) {
-	if (!sourcePath || !targetPath || !tmpPath || !projectName || !namespace || !version) {
+const jsdocGenerator = async function({sourcePath, targetPath, tmpPath, options} = {}) {
+	if (!sourcePath || !targetPath || !tmpPath || !options.projectName || !options.namespace || !options.version) {
 		throw new Error("[jsdocGenerator]: One or more mandatory parameters not provided");
 	}
 
-	if (!variants || variants.length === 0) {
-		variants = ["apijson"];
+	if (!options.variants || options.variants.length === 0) {
+		options.variants = ["apijson"];
 	}
 
 	const config = await jsdocGenerator._generateJsdocConfig({
 		targetPath,
 		tmpPath,
-		namespace,
-		projectName,
-		version,
-		variants
+		namespace: options.namespace,
+		projectName: options.projectName,
+		version: options.version,
+		variants: options.variants
 	});
 
 	const configPath = await jsdocGenerator._writeJsdocConfig(tmpPath, config);
@@ -56,29 +46,28 @@ export default async function jsdocGenerator(
 		configPath
 	});
 
-	const fsTarget = createAdapter({
+	const fsTarget = resourceFactory.createAdapter({
 		fsBasePath: targetPath,
 		virBasePath: "/"
 	});
 
 	// create resources from the output files
 	return Promise.all([
-		fsTarget.byPath(`/test-resources/${namespace}/designtime/api.json`)
+		fsTarget.byPath(`/test-resources/${options.namespace}/designtime/api.json`)
 		// fsTarget.byPath(`/libraries/${options.projectName}.js`)
 	]).then((res) => res.filter(($)=>$));
-}
+};
 
 
 /**
  * Generate jsdoc-config.json content
  *
  * @private
- * @param {object} parameters Parameters
+ * @param {Object} parameters Parameters
  * @param {string} parameters.targetPath Path to write any output files
  * @param {string} parameters.tmpPath Path to write temporary and debug files
  * @param {string} parameters.projectName Project name
  * @param {string} parameters.version Project version
- * @param {string} parameters.namespace Namespace to use (e.g. <code>some/project/name</code>)
  * @param {Array} parameters.variants JSDoc variants to be built
  * @returns {string} jsdoc-config.json content string
  */
@@ -89,7 +78,7 @@ async function generateJsdocConfig({targetPath, tmpPath, namespace, projectName,
 	const backslashRegex = /\\/g;
 
 	// Resolve path to this script to get the path to the JSDoc extensions folder
-	const jsdocPath = path.normalize(import.meta.dirname);
+	const jsdocPath = path.normalize(__dirname);
 	const pluginPath = path.join(jsdocPath, "lib", "ui5", "plugin.js").replace(backslashRegex, "\\\\");
 	const templatePath = path.join(jsdocPath, "lib", "ui5", "template").replace(backslashRegex, "\\\\");
 	const destinationPath = path.normalize(tmpPath).replace(backslashRegex, "\\\\");
@@ -98,13 +87,6 @@ async function generateJsdocConfig({targetPath, tmpPath, namespace, projectName,
 	const apiJsonFilePath =
 		path.join(targetPath, "test-resources", path.normalize(namespace), "designtime", "api.json")
 			.replace(backslashRegex, "\\\\");
-
-	// Note: While the projectName could also be used here, it is not ensured that it fits to
-	// the library namespace.
-	// As the "uilib" information is used to check for certain constraints it must be aligned with
-	// the technical namespace that is used in the folder-structure, library.js and for the
-	// sap.ui.base.Object based classes.
-	const uilib = namespace.replace(/\//g, ".");
 
 	const config = `{
 		"plugins": ["${pluginPath}"],
@@ -121,7 +103,6 @@ async function generateJsdocConfig({targetPath, tmpPath, namespace, projectName,
 			"ui5": {
 				"variants": ${JSON.stringify(variants)},
 				"version": "${version}",
-				"uilib": "${uilib}",
 				"jsapiFile": "${jsapiFilePath}",
 				"apiJsonFolder": "${apiJsonFolderPath}",
 				"apiJsonFile": "${apiJsonFilePath}"
@@ -150,31 +131,34 @@ async function writeJsdocConfig(targetDirPath, config) {
  * Execute JSDoc build by spawning JSDoc as an external process
  *
  * @private
- * @param {object} parameters Parameters
+ * @param {Object} parameters Parameters
  * @param {string} parameters.sourcePath Project resources (input for JSDoc generation)
  * @param {string} parameters.configPath Full path to jsdoc-config.json file
  * @returns {Promise<undefined>}
  */
 async function buildJsdoc({sourcePath, configPath}) {
 	const args = [
-		fileURLToPath(import.meta.resolve("jsdoc/jsdoc.js")),
+		require.resolve("jsdoc/jsdoc"),
 		"-c",
 		configPath,
 		"--verbose",
 		sourcePath
 	];
-	const exitCode = await new Promise((resolve /* , reject */) => {
+	return new Promise((resolve, reject) => {
 		const child = spawn("node", args, {
-			stdio: ["ignore", "ignore", "inherit"]
+			stdio: ["ignore", "ignore", process.stderr]
 		});
-		child.on("close", resolve);
+		child.on("close", function(code) {
+			if (code === 0 || code === 1) {
+				resolve();
+			} else {
+				reject(new Error(`JSDoc child process closed with code ${code}`));
+			}
+		});
 	});
-
-	if (exitCode !== 0) {
-		throw new Error(`JSDoc reported an error, check the log for issues (exit code: ${exitCode})`);
-	}
 }
 
-jsdocGenerator._generateJsdocConfig = generateJsdocConfig;
-jsdocGenerator._writeJsdocConfig = writeJsdocConfig;
-jsdocGenerator._buildJsdoc = buildJsdoc;
+module.exports = jsdocGenerator;
+module.exports._generateJsdocConfig = generateJsdocConfig;
+module.exports._writeJsdocConfig = writeJsdocConfig;
+module.exports._buildJsdoc = buildJsdoc;
