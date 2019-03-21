c7f48d70b05b1bc71f065dfda2906be5414f64ab
Frank Weigel <frank.weigel@sap.com>
2019-03-21 15:59:02 +0100

    [builder][FEATURE] Add JSDoc build functionalities (https://github.com/SAP/ui5-builder/pull/42)
    New tasks:
    - generateJsdoc
    - executeJsdocSdkTransformation
    - generateApiIndex
    
    New processors:
    - jsdocGenerator
    - sdkTransformer
    - apiIndexGenerator
    
    Can be tested by executing `npm run build-sdk` in the OpenUI5 testsuite project

--- packages/builder/lib/processors/jsdoc/sdkTransformer.js
+++ packages/builder/lib/processors/jsdoc/sdkTransformer.js
@@ -1,30 +1,23 @@
-import {createResource} from "@ui5/fs/resourceFactory";
-import transformer from "./lib/transformApiJson.js";
+const resourceFactory = require("@ui5/fs").resourceFactory;
+const transformer = require("./lib/transform-apijson-for-sdk");
 
 /**
- * @public
- * @module @ui5/builder/processors/jsdoc/sdkTransformer
- */
-
-/**
- * Transform api.json as created by [jsdocGenerator]{@link @ui5/builder/processors/jsdoc/jsdocGenerator}
+ * Transform api.json as created by [jsdocGenerator]{@link module:@ui5/builder.processors.jsdocGenerator}
  * for usage in a UI5 SDK
  *
  * @public
- * @function default
- * @static
- *
- * @param {object} parameters Parameters
+ * @alias module:@ui5/builder.processors.sdkTransformer
+ * @param {Object} parameters Parameters
  * @param {string} parameters.apiJsonPath Path to the projects api.json file as created by
- *				[jsdocGenerator]{@link @ui5/builder/processors/jsdoc/jsdocGenerator}
+ *				[jsdocGenerator]{@link module:@ui5/builder.processors.jsdoc.jsdocGenerator}
  * @param {string} parameters.dotLibraryPath Path to the projects .library file
  * @param {string[]} parameters.dependencyApiJsonPaths List of paths to the api.json files of all dependencies of
- *				the project as created by [jsdocGenerator]{@link @ui5/builder/processors/jsdoc/jsdocGenerator}
+ *				the project as created by [jsdocGenerator]{@link module:@ui5/builder.processors.jsdoc.jsdocGenerator}
  * @param {string} parameters.targetApiJsonPath Path to create the new, transformed api.json resource for
- * @param {fs|module:@ui5/fs/fsInterface} parameters.fs Node fs or
- * 				custom [fs interface]{@link module:@ui5/fs/fsInterface} to use
+ * @param {fs|module:@ui5/fs.fsInterface} parameters.fs Node fs or
+ * 				custom [fs interface]{@link module:resources/module:@ui5/fs.fsInterface} to use
  *
- * @returns {Promise<@ui5/fs/Resource[]>} Promise resolving with created resources
+ * @returns {Promise<module:@ui5/fs.Resource[]>} Promise resolving with created resources
  */
 const sdkTransformer = async function({
 	apiJsonPath, dotLibraryPath, dependencyApiJsonPaths, targetApiJsonPath, fs} = {}
@@ -33,14 +26,14 @@ const sdkTransformer = async function({
 		throw new Error("[sdkTransformer]: One or more mandatory parameters not provided");
 	}
 	const fakeTargetPath = "/ignore/this/path/resource/will/be/returned";
-	const apiJsonContent = await transformer(apiJsonPath, fakeTargetPath, dotLibraryPath, dependencyApiJsonPaths, "", {
+	const apiJsonContent = await transformer(apiJsonPath, fakeTargetPath, dotLibraryPath, dependencyApiJsonPaths, {
 		fs,
 		returnOutputFiles: true
 	});
-	return [createResource({
+	return [resourceFactory.createResource({
 		path: targetApiJsonPath,
 		string: apiJsonContent
 	})];
 };
 
-export default sdkTransformer;
+module.exports = sdkTransformer;
