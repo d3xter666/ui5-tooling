4b6c6ae719573da810b661f95a97f77ded281ecf
Peter Muessig <peter.muessig@sap.com>
2019-04-08 15:31:52 +0200

    [builder][FEATURE] Generate the AppCacheBuster index file for apps

--- packages/builder/test/lib/tasks/generateCachebusterInfo.js
+++ packages/builder/test/lib/tasks/generateCachebusterInfo.js
@@ -1,89 +1,60 @@
-import test from "ava";
-import path from "node:path";
-import {directoryDeepEqual, fileEqual, findFiles, readFileContent} from "../../utils/fshelper.js";
+const {test} = require("ava");
+const fs = require("fs");
+const path = require("path");
+const chai = require("chai");
+chai.use(require("chai-fs"));
+const assert = chai.assert;
 
-import {graphFromObject} from "@ui5/project/graph";
-import * as taskRepository from "../../../lib/tasks/taskRepository.js";
 
-const __dirname = import.meta.dirname;
-
+const ui5Builder = require("../../../");
+const builder = ui5Builder.builder;
 const applicationGPath = path.join(__dirname, "..", "..", "fixtures", "application.g");
 
-test("integration: Build application.g", async (t) => {
+const recursive = require("recursive-readdir");
+
+const findFiles = (folder) => {
+	return new Promise((resolve, reject) => {
+		recursive(folder, (err, files) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(files);
+			}
+		});
+	});
+};
+
+test("integration: Build application.g with manifestBundler", (t) => {
 	const destPath = path.join("test", "tmp", "build", "application.g", "cachebuster");
 	const expectedPath = path.join("test", "expected", "build", "application.g", "cachebuster");
-	const excludedTasks = ["escapeNonAsciiCharacters", "generateVersionInfo"];
+	const excludedTasks = ["generateVersionInfo"];
 	const includedTasks = ["generateCachebusterInfo"];
 
-	const cleanupCacheBusterInfo = (fileContent) => fileContent.replace(/(:\s+)(\d+)/g, ": 0");
-
-	const graph = await graphFromObject({
-		dependencyTree: applicationGTree
-	});
-
-	graph.setTaskRepository(taskRepository);
-	await graph.build({
+	return builder.build({
+		tree: applicationGTree,
 		destPath,
 		excludedTasks,
 		includedTasks
+	}).then(() => {
+		return findFiles(expectedPath);
+	}).then((expectedFiles) => {
+		// Check for all directories and files
+		assert.directoryDeepEqual(destPath, expectedPath);
+
+		// Check for all file contents
+		expectedFiles.forEach((expectedFile) => {
+			const relativeFile = path.relative(expectedPath, expectedFile);
+			const destFile = path.join(destPath, relativeFile);
+			if (expectedFile.endsWith("sap-ui-cachebuster-info.json")) {
+				const currentContent = JSON.parse(fs.readFileSync(destFile, "utf-8").replace(/(:\s+)(\d+)/g, ": 0"));
+				const expectedContent = JSON.parse(fs.readFileSync(expectedFile, "utf-8").replace(/(:\s+)(\d+)/g, ": 0"));
+				assert.deepEqual(currentContent, expectedContent);
+			} else {
+				assert.fileEqual(destFile, expectedFile);
+			}
+		});
+		t.pass();
 	});
-	const expectedFiles = await findFiles(expectedPath);
-
-	// Check for all directories and files
-	await directoryDeepEqual(t, destPath, expectedPath);
-
-	// Check for all file contents
-	await Promise.all(expectedFiles.map(async (expectedFile) => {
-		const relativeFile = path.relative(expectedPath, expectedFile);
-		const destFile = path.join(destPath, relativeFile);
-		if (expectedFile.endsWith("sap-ui-cachebuster-info.json")) {
-			const destContent = JSON.parse(cleanupCacheBusterInfo(await readFileContent(destFile)));
-			const expectedContent = JSON.parse(cleanupCacheBusterInfo(await readFileContent(expectedFile)));
-			t.deepEqual(destContent, expectedContent);
-		} else {
-			await fileEqual(t, destFile, expectedFile);
-		}
-	}));
-	t.pass();
-});
-
-test("integration: Build application.g with cachebuster using hashes", async (t) => {
-	const destPath = path.join("test", "tmp", "build", "application.g", "cachebuster_hash");
-	const expectedPath = path.join("test", "expected", "build", "application.g", "cachebuster");
-	const excludedTasks = ["escapeNonAsciiCharacters", "generateVersionInfo"];
-	const includedTasks = ["generateCachebusterInfo"];
-
-	const cleanupCacheBusterInfo = (fileContent) => fileContent.replace(/(:\s+)("[^"]+")/g, ": \"\"");
-
-	const graph = await graphFromObject({
-		dependencyTree: applicationGTreeWithCachebusterHash
-	});
-
-	graph.setTaskRepository(taskRepository);
-	await graph.build({
-		destPath,
-		excludedTasks,
-		includedTasks
-	});
-
-	const expectedFiles = await findFiles(expectedPath);
-
-	// Check for all directories and files
-	await directoryDeepEqual(t, destPath, expectedPath);
-
-	// Check for all file contents
-	await Promise.all(expectedFiles.map(async (expectedFile) => {
-		const relativeFile = path.relative(expectedPath, expectedFile);
-		const destFile = path.join(destPath, relativeFile);
-		if (expectedFile.endsWith("sap-ui-cachebuster-info.json")) {
-			const destContent = JSON.parse(cleanupCacheBusterInfo(await readFileContent(destFile)));
-			const expectedContent = JSON.parse(cleanupCacheBusterInfo(await readFileContent(destFile)));
-			t.deepEqual(destContent, expectedContent);
-		} else {
-			await fileEqual(t, destFile, expectedFile);
-		}
-	}));
-	t.pass();
 });
 
 const applicationGTree = {
@@ -91,47 +62,23 @@ const applicationGTree = {
 	"version": "1.0.0",
 	"path": applicationGPath,
 	"dependencies": [],
-	"configuration": {
-		"builder": {},
-		"specVersion": "2.0",
-		"type": "application",
-		"metadata": {
-			"name": "application.g",
-			"copyright": "Some fancy copyright"
-		},
-		"resources": {
-			"configuration": {
-				"paths": {
-					"webapp": "webapp"
-				}
-			}
-		}
-	}
-};
-
-const applicationGTreeWithCachebusterHash = {
-	"id": "application.g",
-	"version": "1.0.0",
-	"path": applicationGPath,
-	"dependencies": [],
-	"configuration": {
-		"builder": {
-			"cachebuster": {
-				"signatureType": "hash"
+	"builder": {},
+	"_level": 0,
+	"specVersion": "0.1",
+	"type": "application",
+	"metadata": {
+		"name": "application.g",
+		"namespace": "application.g",
+		"copyright": "Some fancy copyright"
+	},
+	"resources": {
+		"configuration": {
+			"paths": {
+				"webapp": "webapp"
 			}
 		},
-		"specVersion": "2.0",
-		"type": "application",
-		"metadata": {
-			"name": "application.g",
-			"copyright": "Some fancy copyright"
-		},
-		"resources": {
-			"configuration": {
-				"paths": {
-					"webapp": "webapp"
-				}
-			}
+		"pathMappings": {
+			"/": "webapp"
 		}
 	}
 };
