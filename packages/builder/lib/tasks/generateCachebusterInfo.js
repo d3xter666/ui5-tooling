ed95a34d251542b51ccf7bec8d97b3ffe86461d5
Peter Muessig <peter.muessig@sap.com>
2019-04-08 15:31:52 +0200

    [builder][FEATURE] Generate the AppCacheBuster index file for apps

--- packages/builder/lib/tasks/generateCachebusterInfo.js
+++ packages/builder/lib/tasks/generateCachebusterInfo.js
@@ -1,79 +1,30 @@
-import crypto from "node:crypto";
-import {createResource} from "@ui5/fs/resourceFactory";
-import {getLogger} from "@ui5/logger";
-const log = getLogger("builder:tasks:generateCachebusterInfo");
+const resourceFactory = require("@ui5/fs").resourceFactory;
 
-/**
- * @public
- * @module @ui5/builder/tasks/generateCachebusterInfo
- */
-
-async function signByTime(resource) {
-	return resource.getStatInfo().mtime.getTime();
-}
-
-async function signByHash(resource) {
-	const hasher = crypto.createHash("sha1");
-	const buffer = await resource.getBuffer();
-
-	hasher.update(buffer.toString("binary"));
-	return hasher.digest("hex");
-}
-
-function getSigner(type) {
-	type = type || "time";
-
-	switch (type) {
-	case "time":
-		return signByTime;
-	case "hash":
-		return signByHash;
-
-	default:
-		throw new Error(`Invalid signature type: '${type}'. Valid ones are: 'time' or 'hash'`);
-	}
-}
-
-/* eslint "jsdoc/check-param-names": ["error", {"disableExtraPropertyReporting":true}] */
 /**
  * Task to generate the application cachebuster info file.
  *
  * @public
- * @function default
- * @static
- *
- * @param {object} parameters Parameters
- * @param {@ui5/fs/DuplexCollection} parameters.workspace DuplexCollection to read and write files
- * @param {object} parameters.options Options
- * @param {string} parameters.options.projectNamespace Namespace of the application
- * @param {string} [parameters.options.signatureType='time'] Type of signature to be used ('time' or 'hash')
+ * @alias module:@ui5/builder.tasks.generateCachebusterInfo
+ * @param {Object} parameters Parameters
+ * @param {module:@ui5/fs.DuplexCollection} parameters.workspace DuplexCollection to read and write files
+ * @param {module:@ui5/fs.AbstractReader} parameters.dependencies Reader or Collection to read dependency files
+ * @param {Object} parameters.options Options
+ * @param {string} parameters.options.namespace Namespace of the application
  * @returns {Promise<undefined>} Promise resolving with <code>undefined</code> once data has been written
  */
-export default function({workspace, options}) {
-	const {signatureType} = options;
-	const namespace = options.projectNamespace;
-
-	const basePath = `/resources/${namespace}/`;
-	return workspace.byGlob(`/resources/${namespace}/**/*`)
+module.exports = function({workspace, dependencies, options}) {
+	return workspace.byGlob(`/resources/${options.namespace}/**/*`)
 		.then(async (resources) => {
-			const cachebusterInfo = Object.create(null);
-			const signer = getSigner(signatureType);
-
-			await Promise.all(resources.map(async (resource) => {
-				let resourcePath = resource.getPath();
-				if (!resourcePath.startsWith(basePath)) {
-					log.verbose(
-						`Ignoring resource with path ${resourcePath} since it is not based on path ${basePath}`);
-					return;
-				}
-				// Remove base path. Absolute paths are not allowed in cachebuster info
-				resourcePath = resourcePath.replace(basePath, "");
-				cachebusterInfo[resourcePath] = await signer(resource);
-			}));
-			const cachebusterInfoResource = createResource({
-				path: `/resources/${namespace}/sap-ui-cachebuster-info.json`,
+			const cachebusterInfo = {};
+			const regex = new RegExp(`^/resources/${options.namespace}/`);
+			resources.forEach((resource) => {
+				const normalizedPath = resource.getPath().replace(regex, "");
+				cachebusterInfo[normalizedPath] = resource.getStatInfo().mtime.getTime();
+			});
+			const cachebusterInfoResource = resourceFactory.createResource({
+				path: `/resources/${options.namespace}/sap-ui-cachebuster-info.json`,
 				string: JSON.stringify(cachebusterInfo, null, 2)
 			});
 			return workspace.write(cachebusterInfoResource);
 		});
-}
+};
