5876b499ea146921bd82a60f58f922e500ca82fa
Merlin Beutlberger <m.beutlberger@sap.com>
2019-07-10 14:42:12 +0200

    [server][FEATURE] Server: Add handling for custom middleware (https://github.com/SAP/ui5-server/pull/200)
    As per RFC 0005: https://github.com/SAP/ui5-tooling/pull/151

--- packages/server/lib/middleware/middlewareRepository.js
+++ packages/server/lib/middleware/middlewareRepository.js
@@ -1,54 +1,33 @@
-const middlewareInfos = {
-	compression: {path: "compression"},
-	cors: {path: "cors"},
-	csp: {path: "./csp.js"},
-	serveResources: {path: "./serveResources.js"},
-	serveIndex: {path: "./serveIndex.js"},
-	discovery: {path: "./discovery.js"},
-	versionInfo: {path: "./versionInfo.js"},
-	serveThemes: {path: "./serveThemes.js"},
-	testRunner: {path: "./testRunner.js"},
-	nonReadRequests: {path: "./nonReadRequests.js"}
+const middlewares = {
+	compression: "compression",
+	cors: "cors",
+	csp: "./csp",
+	serveResources: "./serveResources",
+	serveIndex: "./serveIndex",
+	discovery: "./discovery",
+	versionInfo: "./versionInfo",
+	connectUi5Proxy: "./connectUi5Proxy",
+	serveThemes: "./serveThemes",
+	nonReadRequests: "./nonReadRequests"
 };
 
-// see  @ui5/server/internal/middlewareRepository#getMiddleware
-async function getMiddleware(middlewareName) {
-	const middlewareInfo = middlewareInfos[middlewareName];
+function getMiddleware(middlewareName) {
+	const middlewarePath = middlewares[middlewareName];
 
-	if (!middlewareInfo) {
+	if (!middlewarePath) {
 		throw new Error(`middlewareRepository: Unknown Middleware ${middlewareName}`);
 	}
-	try {
-		const {default: middleware} = await import(middlewareInfo.path);
-		return {
-			middleware
-		};
-	} catch (err) {
-		throw new Error(
-			`middlewareRepository: Failed to require middleware module for ${middlewareName}:\n${err.stack}`);
-	}
+	return require(middlewarePath);
 }
-/**
- * @private
- * @typedef {object} module:@ui5/server/internal/middlewareRepository~Middleware
- * @property {object} middleware The middleware
- */
 
-/**
- * @private
- * @module @ui5/server/internal/middlewareRepository
- * @borrows getMiddleware as getMiddleware
- */
-export default {
+function addMiddleware(name, middlewarePath) {
+	if (middlewares[name]) {
+		throw new Error(`middlewareRepository: Middleware ${name} already registered`);
+	}
+	middlewares[name] = middlewarePath;
+}
 
-	/**
-	 * Determines the desired middleware
-	 *
-	 * @private
-	 * @static
-	 * @function
-	 * @param {string} middlewareName The name of the middleware
-	 * @returns {module:@ui5/server/internal/middlewareRepository~Middleware} The middleware
-	 */
-	getMiddleware: getMiddleware
+module.exports = {
+	getMiddleware: getMiddleware,
+	addMiddleware: addMiddleware
 };
