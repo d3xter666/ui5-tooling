6e176661df96dabf200595ef9d8346dcbd326619
Merlin Beutlberger <m.beutlberger@sap.com>
2019-07-10 14:42:12 +0200

    [server][FEATURE] Server: Add handling for custom middleware (https://github.com/SAP/ui5-server/pull/200)
    As per RFC 0005: https://github.com/SAP/ui5-tooling/pull/151

--- packages/server/test/lib/server/middleware/middlewareRepository.js
+++ packages/server/test/lib/server/middleware/middlewareRepository.js
@@ -1,16 +1,32 @@
-import test from "ava";
-import middlewareRepository from "../../../../lib/middleware/middlewareRepository.js";
-import cspModule from "../../../../lib/middleware/csp.js";
+const test = require("ava");
+const middlewareRepository = require("../../../../lib/middleware/middlewareRepository");
 
 test("getMiddleware", async (t) => {
-	const res = await middlewareRepository.getMiddleware("csp");
-	t.deepEqual(res, {
-		middleware: cspModule
-	}, "Returned correct middleware module");
+	const cspModule = require("../../../../lib/middleware/csp");
+	const res = middlewareRepository.getMiddleware("csp");
+	t.is(res, cspModule, "Returned correct middleware module");
 });
 
-test("getMiddleware: Unknown middleware", async (t) => {
-	const err = await t.throwsAsync(middlewareRepository.getMiddleware("🐬"));
-	t.is(err.message, "middlewareRepository: Unknown Middleware 🐬",
+test("getMiddleware: Unkown middleware", async (t) => {
+	const err = t.throws(() => {
+		middlewareRepository.getMiddleware("🐬");
+	});
+	t.deepEqual(err.message, "middlewareRepository: Unknown Middleware 🐬",
+		"Threw error with correct message");
+});
+
+test("addMiddleware", async (t) => {
+	const cspModule = require("../../../../lib/middleware/csp");
+	middlewareRepository.addMiddleware("🐠", "./csp");
+	const res = middlewareRepository.getMiddleware("🐠");
+
+	t.is(res, cspModule, "Returned added middleware module");
+});
+
+test("addMiddleware: Duplicate middleware", async (t) => {
+	const err = t.throws(() => {
+		middlewareRepository.addMiddleware("cors");
+	});
+	t.deepEqual(err.message, "middlewareRepository: Middleware cors already registered",
 		"Threw error with correct message");
 });
