9ad676ba82802644f627c4b1fecfe4b5c124308f
Tobias Sorn <tobias.sorn@sap.com>
2019-07-29 16:39:43 +0200

    [builder][FEATURE] Properties File Escaping (https://github.com/SAP/ui5-builder/pull/293)
    Add processor nonAsciiEscaper which escapes non ASCII characters in resources.
    Add task escapeNonAsciiCharacters which uses nonAsciiEscaper to escape
    non ascii characters.

--- packages/builder/lib/lbt/utils/escapePropertiesFile.js
+++ packages/builder/lib/lbt/utils/escapePropertiesFile.js
@@ -1,29 +1,21 @@
-import nonAsciiEscaper from "../../processors/nonAsciiEscaper.js";
+const nonAsciiEscaper = require("../../processors/nonAsciiEscaper");
 
 /**
  * Can be used to escape *.properties files.
  *
  * Input encoding is read from project configuration.
- * In case the resource belongs to no project (e.g. bundler is used standalone) the default is "UTF-8".
+ * In case the resource belongs to no project (e.g. bundler is used standalone) the default is "ISO-8859-1".
  *
  * @private
  * @param {Resource} resource the resource for which the content will be escaped
  * @returns {Promise<string>} resolves with the escaped string content of the given Resource
  */
-export default async function(resource) {
-	const project = resource.getProject();
-	let propertiesFileSourceEncoding = project && project.getPropertiesFileSourceEncoding();
-
-	if (!propertiesFileSourceEncoding) {
-		if (project && project.getSpecVersion().lte("1.1")) {
-			// default encoding to "ISO-8859-1" for old specVersions
-			propertiesFileSourceEncoding = "ISO-8859-1";
-		} else {
-			// default encoding to "UTF-8" for all projects starting with specVersion 2.0
-			propertiesFileSourceEncoding = "UTF-8";
-		}
-	}
-	const encoding = nonAsciiEscaper.getEncodingFromAlias(propertiesFileSourceEncoding);
+module.exports = async function(resource) {
+	const propertiesFileSourceEncoding = resource.getProject()
+			&& resource.getProject().resources
+			&& resource.getProject().resources.configuration
+			&& resource.getProject().resources.configuration.propertiesFileSourceEncoding;
+	const encoding = nonAsciiEscaper.getEncodingFromAlias(propertiesFileSourceEncoding || "ISO-8859-1");
 	await nonAsciiEscaper({
 		resources: [resource.resource],
 		options: {
@@ -34,4 +26,4 @@ export default async function(resource) {
 	const fileContent = await resource.buffer();
 
 	return fileContent.toString();
-}
+};
