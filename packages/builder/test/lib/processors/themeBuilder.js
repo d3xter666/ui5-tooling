eab55dfd7289b81aab3dd59f840166855568c87c
Matthias Oßwald <1410947+matz3@users.noreply.github.com>
2019-08-01 13:53:55 +0200

    [builder][FEATURE] Theme Build: Add compress option to minify output (https://github.com/SAP/ui5-builder/pull/295)

--- packages/builder/test/lib/processors/themeBuilder.js
+++ packages/builder/test/lib/processors/themeBuilder.js
@@ -1,11 +1,12 @@
-import test from "ava";
-import {createAdapter, createResource} from "@ui5/fs/resourceFactory";
-import fsInterface from "@ui5/fs/fsInterface";
+const test = require("ava");
 
-import themeBuilderProcessor from "../../../lib/processors/themeBuilder.js";
-import {ThemeBuilder} from "../../../lib/processors/themeBuilder.js";
+const resourceFactory = require("@ui5/fs").resourceFactory;
+const fsInterface = require("@ui5/fs").fsInterface;
 
-async function prepareResources({library} = {}) {
+const themeBuilderProcessor = require("../../../lib/processors/themeBuilder");
+const ThemeBuilder = require("../../../lib/processors/themeBuilder").ThemeBuilder;
+
+function prepareResources({library} = {}) {
 	const input =
 `@someColor: black;
 .someClass {
@@ -13,7 +14,7 @@ async function prepareResources({library} = {}) {
 	padding: 1px 2px 3px 4px;
 }`;
 
-	const memoryAdapter = createAdapter({
+	const memoryAdapter = resourceFactory.createAdapter({
 		virBasePath: "/"
 	});
 
@@ -24,12 +25,12 @@ async function prepareResources({library} = {}) {
 		lessFilePath = "/resources/sap/ui/foo/themes/base/library.source.less";
 	}
 
-	const resource = createResource({
+	const resource = resourceFactory.createResource({
 		path: lessFilePath,
 		string: input
 	});
 
-	await memoryAdapter.write(resource);
+	memoryAdapter.write(resource);
 
 	return {
 		resource,
@@ -37,86 +38,54 @@ async function prepareResources({library} = {}) {
 	};
 }
 
-function getExpectedResults({compress, library, cssVariables}) {
-	const result = {};
+function getExpectedResults({compress, library}) {
+	let css; let cssRtl; let json;
 	if (compress) {
-		result.css =
+		css =
 `.someClass{color:#000;padding:1px 2px 3px 4px}`;
 
-		result.cssRtl =
+		cssRtl =
 `.someClass{color:#000;padding:1px 4px 3px 2px}`;
-		result.json = `{"someColor":"#000"}`;
+		json = `{"someColor":"#000"}`;
 	} else {
-		result.css =
+		css =
 `.someClass {
   color: #000000;
   padding: 1px 2px 3px 4px;
 }
 `;
 
-		result.cssRtl =
+		cssRtl =
 `.someClass {
   color: #000000;
   padding: 1px 4px 3px 2px;
 }
 `;
 
-		result.json =
+		json =
 `{
 	"someColor": "#000000"
 }`;
 	}
 
 	if (library !== false) {
-		result.css +=
+		css +=
 `
 /* Inline theming parameters */
-#sap-ui-theme-sap\\.ui\\.foo{background-image:url('data:text/plain;utf-8,%7B%22someColor%22%3A%22%23` +
-`${compress ? "000" : "000000"}%22%7D')}
+#sap-ui-theme-sap\\.ui\\.foo{background-image:url('data:text/plain;utf-8,%7B%22someColor%22%3A%22%23${compress ? "000" : "000000"}%22%7D')}
 `;
-		result.cssRtl +=
+		cssRtl +=
 `
 /* Inline theming parameters */
-#sap-ui-theme-sap\\.ui\\.foo{background-image:url('data:text/plain;utf-8,%7B%22someColor%22%3A%22%23` +
-`${compress ? "000" : "000000"}%22%7D')}
+#sap-ui-theme-sap\\.ui\\.foo{background-image:url('data:text/plain;utf-8,%7B%22someColor%22%3A%22%23${compress ? "000" : "000000"}%22%7D')}
 `;
 	}
 
-	if (cssVariables) {
-		result.cssVariablesSource =
-`@someColor: #000000;
-
-:root {
---someColor: @someColor;
-}
-`;
-		result.cssVariables =
-`:root {
-  --someColor: #000000;
-}
-
-/* Inline theming parameters */
-#sap-ui-theme-sap\\.ui\\.foo{background-image:url('data:text/plain;utf-8,%7B%22someColor%22%3A%22%23000000%22%7D')}
-`;
-		result.cssSkeleton =
-`.someClass {
-  color: var(--someColor);
-  padding: 1px 2px 3px 4px;
-}
-`;
-		result.cssSkeletonRtl =
-`.someClass {
-  color: var(--someColor);
-  padding: 1px 4px 3px 2px;
-}
-`;
-	}
-
-	return result;
+	return {css, cssRtl, json};
 }
 
 test("Processor: Builds a less file (default options)", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources();
+	const {resource, memoryAdapter} = prepareResources();
 
 	const [cssResource, cssRtlResource, jsonResource] = await themeBuilderProcessor({
 		resources: [resource],
@@ -127,16 +96,10 @@ test("Processor: Builds a less file (default options)", async (t) => {
 	t.is(await cssResource.getString(), expected.css, "CSS should be correct");
 	t.is(await cssRtlResource.getString(), expected.cssRtl, "Right-to-left CSS should be correct");
 	t.is(await jsonResource.getString(), expected.json, "JSON should be correct");
-
-	t.is(cssResource.getPath(), "/resources/sap/ui/foo/themes/base/library.css", "CSS resource path should be correct");
-	t.is(cssRtlResource.getPath(), "/resources/sap/ui/foo/themes/base/library-RTL.css",
-		"Right-to-left CSS resource path should be correct");
-	t.is(jsonResource.getPath(), "/resources/sap/ui/foo/themes/base/library-parameters.json",
-		"JSON resource path should be correct");
 });
 
 test("Processor: Builds a less file (compress = true)", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources();
+	const {resource, memoryAdapter} = prepareResources();
 
 	const [cssResource, cssRtlResource, jsonResource] = await themeBuilderProcessor({
 		resources: [resource],
@@ -153,7 +116,7 @@ test("Processor: Builds a less file (compress = true)", async (t) => {
 });
 
 test("Processor: Builds a less file (compress = false)", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources();
+	const {resource, memoryAdapter} = prepareResources();
 
 	const [cssResource, cssRtlResource, jsonResource] = await themeBuilderProcessor({
 		resources: [resource],
@@ -170,7 +133,7 @@ test("Processor: Builds a less file (compress = false)", async (t) => {
 });
 
 test("Processor: Builds a less file (no library)", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources({library: false});
+	const {resource, memoryAdapter} = prepareResources({library: false});
 
 	const [cssResource, cssRtlResource, jsonResource] = await themeBuilderProcessor({
 		resources: [resource],
@@ -187,7 +150,7 @@ test("Processor: Builds a less file (no library)", async (t) => {
 });
 
 test("ThemeBuilder: Builds a less file", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources();
+	const {resource, memoryAdapter} = prepareResources();
 
 	const themeBuilder = new ThemeBuilder({fs: fsInterface(memoryAdapter)});
 
@@ -200,7 +163,7 @@ test("ThemeBuilder: Builds a less file", async (t) => {
 });
 
 test("ThemeBuilder: Builds a less file (compress = true)", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources();
+	const {resource, memoryAdapter} = prepareResources();
 
 	const themeBuilder = new ThemeBuilder({fs: fsInterface(memoryAdapter)});
 
@@ -215,7 +178,7 @@ test("ThemeBuilder: Builds a less file (compress = true)", async (t) => {
 });
 
 test("ThemeBuilder: Builds a less file (compress = false)", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources();
+	const {resource, memoryAdapter} = prepareResources();
 
 	const themeBuilder = new ThemeBuilder({fs: fsInterface(memoryAdapter)});
 
@@ -230,7 +193,7 @@ test("ThemeBuilder: Builds a less file (compress = false)", async (t) => {
 });
 
 test("ThemeBuilder: Builds a less file (no library)", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources({library: false});
+	const {resource, memoryAdapter} = prepareResources({library: false});
 
 	const themeBuilder = new ThemeBuilder({fs: fsInterface(memoryAdapter)});
 
@@ -242,52 +205,4 @@ test("ThemeBuilder: Builds a less file (no library)", async (t) => {
 	t.is(await cssResource.getString(), expected.css, "CSS should be correct");
 	t.is(await cssRtlResource.getString(), expected.cssRtl, "Right-to-left CSS should be correct");
 	t.is(await jsonResource.getString(), expected.json, "JSON should be correct");
-});
-
-test("Processor: Builds a less file (cssVariables = true)", async (t) => {
-	const {resource, memoryAdapter} = await prepareResources();
-
-	const [
-		cssResource,
-		cssRtlResource,
-		jsonResource,
-		cssVariablesSourceResource,
-		cssVariablesResource,
-		cssSkeletonResource,
-		cssSkeletonRtlResource
-	] = await themeBuilderProcessor({
-		resources: [resource],
-		fs: fsInterface(memoryAdapter),
-		options: {
-			cssVariables: true
-		}
-	});
-
-	const expected = getExpectedResults({cssVariables: true});
-	t.is(await cssResource.getString(), expected.css, "CSS should be correct");
-	t.is(await cssRtlResource.getString(), expected.cssRtl, "Right-to-left CSS should be correct");
-	t.is(await jsonResource.getString(), expected.json, "JSON should be correct");
-	t.is(await cssVariablesSourceResource.getString(), expected.cssVariablesSource,
-		"CSS Variables source should be correct");
-	t.is(await cssVariablesResource.getString(), expected.cssVariables,
-		"CSS Variables should be correct");
-	t.is(await cssSkeletonResource.getString(), expected.cssSkeleton,
-		"Skeleton CSS should be correct");
-	t.is(await cssSkeletonRtlResource.getString(), expected.cssSkeletonRtl,
-		"Right-to-left skeleton CSS should be correct");
-
-	t.is(cssResource.getPath(), "/resources/sap/ui/foo/themes/base/library.css",
-		"CSS resource path should be correct");
-	t.is(cssRtlResource.getPath(), "/resources/sap/ui/foo/themes/base/library-RTL.css",
-		"Right-to-left CSS resource path should be correct");
-	t.is(jsonResource.getPath(), "/resources/sap/ui/foo/themes/base/library-parameters.json",
-		"JSON resource path should be correct");
-	t.is(cssVariablesSourceResource.getPath(), "/resources/sap/ui/foo/themes/base/css_variables.source.less",
-		"CSS Variables source path should be correct");
-	t.is(cssVariablesResource.getPath(), "/resources/sap/ui/foo/themes/base/css_variables.css",
-		"CSS Variables resource path should be correct");
-	t.is(cssSkeletonResource.getPath(), "/resources/sap/ui/foo/themes/base/library_skeleton.css",
-		"Skeleton CSS resource path should be correct");
-	t.is(cssSkeletonRtlResource.getPath(), "/resources/sap/ui/foo/themes/base/library_skeleton-RTL.css",
-		"Right-to-left skeleton CSS resource path should be correct");
 });
