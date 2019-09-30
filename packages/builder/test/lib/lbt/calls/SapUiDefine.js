c298b6d6e6718398c7d32c1d3d8f3bc80dba7ff4
Frank Weigel <frank.weigel@sap.com>
2019-09-30 18:11:54 +0200

    [builder][FIX] Improve recognition of main module in case of bundles (https://github.com/SAP/ui5-builder/pull/341)
    This is a port of several changes from the Java bundle tooling, which have been made after the initial migration of the tooling. Additionally, code coverage has been slightly improved by adding further unit and integration tests. Errors that have been revealed by the new tests have been fixed, too.
    
    Details about the migrated changes:
    
    ----
    Change 3312369 (improve recognition of main module in case of bundles):
    
    So far, the module analyzer always assumed the first module definition to be the main definition. This failed at least for CVOM bundles but might also fail for others.
    
    The analysis has been improved and now accepts a module as main module only in the following cases:
    - if it is an unnamed module
    - if it is a named module whose name matches the external name
    - if it is a named module and there's only one module (no bundle)
    
    If multiple module definitions match the above criteria, this is
    reported as an error.
    
    Additionally, AMD special dependencies ('require', 'exports', 'module') are no longer reported in the dependency info.
    
    ----
    Change 3945349 (JSModuleAnalyzer fails on modules that don't declare a name)
    
    Basically, the JavaScript implementation was less sensitive to this issue. But if a resource could have a dependency to another resource named "null", the JavaScript tooling would fail, too. Although this looked like a purely theoretical scenario, adding a protective 'if' was trivial enough to just do it. 
    
    ----
    Change 3562114 (fix dep. analysis of new evo bundles)
    
    - recognize and evaluate sap.ui.require.preload calls
    - write and extract bundle name and raw module names

--- packages/builder/test/lib/lbt/calls/SapUiDefine.js
+++ packages/builder/test/lib/lbt/calls/SapUiDefine.js
@@ -1,48 +1,16 @@
-import test from "ava";
-import {parseJS, Syntax} from "../../../../lib/lbt/utils/parseUtils.js";
-import SapUiDefineCall from "../../../../lib/lbt/calls/SapUiDefine.js";
-import sinonGlobal from "sinon";
-import esmock from "esmock";
+const test = require("ava");
+const esprima = require("esprima");
+const Syntax = esprima.Syntax;
+
+const SapUiDefineCall = require("../../../../lib/lbt/calls/SapUiDefine");
 
 function parse(code) {
-	const ast = parseJS(code);
+	const ast = esprima.parseScript(code);
 	return ast.body[0].expression;
 }
 
-async function setupSapUiDefineCallWithStubbedLogger({context}) {
-	const {sinon} = context;
-	context.warningLogSpy = sinon.spy();
-	context.SapUiDefineCallWithStubbedLogger = await esmock("../../../../lib/lbt/calls/SapUiDefine.js", {
-		"@ui5/logger": {
-			getLogger: {
-				warn: context.warningLogSpy
-			}
-		}
-	});
-}
-
-test.beforeEach((t) => {
-	t.context.sinon = sinonGlobal.createSandbox();
-});
-
-test.afterEach.always((t) => {
-	t.context.sinon.restore();
-});
-
-test("Empty Define", (t) => {
-	const ast = parse("sap.ui.define();");
-	const call = new SapUiDefineCall(ast, "FileSystemName");
-	t.true(call != null, "call could be parsed");
-});
-
 test("Named Define", (t) => {
 	const ast = parse("sap.ui.define('HardcodedName', [], function() {});");
-	const call = new SapUiDefineCall(ast, "FileSystemName");
-	t.is(call.name, "HardcodedName");
-});
-
-test("Named Define (template literal)", (t) => {
-	const ast = parse("sap.ui.define(`HardcodedName`, [], function() {});");
 	const call = new SapUiDefineCall(ast, "FileSystemName");
 	t.is(call.name, "HardcodedName");
 });
@@ -74,13 +42,6 @@ test("Factory", (t) => {
 	t.is(call.factory.type, Syntax.FunctionExpression);
 });
 
-test("No Factory", (t) => {
-	const ast = parse("sap.ui.define(['a', 'b']);");
-	const call = new SapUiDefineCall(ast, "FileSystemName");
-	t.deepEqual(call.dependencies, ["a.js", "b.js"]);
-	t.is(call.factory, null);
-});
-
 test("Find Import Name (successful)", (t) => {
 	const ast = parse("sap.ui.define(['wanted'], function(johndoe) {});");
 	const call = new SapUiDefineCall(ast, "FileSystemName");
@@ -98,63 +59,3 @@ test("Find Import Name (no dependencies)", (t) => {
 	const call = new SapUiDefineCall(ast, "FileSystemName");
 	t.is(call.findImportName("wanted.js"), null);
 });
-
-test("Find Import Name (template literal)", (t) => {
-	const ast = parse("sap.ui.define([`wanted`], function(johndoe) {});");
-	const call = new SapUiDefineCall(ast, "FileSystemName");
-	t.is(call.findImportName("wanted.js"), "johndoe");
-});
-
-test("Find Import Name (destructuring)", (t) => {
-	const ast = parse("sap.ui.define(['invalid', 'wanted', 'invalid1'], function({inv}, johndoe, [inv1]) {});");
-	const call = new SapUiDefineCall(ast, "FileSystemName");
-	t.is(call.findImportName("invalid.js"), null);
-	t.is(call.findImportName("wanted.js"), "johndoe");
-	t.is(call.findImportName("invalid1.js"), null);
-});
-
-test.serial("Find Import Name (async function)", async (t) => {
-	await setupSapUiDefineCallWithStubbedLogger(t);
-	const {SapUiDefineCallWithStubbedLogger, warningLogSpy} = t.context;
-	const ast = parse("sap.ui.define(['wanted'], async function(johndoe) {});");
-	const call = new SapUiDefineCallWithStubbedLogger(ast, "FileSystemName");
-	t.is(call.findImportName("wanted.js"), "johndoe");
-	t.is(warningLogSpy.callCount, 0, "Warning log is not called");
-});
-
-test.serial("Find Import Name (async arrow function)", async (t) => {
-	await setupSapUiDefineCallWithStubbedLogger(t);
-	const {SapUiDefineCallWithStubbedLogger, warningLogSpy} = t.context;
-	const ast = parse("sap.ui.define(['wanted'], async (johndoe) => {return johndoe});");
-	const call = new SapUiDefineCallWithStubbedLogger(ast, "FileSystemName");
-	t.is(call.findImportName("wanted.js"), "johndoe");
-	t.is(warningLogSpy.callCount, 0, "Warning log is not called");
-});
-
-test.serial("Find Import Name (async arrow function with implicit return)", async (t) => {
-	await setupSapUiDefineCallWithStubbedLogger(t);
-	const {SapUiDefineCallWithStubbedLogger, warningLogSpy} = t.context;
-	const ast = parse("sap.ui.define(['wanted'], async (johndoe) => johndoe);");
-	const call = new SapUiDefineCallWithStubbedLogger(ast, "FileSystemName");
-	t.is(call.findImportName("wanted.js"), "johndoe");
-	t.is(warningLogSpy.callCount, 0, "Warning log is not called");
-});
-
-test("Export as Global: omitted", (t) => {
-	const ast = parse("sap.ui.define(['wanted'], function(johndoe) {});");
-	const call = new SapUiDefineCall(ast, "FileSystemName");
-	t.is(call.exportAsGlobal, false);
-});
-
-test("Export as Global: false", (t) => {
-	const ast = parse("sap.ui.define(['wanted'], function(johndoe) {}, false);");
-	const call = new SapUiDefineCall(ast, "FileSystemName");
-	t.is(call.exportAsGlobal, false);
-});
-
-test("Export as Global: true", (t) => {
-	const ast = parse("sap.ui.define(['wanted'], function(johndoe) {}, true);");
-	const call = new SapUiDefineCall(ast, "FileSystemName");
-	t.is(call.exportAsGlobal, true);
-});
-
