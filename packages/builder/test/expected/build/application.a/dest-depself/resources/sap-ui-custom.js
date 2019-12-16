fbe18099d6058d3edc7fa8aa46a7e91223cb849d
Kristian Kraljic <kris@kra.lc>
2019-12-16 16:06:34 +0100

    [builder][FEATURE] Add included/excludedDependencies parameter (https://github.com/SAP/ui5-builder/pull/380)
    When setting the buildDependencies parameter to "true" builder
    used to build all dependencies of the project. By adding two new
    parameters includedDependencies and excludedDependencies,
    similar to the existing included/excludedTasks parameters, one
    can now choose which dependencies to include into or exclude from
    the build.
    
    This is also useful for self-contained builds to include the built
    dependency into the bundle, instead of the unbuilt source read
    from the file system (e.g. in case a transpile task is present).
    
    Default behavior is unchanged. Parameter "buildDependencies" still
    needs to be set before dependencies can be excluded or included.
    
    * Allow RegExp for included/excludedTasks
    
    This comes in handy, e.g. in case all UI5 libraries should be excluded
    from the build (excludedDependencies=[/^sap\.ui\./])
    
    * Add tests for builder w/ buildDeps & selfContained
    
    This change adds tests to builder for building bundles using parameters
    buildDependencies, included/excludedDependencies, selfContained and a
    combination thereof. All these have previously been untested.

--- packages/builder/test/expected/build/application.a/dest-depself/resources/sap-ui-custom.js
+++ packages/builder/test/expected/build/application.a/dest-depself/resources/sap-ui-custom.js
@@ -1,14 +1,10 @@
 //@ui5-bundle sap-ui-custom.js
-sap.ui.predefine("id1/test", ["library/d/some"],function(n){function o(n){var o=n;console.log(o)}o()});
-/*
- * Some fancy copyright
- */
-sap.ui.predefine("library/d/library", ["sap/ui/core/Lib","sap/ui/core/library"],function(e){"use strict";return e.init({name:"library.d",version:"1.0.0",dependencies:["sap.ui.core"],types:[],interfaces:[],controls:[],elements:[],noLibraryCSS:true})});
-/*!
- * Some fancy copyright
- */
-sap.ui.predefine("library/d/some", ["./library"],l=>{var o="World";console.log("Hello "+o)});
 sap.ui.require.preload({
-	"id1/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"id1","type":"application","applicationVersion":{"version":"1.2.2"},"embeds":["embedded"],"title":"{{title}}"}}'
+	"application/a/test.js":function(){sap.ui.define(["library/d/some"],function(n){function o(n){var o=n;console.log(o)}o()});
+},
+	"library/d/some.js":function(){/*!
+ * Some fancy copyright
+ */
+(function(){var o="World";console.log("Hello "+o)})();
+}
 });
-//# sourceMappingURL=sap-ui-custom.js.map
