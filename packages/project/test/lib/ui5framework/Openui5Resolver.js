0912dbc0bfd5ba68cba5369e1886fc27e3df0cf1
Matthias Oßwald <1410947+matz3@users.noreply.github.com>
2020-03-10 17:56:25 +0100

    [project][FEATURE] Add ui5Framework translator and resolvers (https://github.com/SAP/ui5-project/pull/265)

--- packages/project/test/lib/ui5framework/Openui5Resolver.js
+++ packages/project/test/lib/ui5framework/Openui5Resolver.js
@@ -1,53 +1,25 @@
-import test from "ava";
-import sinon from "sinon";
-import esmock from "esmock";
-import path from "node:path";
-import os from "node:os";
+const test = require("ava");
+const sinon = require("sinon");
 
-test.beforeEach(async (t) => {
-	t.context.InstallerStub = sinon.stub();
-	t.context.fetchPackageDistTags = sinon.stub();
-	t.context.fetchPackageManifestStub = sinon.stub();
-	t.context.fetchPackageVersionsStub = sinon.stub();
-	t.context.installPackageStub = sinon.stub();
-	t.context.InstallerStub.callsFake(() => {
-		return {
-			fetchPackageDistTags: t.context.fetchPackageDistTags,
-			fetchPackageManifest: t.context.fetchPackageManifestStub,
-			fetchPackageVersions: t.context.fetchPackageVersionsStub,
-			installPackage: t.context.installPackageStub
-		};
-	});
+const Openui5Resolver = require("../../../lib/ui5Framework/Openui5Resolver");
 
-	t.context.Openui5Resolver = await esmock("../../../lib/ui5Framework/Openui5Resolver.js", {
-		"../../../lib/ui5Framework/npm/Installer": t.context.InstallerStub
-	});
-});
-
-test.afterEach.always(() => {
-	sinon.restore();
-});
-
-test.serial("Openui5Resolver: _getNpmPackageName", (t) => {
-	const {Openui5Resolver} = t.context;
+test("Openui5Resolver: _getNpmPackageName", (t) => {
 	t.is(Openui5Resolver._getNpmPackageName("foo"), "@openui5/foo");
 });
 
-test.serial("Openui5Resolver: _getLibaryName", (t) => {
-	const {Openui5Resolver} = t.context;
+test("Openui5Resolver: _getLibaryName", (t) => {
 	t.is(Openui5Resolver._getLibaryName("@openui5/foo"), "foo");
 	t.is(Openui5Resolver._getLibaryName("@something/else"), "@something/else");
 });
 
-test.serial("Openui5Resolver: getLibraryMetadata", async (t) => {
-	const {Openui5Resolver} = t.context;
-
+test("Openui5Resolver: _getLibraryMetadata", async (t) => {
 	const resolver = new Openui5Resolver({
 		cwd: "/test-project/",
 		version: "1.75.0"
 	});
 
-	t.context.fetchPackageManifestStub
+	const fetchPackageManifest = sinon.stub(resolver._installer, "fetchPackageManifest");
+	fetchPackageManifest
 		.callsFake(async ({pkgName}) => {
 			throw new Error(`Unknown install call: ${pkgName}`);
 		})
@@ -62,8 +34,8 @@ test.serial("Openui5Resolver: getLibraryMetadata", async (t) => {
 		});
 
 	async function assert(libraryName, expectedMetadata) {
-		const pLibraryMetadata = resolver.getLibraryMetadata(libraryName);
-		const pLibraryMetadata2 = resolver.getLibraryMetadata(libraryName);
+		const pLibraryMetadata = resolver._getLibraryMetadata(libraryName);
+		const pLibraryMetadata2 = resolver._getLibraryMetadata(libraryName);
 
 		const libraryMetadata = await pLibraryMetadata;
 		t.deepEqual(libraryMetadata, expectedMetadata,
@@ -72,7 +44,7 @@ test.serial("Openui5Resolver: getLibraryMetadata", async (t) => {
 		t.deepEqual(libraryMetadata2, expectedMetadata,
 			libraryName + ": Second call should also resolve with expected metadata");
 
-		const libraryMetadata3 = await resolver.getLibraryMetadata(libraryName);
+		const libraryMetadata3 = await resolver._getLibraryMetadata(libraryName);
 
 		t.deepEqual(libraryMetadata3, expectedMetadata,
 			libraryName + ": Third call should still return the same metadata");
@@ -96,21 +68,19 @@ test.serial("Openui5Resolver: getLibraryMetadata", async (t) => {
 		]
 	});
 
-	t.is(t.context.fetchPackageManifestStub.callCount, 2, "fetchPackageManifest should be called twice");
+	t.is(fetchPackageManifest.callCount, 2, "fetchPackageManifest should be called twice");
 });
 
-test.serial("Openui5Resolver: handleLibrary", async (t) => {
-	const {Openui5Resolver} = t.context;
-
+test("Openui5Resolver: handleLibrary", async (t) => {
 	const resolver = new Openui5Resolver({
 		cwd: "/test-project/",
 		version: "1.75.0"
 	});
 
-	const getLibraryMetadataStub = sinon.stub(resolver, "getLibraryMetadata");
+	const getLibraryMetadataStub = sinon.stub(resolver, "_getLibraryMetadata");
 	getLibraryMetadataStub
 		.callsFake(async (libraryName) => {
-			throw new Error("getLibraryMetadata stub called with unknown libraryName: " + libraryName);
+			throw new Error("_getLibraryMetadata stub called with unknown libraryName: " + libraryName);
 		})
 		.withArgs("sap.ui.lib1").resolves({
 			"id": "@openui5/sap.ui.lib1",
@@ -119,7 +89,8 @@ test.serial("Openui5Resolver: handleLibrary", async (t) => {
 			"optionalDependencies": []
 		});
 
-	t.context.installPackageStub
+	const _installPackage = sinon.stub(resolver._installer, "installPackage");
+	_installPackage
 		.callsFake(async ({pkgName, version}) => {
 			throw new Error(`Unknown install call: ${pkgName}@${version}`);
 		})
@@ -139,79 +110,4 @@ test.serial("Openui5Resolver: handleLibrary", async (t) => {
 	}, "Expected library metadata should be returned");
 
 	t.deepEqual(await promises.install, {pkgPath: "/foo/sap.ui.lib1"}, "Install should resolve with expected object");
-});
-
-test.serial("Openui5Resolver: Static _getInstaller", (t) => {
-	const {Openui5Resolver} = t.context;
-
-	const options = {
-		cwd: "/cwd",
-		ui5DataDir: "/ui5DataDir"
-	};
-
-	const installer = Openui5Resolver._getInstaller(options);
-
-	t.is(t.context.InstallerStub.callCount, 1, "Installer should be called once");
-	t.true(t.context.InstallerStub.calledWithNew(), "Installer should be called with new");
-	t.is(installer, t.context.InstallerStub.getCall(0).returnValue, "Installer instance is returned");
-	t.deepEqual(t.context.InstallerStub.getCall(0).args, [{
-		cwd: path.resolve("/cwd"),
-		ui5DataDir: path.resolve("/ui5DataDir")
-	}], "Installer should be called with expected arguments");
-});
-
-test.serial("Openui5Resolver: Static _getInstaller without options", (t) => {
-	const {Openui5Resolver} = t.context;
-
-	const installer = Openui5Resolver._getInstaller();
-
-	t.is(t.context.InstallerStub.callCount, 1, "Installer should be called once");
-	t.true(t.context.InstallerStub.calledWithNew(), "Installer should be called with new");
-	t.is(installer, t.context.InstallerStub.getCall(0).returnValue, "Installer instance is returned");
-	t.deepEqual(t.context.InstallerStub.getCall(0).args, [{
-		cwd: process.cwd(),
-		ui5DataDir: path.join(os.homedir(), ".ui5")
-	}], "Installer should be called with expected arguments");
-});
-
-test.serial("Openui5Resolver: Static fetchAllVersions", async (t) => {
-	const {Openui5Resolver} = t.context;
-
-	const expectedVersions = ["1.75.0", "1.75.1", "1.76.0"];
-
-	t.context.fetchPackageVersionsStub.returns(expectedVersions);
-
-	const getInstallerSpy = sinon.spy(Openui5Resolver, "_getInstaller");
-
-	const versions = await Openui5Resolver.fetchAllVersions();
-
-	t.deepEqual(versions, expectedVersions, "Fetched versions should be correct");
-
-	t.is(t.context.fetchPackageVersionsStub.callCount, 1, "fetchPackageVersions should be called once");
-	t.deepEqual(t.context.fetchPackageVersionsStub.getCall(0).args, [{pkgName: "@openui5/sap.ui.core"}],
-		"fetchPackageVersions should be called with expected arguments");
-
-	t.is(getInstallerSpy.callCount, 1, "_getInstaller should be called once");
-	t.is(getInstallerSpy.getCall(0).args[0], undefined, "_getInstaller should be called without any options");
-});
-
-test.serial("Openui5Resolver: Static fetchAllTags", async (t) => {
-	const {Openui5Resolver} = t.context;
-
-	const expectedTags = ["latest", "latest-1.71", "latest-1"];
-
-	t.context.fetchPackageDistTags.returns(expectedTags);
-
-	const getInstallerSpy = sinon.spy(Openui5Resolver, "_getInstaller");
-
-	const tags = await Openui5Resolver.fetchAllTags();
-
-	t.deepEqual(tags, expectedTags, "Fetched tags should be correct");
-
-	t.is(t.context.fetchPackageDistTags.callCount, 1, "fetchPackageVersions should be called once");
-	t.deepEqual(t.context.fetchPackageDistTags.getCall(0).args, [{pkgName: "@openui5/sap.ui.core"}],
-		"fetchPackageVersions should be called with expected arguments");
-
-	t.is(getInstallerSpy.callCount, 1, "_getInstaller should be called once");
-	t.is(getInstallerSpy.getCall(0).args[0], undefined, "_getInstaller should be called without any options");
 });
