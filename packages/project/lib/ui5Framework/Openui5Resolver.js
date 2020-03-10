ecfca9e90a6b2ec86bd43cd32541f9b3dea2f514
Matthias Oßwald <1410947+matz3@users.noreply.github.com>
2020-03-10 17:56:25 +0100

    [project][FEATURE] Add ui5Framework translator and resolvers (https://github.com/SAP/ui5-project/pull/265)

--- packages/project/lib/ui5Framework/Openui5Resolver.js
+++ packages/project/lib/ui5Framework/Openui5Resolver.js
@@ -1,40 +1,28 @@
-import path from "node:path";
-import os from "node:os";
-import AbstractResolver from "./AbstractResolver.js";
-import Installer from "./npm/Installer.js";
-
-const OPENUI5_CORE_PACKAGE = "@openui5/sap.ui.core";
+const AbstractResolver = require("./AbstractResolver");
+const Installer = require("./npm/Installer");
 
 /**
  * Resolver for the OpenUI5 framework
  *
  * @public
- * @class
- * @alias @ui5/project/ui5Framework/Openui5Resolver
- * @extends @ui5/project/ui5Framework/AbstractResolver
+ * @memberof module:@ui5/project.ui5Framework
+ * @extends  module:@ui5/project.ui5Framework.AbstractResolver
  */
 class Openui5Resolver extends AbstractResolver {
 	/**
 	 * @param {*} options options
 	 * @param {string} options.version OpenUI5 version to use
 	 * @param {string} [options.cwd=process.cwd()] Working directory to resolve configurations like .npmrc
-	 * @param {string} [options.ui5DataDir="~/.ui5"] UI5 home directory location. This will be used to store packages,
-	 * metadata and configuration used by the resolvers. Relative to `process.cwd()`
-	 * @param {string} [options.cacheDir] Where to store temp/cached packages.
-	 * @param {string} [options.packagesDir] Where to install packages
-	 * @param {string} [options.stagingDir] The staging directory for the packages
+	 * @param {string} [options.ui5HomeDir="~/.ui5"] UI5 home directory location. This will be used to store packages, metadata and configuration used by the resolvers. Relative to `process.cwd()`
 	 */
 	constructor(options) {
 		super(options);
 
-		const {cacheDir, packagesDir, stagingDir} = options;
-
 		this._installer = new Installer({
 			cwd: this._cwd,
-			ui5DataDir: this._ui5DataDir,
-			cacheDir, packagesDir, stagingDir
+			ui5HomeDir: this._ui5HomeDir
 		});
-		this._loadLibraryMetadata = Object.create(null);
+		this._loadLibraryMetadata = {};
 	}
 	static _getNpmPackageName(libraryName) {
 		return "@openui5/" + libraryName;
@@ -42,7 +30,7 @@ class Openui5Resolver extends AbstractResolver {
 	static _getLibaryName(pkgName) {
 		return pkgName.replace(/^@openui5\//, "");
 	}
-	getLibraryMetadata(libraryName) {
+	_getLibraryMetadata(libraryName) {
 		if (!this._loadLibraryMetadata[libraryName]) {
 			this._loadLibraryMetadata[libraryName] = Promise.resolve().then(async () => {
 				// Trigger manifest request to gather transitive dependencies
@@ -76,7 +64,7 @@ class Openui5Resolver extends AbstractResolver {
 		const pkgName = Openui5Resolver._getNpmPackageName(libraryName);
 		return {
 			// Trigger metadata request
-			metadata: this.getLibraryMetadata(libraryName),
+			metadata: this._getLibraryMetadata(libraryName),
 			// Also trigger installation of package
 			install: this._installer.installPackage({
 				pkgName,
@@ -84,24 +72,6 @@ class Openui5Resolver extends AbstractResolver {
 			})
 		};
 	}
-	static async fetchAllVersions(options) {
-		const installer = this._getInstaller(options);
-		return await installer.fetchPackageVersions({pkgName: OPENUI5_CORE_PACKAGE});
-	}
-
-	static async fetchAllTags(options) {
-		const installer = this._getInstaller(options);
-		return installer.fetchPackageDistTags({pkgName: OPENUI5_CORE_PACKAGE});
-	}
-
-	static _getInstaller({ui5DataDir, cwd} = {}) {
-		return new Installer({
-			cwd: cwd ? path.resolve(cwd) : process.cwd(),
-			ui5DataDir:
-				ui5DataDir ? path.resolve(ui5DataDir) :
-					path.join(os.homedir(), ".ui5")
-		});
-	}
 }
 
-export default Openui5Resolver;
+module.exports = Openui5Resolver;
