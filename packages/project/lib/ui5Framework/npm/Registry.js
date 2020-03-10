158289b3da03c9b629d2b8c0c21a4b26c727e1c3
Matthias Oßwald <1410947+matz3@users.noreply.github.com>
2020-03-10 17:56:25 +0100

    [project][FEATURE] Add ui5Framework translator and resolvers (https://github.com/SAP/ui5-project/pull/265)

--- packages/project/lib/ui5Framework/npm/Registry.js
+++ packages/project/lib/ui5Framework/npm/Registry.js
@@ -1,5 +1,4 @@
-import {getLogger} from "@ui5/logger";
-const log = getLogger("ui5Framework:npm:Registry");
+const log = require("@ui5/logger").getLogger("normalizer:ui5Framework:npm:Registry");
 
 function logConfig(config, configName) {
 	const configValue = config[configName];
@@ -9,86 +8,47 @@ function logConfig(config, configName) {
 }
 
 class Registry {
-	/**
-	 * @param {object} parameters Parameters
-	 * @param {string} parameters.cwd Current working directory
-	 * @param {string} parameters.cacheDir Cache directory
-	 */
 	constructor({cwd, cacheDir}) {
-		if (!cwd) {
-			throw new Error(`Registry: Missing parameter "cwd"`);
-		}
-		if (!cacheDir) {
-			throw new Error(`Registry: Missing parameter "cacheDir"`);
-		}
+		this._pacote = require("pacote");
 		this._cwd = cwd;
 		this._cacheDir = cacheDir;
 	}
-	async requestPackagePackument(pkgName) {
-		const {pacote, pacoteOptions} = await this._getPacote();
-		return pacote.packument(pkgName, pacoteOptions);
+	requestPackageManifest(pkgName, version) {
+		return this._pacote.manifest(`${pkgName}@${version}`, this._getPacoteOptions());
 	}
-	async requestPackageManifest(pkgName, version) {
-		const {pacote, pacoteOptions} = await this._getPacote();
-
-		return pacote.manifest(`${pkgName}@${version}`, pacoteOptions);
+	extractPackage(pkgName, version, targetDir) {
+		return this._pacote.extract(`${pkgName}@${version}`, targetDir, this._getPacoteOptions());
 	}
-	async extractPackage(pkgName, version, targetDir) {
-		const {pacote, pacoteOptions} = await this._getPacote();
-		try {
-			await pacote.extract(`${pkgName}@${version}`, targetDir, pacoteOptions);
-		} catch (err) {
-			throw new Error(`Failed to extract package ${pkgName}@${version}: ${err.message}`);
-		}
-	}
-
-	async _getPacote() {
-		if (this._pGetPacote) {
-			return this._pGetPacote;
-		}
-		return this._pGetPacote = (async () => {
-			return {
-				pacote: (await import("pacote")).default,
-				pacoteOptions: await this._getPacoteOptions()
+	_getPacoteOptions() {
+		if (!this._npmConfig) {
+			const libnpmconfig = require("libnpmconfig");
+			const opts = {
+				cache: this._cacheDir
 			};
-		})();
-	}
+			if (log.isLevelEnabled("verbose")) {
+				opts.log = log._getLogger();
+			}
+			const config = libnpmconfig.read(opts, {
+				cwd: this._cwd
+			}).toJSON();
 
-	async _getPacoteOptions() {
-		const {default: Config} = await import("@npmcli/config");
-		const {
-			default: {flatten, definitions, shorthands, defaults},
-		} = await import("@npmcli/config/lib/definitions/index.js");
+			log.verbose(`Using npm configuration (extract):`);
+			// Do not log full configuration as it may contain authentication tokens
+			logConfig(config, "registry");
+			logConfig(config, "@sapui5:registry");
+			logConfig(config, "@openui5:registry");
+			logConfig(config, "proxy");
+			logConfig(config, "globalconfig");
+			logConfig(config, "userconfig");
+			logConfig(config, "cache");
+			logConfig(config, "cwd");
 
-		const configuration = new Config({
-			cwd: this._cwd,
-			npmPath: this._cwd,
-			definitions,
-			flatten,
-			shorthands,
-			defaults
-		});
+			this._npmConfig = config;
+		}
 
-		await configuration.load(); // Reads through the configurations
-		const config = configuration.flat; // JSON. Formatted via "flatten"
-
-		// Always use our cache dir instead of the configured one
-		config.cache = this._cacheDir;
-
-		log.verbose(`Using npm configuration (extract):`);
-		// Do not log full configuration as it may contain authentication tokens
-		logConfig(config, "registry");
-		logConfig(config, "@sapui5:registry");
-		logConfig(config, "@openui5:registry");
-		logConfig(config, "proxy");
-		logConfig(config, "httpsProxy");
-		logConfig(config, "globalconfig");
-		logConfig(config, "userconfig");
-		logConfig(config, "cache");
-		logConfig(config, "cwd");
-
-		return config;
+		// Use cached config
+		return this._npmConfig;
 	}
 }
 
-export default Registry;
+module.exports = Registry;
