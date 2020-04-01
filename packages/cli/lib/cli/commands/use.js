6d974b11ce84a1b40a8d483c6012bf568f74cfb6
Matthias Oßwald <1410947+matz3@users.noreply.github.com>
2020-04-01 11:41:05 +0200

    [cli][FEATURE] Add "ui5 use/add" commands (https://github.com/SAP/ui5-cli/pull/315)
    Co-authored-by: Merlin Beutlberger <m.beutlberger@sap.com>

--- packages/cli/lib/cli/commands/use.js
+++ packages/cli/lib/cli/commands/use.js
@@ -1,10 +1,8 @@
 // Use
-import baseMiddleware from "../middlewares/base.js";
-
 const useCommand = {
 	command: "use <framework-info>",
 	describe: "Initialize or update the project's framework configuration.",
-	middlewares: [baseMiddleware]
+	middlewares: [require("../middlewares/base.js")]
 };
 
 useCommand.builder = function(cli) {
@@ -12,18 +10,16 @@ useCommand.builder = function(cli) {
 		.positional("framework-info", {
 			describe: "Framework name, version or both (name@version).\n" +
 			"Name can be \"SAPUI5\" or \"OpenUI5\" (case-insensitive).\n" +
-			"Version can be \"latest\" (default), a version or range according to the Semantic Versioning specification (https://semver.org/), " +
-			"or a tag available in the npm registry.\n" +
-			"For SAP-internal usage the version can also be \"latest-snapshot\", " +
-			"a version or range ending with -SNAPSHOT, " +
-			"or a simplified range such as \"1-SNAPSHOT\", \"1.x-SNAPSHOT\" or \"1.108-SNAPSHOT\".",
+			"Version can be \"latest\", \"1.xx\" or \"1.xx.x\".",
 			type: "string"
 		})
 		.example("$0 use sapui5@latest", "Use SAPUI5 in the latest available version")
 		.example("$0 use openui5@1.76", "Use OpenUI5 in the latest available 1.76 patch version")
 		.example("$0 use latest", "Use the latest available version of the configured framework")
-		.example("$0 use openui5", "Use OpenUI5 in the latest available version");
+		.example("$0 use openui5", "Use OpenUI5 without a version (or use existing version)");
 };
+
+const versionRegExp = /^(0|[1-9]\d*)\.(0|[1-9]\d*)(?:\.(0|[1-9]\d*))?$/;
 
 function parseFrameworkInfo(frameworkInfo) {
 	const parts = frameworkInfo.split("@");
@@ -37,16 +33,15 @@ function parseFrameworkInfo(frameworkInfo) {
 		if (!nameOrVersion) {
 			throw new Error("Invalid framework info: " + frameworkInfo);
 		}
-		if (["sapui5", "openui5"].includes(nameOrVersion.toLowerCase())) {
-			// Framework name without version uses "latest", similar to npm install behavior
-			return {
-				name: nameOrVersion,
-				version: "latest"
-			};
-		} else {
+		if (nameOrVersion === "latest" || versionRegExp.test(nameOrVersion)) {
 			return {
 				name: null,
 				version: nameOrVersion
+			};
+		} else {
+			return {
+				name: nameOrVersion,
+				version: null
 			};
 		}
 	} else {
@@ -61,14 +56,13 @@ function parseFrameworkInfo(frameworkInfo) {
 useCommand.handler = async function(argv) {
 	const frameworkOptions = parseFrameworkInfo(argv["framework-info"]);
 
-	const projectGraphOptions = {
-		dependencyDefinition: argv.dependencyDefinition,
-		config: argv.config
+	const normalizerOptions = {
+		translatorName: argv.translator,
+		configPath: argv.config
 	};
 
-	const {default: use} = await import("../../framework/use.js");
-	const {usedFramework, usedVersion, yamlUpdated} = await use({
-		projectGraphOptions,
+	const {usedFramework, usedVersion, yamlUpdated} = await require("../../framework/use")({
+		normalizerOptions,
 		frameworkOptions
 	});
 
@@ -81,11 +75,9 @@ useCommand.handler = async function(argv) {
 			throw new Error(`Internal error while updating ui5.yaml to ${usedFramework} version ${usedVersion}`);
 		}
 	} else {
-		process.stdout.write(`Updated configuration written to ${argv.config || "ui5.yaml"}`);
-		process.stdout.write("\n");
-		process.stdout.write(`This project is now using ${usedFramework} version ${usedVersion}`);
-		process.stdout.write("\n");
+		console.log(`Updated configuration written to ${argv.config || "ui5.yaml"}`);
+		console.log(`This project is now using ${usedFramework} version ${usedVersion}`);
 	}
 };
 
-export default useCommand;
+module.exports = useCommand;
