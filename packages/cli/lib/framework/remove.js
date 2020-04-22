c17ef3cf5e37f270e61b058b9d48739d59d8a7e2
Tobias Sorn <tobias.sorn@web.de>
2020-04-22 15:41:48 +0200

    [cli][FEATURE] Add "ui5 remove" command (https://github.com/SAP/ui5-cli/pull/327)
    * [FEATURE] Add "ui5 remove" command
    
    Followup of  https://github.com/SAP/ui5-cli/issues/315.
    Add ui5 remove command which removes libraries from
    the framework libraries section in the ui5.yaml.

--- packages/cli/lib/framework/remove.js
+++ packages/cli/lib/framework/remove.js
@@ -1,53 +1,47 @@
-import {getRootProjectConfiguration} from "./utils.js";
-import {getLogger} from "@ui5/logger";
+const {getRootProjectConfiguration, isValidSpecVersion} = require("./utils");
+const log = require("@ui5/logger").getLogger("cli:framework:remove");
 
-const log = getLogger("cli:framework:remove");
 
 /**
  * Removes the given set of libraries from the framework libraries section in the ui5.yaml
- *
- * @param {object} parameters Parameters
- * @param {object} parameters.projectGraphOptions
- * @param {object} parameters.libraries
  */
-export default async function({projectGraphOptions, libraries}) {
-	const project = await getRootProjectConfiguration(projectGraphOptions);
+module.exports = async function({normalizerOptions, libraries}) {
+	const project = await getRootProjectConfiguration({normalizerOptions});
 
-	if (project.getSpecVersion().lt("2.0")) {
+	if (!isValidSpecVersion(project.specVersion)) {
 		throw new Error(
 			`ui5 remove command requires specVersion "2.0" or higher. ` +
-			`Project ${project.getName()} uses specVersion "${project.getSpecVersion().toString()}"`
+			`Project ${project.metadata.name} uses specVersion "${project.specVersion}"`
 		);
 	}
 
-	if (!project.getFrameworkName()) {
+	if (!project.framework) {
 		throw new Error(
-			`Project ${project.getName()} is missing a framework configuration. ` +
+			`Project ${project.metadata.name} is missing a framework configuration. ` +
 			`Please use "ui5 use" to configure a framework and version.`
 		);
 	}
-	if (!project.getFrameworkVersion()) {
+	if (!project.framework.version) {
 		throw new Error(
-			`Project ${project.getName()} does not define a framework version configuration. ` +
+			`Project ${project.metadata.name} does not define a framework version configuration. ` +
 			`Please use "ui5 use" to configure a version.`
 		);
 	}
 
-	if (!project.getFrameworkDependencies().length) {
+	if (!project.framework.libraries) {
 		throw new Error(
-			`Project ${project.getName()} does not define framework libraries.`
+			`Project ${project.metadata.name} does not define framework libraries.`
 		);
 	}
 
-	const allLibraries = [...project.getFrameworkDependencies()];
+	const allLibraries = [...project.framework.libraries];
 
 	libraries.forEach((library) => {
 		const iIndexToRemove = allLibraries.findIndex(($) => $.name === library.name);
 		if (iIndexToRemove === -1) {
 			// do not fail here just log, because the framework library is not present afterwards
 			log.warn(
-				`Failed to remove framework library ${library.name} from project ` +
-				`${project.getName()} because it is not present.`
+				`Failed to remove framework library ${library.name} from project ${project.metadata.name} because it is not present.`
 			);
 		} else {
 			allLibraries.splice(iIndexToRemove, 1);
@@ -62,10 +56,8 @@ export default async function({projectGraphOptions, libraries}) {
 	// Try to update YAML file but still return with name and resolved version in case it failed
 	let yamlUpdated = false;
 	try {
-		const {default: updateYaml} = await import("./updateYaml.js");
-		await updateYaml({
+		await require("./updateYaml")({
 			project,
-			configPathOverride: projectGraphOptions.config,
 			data: {
 				framework: {
 					libraries: allLibraries
@@ -81,4 +73,4 @@ export default async function({projectGraphOptions, libraries}) {
 	return {
 		yamlUpdated
 	};
-}
+};
