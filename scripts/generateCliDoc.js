4b8c28a1246d960f07846a3252f0fed521f2eac0
Wolfram Woerner <woerner.wolfram@web.de>
2020-07-27 16:59:25 +0200

    [INTERNAL] Add automatic generation of CLI documentation (#246)

--- scripts/generateCliDoc.js
+++ scripts/generateCliDoc.js
@@ -1,14 +1,14 @@
-import {execSync as exec} from "node:child_process";
-import {readFileSync, writeFileSync} from "node:fs";
-import {fileURLToPath} from "node:url";
-import Handlebars from "handlebars";
+const exec = require('child_process').execSync;
 
-const source = readFileSync(fileURLToPath(new URL("../scripts/resources/CLI.template.md", import.meta.url)), "utf8");
+const fs = require('fs');
+const Handlebars = require('handlebars');
+
+const source = fs.readFileSync('./scripts/resources/CLI.template.md', 'utf8');
 const template = Handlebars.compile(source);
 
 
 let first = true;
-const obj = {
+let obj = {
 	common: "",
 	usage: "",
 	desc: "",
@@ -24,9 +24,9 @@ function execute(command) {
 }
 
 function parseOutput(stdout) {
-	const sections = stdout.split("\n\n");
+	let sections = stdout.split("\n\n");
 	if (first) {
-		for (const section of sections) {
+		for (let section of sections) {
 			if (section.includes("Usage:")) {
 				obj.common = section;
 			}
@@ -41,8 +41,9 @@ function parseOutput(stdout) {
 			}
 		}
 		first = false;
-	} else {
-		if (sections[0].includes("local")) {
+	}
+	else {
+		if(sections[0].includes("local")) {
 			obj.usage = sections[1];
 			obj.desc = sections[2];
 		} else {
@@ -54,14 +55,14 @@ function parseOutput(stdout) {
 		obj.examples = [];
 		obj.positionals = [];
 		obj.addOptions = [];
-		for (const section of sections) {
+		for (let section of sections) {
 			if (section.includes("Positionals:")) {
 				obj.positionals = section.split("\n");
 			}
 			if (section.includes("Options:")) {
-				obj.addOptions = section.split("\n").filter(function(el) {
-					const array = obj.commonOptions;
-					array.forEach(function(item, index, array) {
+				obj.addOptions = section.split("\n").filter(function (el) {
+					let array = obj.commonOptions;
+					array.forEach(function (item, index, array) {
 						array[index] = item.replace(/\s+/g, "");
 					});
 					return !array.includes(el.replace(/\s+/g, ""));
@@ -75,93 +76,96 @@ function parseOutput(stdout) {
 			}
 		}
 	}
+
 }
 
 function generateDoc() {
+
 	execute("ui5 --help");
 
-	const optionObj = [];
+	let optionObj = [];
 	obj.commonOptions.shift();
-	for (const all of obj.commonOptions) {
-		const temp = checkChars(all);
-		const {command, description, details} = splitString(temp);
-		optionObj.push({commonOption: command, commonOptionDescription: description, commonOptionDetails: details});
+	for (let all of obj.commonOptions) {
+		let temp = checkChars(all);
+		let command, description;
+		({ command, description } = splitString(command, temp, description));
+		optionObj.push({ commonOption: command, commonOptionDescription: description });
 	}
 
 	obj.examples.shift();
-	const examplesObj = [];
-	for (const all of obj.examples) {
-		const temp = checkChars(all);
-		if (temp == "") {
+	let examplesObj = [];
+	index = obj.examples[0].indexOf("Execute");
+	for (let all of obj.examples) {
+		let temp = checkChars(all);
+		if (temp == '') {
 			continue;
 		}
-		const {command, description} = splitString(temp);
-		examplesObj.push({commonExample: command, commonExampleDescription: description});
+		let command, description;
+		({ command, description } = splitString(command, temp, description));
+		examplesObj.push({ commonExample: command, commonExampleDescription: description });
 	}
 
 	obj.commands.shift();
-	const commandsArray = [];
-	const commands = obj.commands;
-	for (const all of commands) {
-		const command = all.trim().split(" ").slice(0, 2).join(" ");
-		execute(command + " --help");
+	let commandsArray = [];
+	let commands = obj.commands;
+	for (let all of commands) {
+		let command = all.trim().split(' ').slice(0, 2).join(' ');
+		execute(command + ' --help');
 
-		const commandsObj = [];
+		let commandsObj = [];
 		obj.commands.shift();
 		if (!(obj.commands.length <= 1)) {
-			for (const all of obj.commands) {
-				const temp = checkChars(all);
-				const {command, description} = splitString(temp);
-				commandsObj.push({childCommand: command, commandDescription: description});
+			for (let all of obj.commands) {
+				let temp = checkChars(all);
+				let command, description;
+				({ command, description } = splitString(command, temp, description));
+				commandsObj.push({ childCommand: command, commandDescription: description });
 			}
 		}
 
-		const positionalObj = [];
+		let positionalObj = [];
 		obj.positionals.shift();
 
 		if (!(obj.positionals.length < 1)) {
 			let index = 0;
-			for (const all of obj.positionals) {
-				const temp = checkChars(all);
-				const {command, description, details} = splitString(temp);
+			for (let all of obj.positionals) {
+				let temp = checkChars(all);
+				let command, description;
+				({ command, description } = splitString(command, temp, description));
 				if (!(/\S/.test(command))) {
-					positionalObj[index - 1].positionalDescription =
-						positionalObj[index - 1].positionalDescription.concat("<br>", description);
-					positionalObj[index - 1].positionalDetails =details;
+					positionalObj[index - 1].positionalDescription = positionalObj[index - 1].positionalDescription.concat("<br>", description);
 					continue;
 				}
-				positionalObj.push({
-					positional: command,
-					positionalDescription: description,
-					positionalDetails: details
-				});
+				positionalObj.push({ positional: command, positionalDescription: description });
 				index++;
 			}
 		}
-		const optionObj = [];
+		let optionObj = [];
 		obj.addOptions.shift();
 		if (!(obj.addOptions.length <= 1)) {
-			for (const all of obj.addOptions) {
-				const temp = checkChars(all);
-				const {command, description, details} = splitString(temp);
-				optionObj.push({option: command, optionDescription: description, optionDetails: details});
+			for (let all of obj.addOptions) {
+				let temp = checkChars(all);
+				let command, description;
+				({ command, description } = splitString(command, temp, description));
+				optionObj.push({ option: command, optionDescription: description });
 			}
 		}
 
-		const exampleObj = [];
+		let exampleObj = [];
 		obj.examples.shift();
 		if (!(obj.examples.length <= 1)) {
-			for (const all of obj.examples) {
-				const temp = checkChars(all);
-				if (temp == "") {
+			for (let all of obj.examples) {
+				let temp = checkChars(all);
+				if (temp == '') {
 					continue;
 				}
-				const {command, description} = splitString(temp);
-				exampleObj.push({example: command, exampleDescription: description});
+				let command, description;
+				({ command, description } = splitString(command, temp, description));
+				exampleObj.push({ example: command, exampleDescription: description });
 			}
 		}
 
-		const commandObj = {
+		let commandObj = {
 			command: command,
 			description: obj.desc,
 			usage: obj.usage,
@@ -181,32 +185,28 @@ function generateDoc() {
 
 	content = content.split("&lt;").join("<").split("&gt;").join(">");
 	content = content.split("&#x3D;").join("=");
-	try {
-		writeFileSync("./packages/documentation/docs/pages/CLI.md", content);
-	} catch (err) {
-		console.error(`Failed to generate docs/pages/CLI.md: ${err.message}.`);
-		throw err;
-	}
-	console.log("Generated docs/pages/CLI.md");
+	fs.writeFile('./docs/pages/CLI.md', content, err => {
+		if (err) {
+			return console.error(`Failed to generate docs/pages/CLI.md: ${err.message}.`);
+		}
+
+		console.log('Generated docs/pages/CLI.md');
+	});
+
 }
 
-function splitString(temp) {
-	let details;
-
-	const match = temp.split("  ").filter((s) => s).map((s) => s.trim());
-	if (match.length && match[match.length - 1].startsWith("[") && match[match.length - 1].endsWith("]")) {
-		details = match.pop();
-	}
-	const description = match.pop() || "";
-	const command = match.pop() || "";
-
-	return {command, description, details};
+function splitString(command, temp, description) {
+	command = temp.split(/[^\w.,]\s(?=[A-Z])/)[0].trim();
+	description = temp.split(/[^\w.,]\s(?=[A-Z])/)[1].trim();
+	return { command, description };
 }
 
 function checkChars(all) {
-	let clean = all.split("|").join("\\|");
-	clean = clean.replace(/"\D+[di]\d{6,}/i, "\"~");
+	let clean = all.split("|").join("\\\|");
+	clean = clean.replace(/"\D+[di]\d{6,}/i, "\"$HOME/");
 	return clean;
 }
 
 generateDoc();
+
+
