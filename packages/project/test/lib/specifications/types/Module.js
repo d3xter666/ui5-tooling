646ebb3ae8b5fc12c0096db5827c7a5bc4c795cb
Merlin Beutlberger <m.beutlberger@sap.com>
2020-12-04 18:55:42 +0100

    [project][BREAKING] Implement Project Graph, build execution
    * Replace the JSON-object based dependency tree handling with a graph
    representation
        * Projects are now represented by classes with documented APIs
        * Projects can be accessed by extensions defining specVersion >=2.7
        * Speed up resolution of package.json dependencies
            * Make "ui5.dependencies" package.json configuration obsolete
    * Move build execution from ui5-builder to ui5-project
        * ui5-builder scope reduced top provides task implementations only
    * Build: Determine automatically whether a project-build requires
      dependencies to be built and build them
    * Build: Add new option 'createBuildManifest'. This will create a
      manifest file in the target directory that allows reuse of the
      build result of  library and theme-library projects in other project
      builds (RFC0011)
    
    This PR will need additional follow-up to add more test cases,
    cleanup JSDoc and possibly add more features described in the RFCs.
    
    This is a nicer version of
    https://github.com/SAP/ui5-project/pull/394
    
    Implements RFC0009: https://github.com/SAP/ui5-tooling/pull/501
    Implements RFC0011: https://github.com/SAP/ui5-tooling/pull/612
    
    BREAKING CHANGE:
    * normalizer and projectTree APIs have been removed. Use
    generateProjectGraph instead
    * Going forward only specification versions 2.0 and higher are supported
        * In case a legacy specification version is detected, an automatic,
          transparent migration is attempted.
    * Build:
        * The "dev" build mode has been removed
        * The task "generateVersionInfo" is no longer executed for
          application projects by default. You may enable it again using the
          includedTasks parameter

--- packages/project/test/lib/specifications/types/Module.js
+++ packages/project/test/lib/specifications/types/Module.js
@@ -1,79 +1,51 @@
-import test from "ava";
-import path from "node:path";
-import sinonGlobal from "sinon";
-import Specification from "../../../../lib/specifications/Specification.js";
+const test = require("ava");
+const path = require("path");
+const sinon = require("sinon");
+const mock = require("mock-require");
+const Specification = require("../../../../lib/specifications/Specification");
 
-const __dirname = import.meta.dirname;
-const moduleAPath = path.join(__dirname, "..", "..", "..", "fixtures", "module.a");
+function clone(obj) {
+	return JSON.parse(JSON.stringify(obj));
+}
 
-test.beforeEach((t) => {
-	t.context.sinon = sinonGlobal.createSandbox();
-	t.context.projectInput = {
-		id: "module.a.id",
-		version: "1.0.0",
-		modulePath: moduleAPath,
-		configuration: {
-			specVersion: "2.3",
-			kind: "project",
-			type: "module",
-			metadata: {
-				name: "module.a",
-				copyright: "Some fancy copyright" // allowed but ignored
-			},
-			resources: {
-				configuration: {
-					paths: {
-						"/": "dist",
-						"/dev/": "dev"
-					}
+const moduleA = path.join(__dirname, "..", "..", "..", "fixtures", "module.a");
+const basicProjectInput = {
+	id: "library.d.id",
+	version: "1.0.0",
+	modulePath: moduleA,
+	configuration: {
+		specVersion: "2.3",
+		kind: "project",
+		type: "module",
+		metadata: {
+			name: "module.a",
+			copyright: "Some fancy copyright" // allowed but ignored
+		},
+		resources: {
+			configuration: {
+				paths: {
+					"/": "dist",
+					"/dev/": "dev"
 				}
 			}
 		}
-	};
-});
+	}
+};
 
 test.afterEach.always((t) => {
-	t.context.sinon.restore();
+	sinon.restore();
+	mock.stopAll();
 });
 
 test("Correct class", async (t) => {
-	const {projectInput} = t.context;
-	const {default: Module} = await import("../../../../lib/specifications/types/Module.js");
-	const project = await Specification.create(projectInput);
+	const Module = mock.reRequire("../../../../lib/specifications/types/Module");
+	const project = await Specification.create(basicProjectInput);
 	t.true(project instanceof Module, `Is an instance of the Module class`);
 });
 
-test("getSourcePath: Throws", async (t) => {
-	const {projectInput} = t.context;
-	const project = await Specification.create(projectInput);
-	const err = t.throws(() => {
-		project.getSourcePath();
-	});
-	t.is(err.message, "Projects of type module have more than one source path",
-		"Threw with expected error message");
-});
-
-test("getNamespace", async (t) => {
-	const {projectInput} = t.context;
-	const project = await Specification.create(projectInput);
-	t.is(project.getNamespace(), null,
-		"Returned no namespace");
-});
-
 test("Access project resources via reader", async (t) => {
-	const {projectInput} = t.context;
-	const project = await Specification.create(projectInput);
-	t.throws(() => {
-		project.getSourcePath();
-	}, {
-		message: "Projects of type module have more than one source path"
-	}, "Threw with expected error message");
-});
-
-test("Access project resources via reader (multiple mappings)", async (t) => {
-	const {projectInput} = t.context;
-	const project = await Specification.create(projectInput);
-	const reader = project.getReader();
+	const project = await Specification.create(basicProjectInput);
+	const reader = await project.getReader();
 	const resource1 = await reader.byPath("/dev/devTools.js");
 	t.truthy(resource1, "Found the requested resource");
 	t.is(resource1.getPath(), "/dev/devTools.js", "Resource has correct path");
@@ -83,151 +55,9 @@ test("Access project resources via reader (multiple mappings)", async (t) => {
 	t.is(resource2.getPath(), "/index.js", "Resource has correct path");
 });
 
-test("Access project resources via reader (one mapping)", async (t) => {
-	const {projectInput} = t.context;
-	delete projectInput.configuration.resources.configuration.paths["/"];
-	const project = await Specification.create(projectInput);
-	const reader = project.getReader();
-	const resource1 = await reader.byPath("/dev/devTools.js");
-	t.truthy(resource1, "Found the requested resource");
-	t.is(resource1.getPath(), "/dev/devTools.js", "Resource has correct path");
-
-	const resource2 = await reader.byPath("/index.js");
-	t.falsy(resource2, "Could not find resource in unmapped path");
-});
-
-test("Access project resources via reader w/ builder excludes", async (t) => {
-	const {projectInput, sinon} = t.context;
-	const baselineProject = await Specification.create(projectInput);
-	const excludesProject = await Specification.create(projectInput);
-
-	// As of specVersion 3.0, modules are not allowed to have a "builder.resources" configuration.
-	// Hence modules can't practically be configured with builder excludes.
-	// We still simply stub the respective API call to test the code and be prepared
-	//
-	// projectInput.configuration.builder = {
-	// 	resources: {
-	// 		excludes: ["**/devTools.js"]
-	// 	}
-	// };
-	// So stub instead:
-	sinon.stub(excludesProject, "getBuilderResourcesExcludes").returns(["**/devTools.js"]);
-
-	// We now have two projects: One with excludes and one without
-	// Always compare the results of both to make sure a file is really excluded because of the
-	// configuration and not because of a typo or because of it's absence in the fixture
-
-	t.is((await baselineProject.getReader({}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for default style");
-	t.is((await excludesProject.getReader({}).byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for default style");
-
-	t.is((await baselineProject.getReader({style: "buildtime"}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for buildtime style");
-	t.is((await excludesProject.getReader({style: "buildtime"}).byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for buildtime style");
-
-	t.is((await baselineProject.getReader({style: "dist"}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for dist style");
-	t.is((await excludesProject.getReader({style: "dist"}).byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for dist style");
-
-	t.is((await baselineProject.getReader({style: "flat"}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for flat style");
-	t.is((await excludesProject.getReader({style: "flat"}).byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for flat style");
-
-	// Excludes are not applied for "runtime" style
-	t.is((await baselineProject.getReader({style: "runtime"}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for runtime style");
-	t.is((await excludesProject.getReader({style: "runtime"}).byGlob("**/devTools.js")).length, 1,
-		"Found excluded resource for runtime style");
-});
-
-test("Access project resources via workspace w/ builder excludes", async (t) => {
-	const {projectInput, sinon} = t.context;
-	const baselineProject = await Specification.create(projectInput);
-	const excludesProject = await Specification.create(projectInput);
-
-	// As of specVersion 3.0, modules are not allowed to have a "builder.resources" configuration.
-	// Hence modules can't practically be configured with builder excludes.
-	// We still simply stub the respective API call to test the code and be prepared
-	//
-	// projectInput.configuration.builder = {
-	// 	resources: {
-	// 		excludes: ["**/devTools.js"]
-	// 	}
-	// };
-	// So stub instead:
-	sinon.stub(excludesProject, "getBuilderResourcesExcludes").returns(["**/devTools.js"]);
-
-	// We now have two projects: One with excludes and one without
-	// Always compare the results of both to make sure a file is really excluded because of the
-	// configuration and not because of a typo or because of it's absence in the fixture
-
-	t.is((await baselineProject.getWorkspace().byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for default style");
-	t.is((await excludesProject.getWorkspace().byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for default style");
-});
-
-test("Access project resources via reader w/ absolute builder excludes", async (t) => {
-	const {projectInput, sinon} = t.context;
-	const baselineProject = await Specification.create(projectInput);
-	const excludesProject = await Specification.create(projectInput);
-
-	// As of specVersion 3.0, modules are not allowed to have a "builder.resources" configuration.
-	// Hence modules can't practically be configured with builder excludes.
-	// We still simply stub the respective API call to test the code and be prepared
-	//
-	// projectInput.configuration.builder = {
-	// 	resources: {
-	// 		excludes: ["/dev/devTools.js"]
-	// 	}
-	// };
-	// So stub instead:
-	sinon.stub(excludesProject, "getBuilderResourcesExcludes").returns(["/dev/devTools.js"]);
-
-	// We now have two projects: One with excludes and one without
-	// Always compare the results of both to make sure a file is really excluded because of the
-	// configuration and not because of a typo or because of it's absence in the fixture
-
-	t.is((await baselineProject.getReader({}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for default style");
-	t.is((await excludesProject.getReader({}).byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for default style");
-
-	t.is((await baselineProject.getReader({style: "buildtime"}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for buildtime style");
-	t.is((await excludesProject.getReader({style: "buildtime"}).byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for buildtime style");
-
-	t.is((await baselineProject.getReader({style: "dist"}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for dist style");
-	t.is((await excludesProject.getReader({style: "dist"}).byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for dist style");
-
-	t.is((await baselineProject.getReader({style: "flat"}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for flat style");
-	t.is((await excludesProject.getReader({style: "flat"}).byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for flat style");
-
-	// Excludes are not applied for "runtime" style
-	t.is((await baselineProject.getReader({style: "runtime"}).byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for runtime style");
-	t.is((await excludesProject.getReader({style: "runtime"}).byGlob("**/devTools.js")).length, 1,
-		"Found excluded resource for runtime style");
-
-	t.is((await baselineProject.getWorkspace().byGlob("**/devTools.js")).length, 1,
-		"Found resource in baseline project for default style");
-	t.is((await excludesProject.getWorkspace().byGlob("**/devTools.js")).length, 0,
-		"Did not find excluded resource for default style");
-});
-
 test("Modify project resources via workspace and access via reader", async (t) => {
-	const {projectInput} = t.context;
-	const project = await Specification.create(projectInput);
-	const workspace = project.getWorkspace();
+	const project = await Specification.create(basicProjectInput);
+	const workspace = await project.getWorkspace();
 	const workspaceResource = await workspace.byPath("/dev/devTools.js");
 	t.truthy(workspaceResource, "Found resource in workspace");
 
@@ -235,7 +65,7 @@ test("Modify project resources via workspace and access via reader", async (t) =
 	workspaceResource.setString(newContent);
 	await workspace.write(workspaceResource);
 
-	const reader = project.getReader();
+	const reader = await project.getReader();
 	const readerResource = await reader.byPath("/dev/devTools.js");
 	t.truthy(readerResource, "Found the requested resource byPath");
 	t.is(readerResource.getPath(), "/dev/devTools.js", "Resource (byPath) has correct path");
@@ -250,9 +80,8 @@ test("Modify project resources via workspace and access via reader", async (t) =
 });
 
 test("Modify project resources via workspace and access via reader for other path mapping", async (t) => {
-	const {projectInput} = t.context;
-	const project = await Specification.create(projectInput);
-	const workspace = project.getWorkspace();
+	const project = await Specification.create(basicProjectInput);
+	const workspace = await project.getWorkspace();
 	const workspaceResource = await workspace.byPath("/index.js");
 	t.truthy(workspaceResource, "Found resource in workspace");
 
@@ -260,7 +89,7 @@ test("Modify project resources via workspace and access via reader for other pat
 	workspaceResource.setString(newContent);
 	await workspace.write(workspaceResource);
 
-	const reader = project.getReader();
+	const reader = await project.getReader();
 	const readerResource = await reader.byPath("/index.js");
 	t.truthy(readerResource, "Found the requested resource byPath");
 	t.is(readerResource.getPath(), "/index.js", "Resource (byPath) has correct path");
@@ -275,7 +104,7 @@ test("Modify project resources via workspace and access via reader for other pat
 });
 
 test("_configureAndValidatePaths: Default path mapping", async (t) => {
-	const {projectInput} = t.context;
+	const projectInput = clone(basicProjectInput);
 	projectInput.configuration.resources = {};
 	const project = await Specification.create(projectInput);
 
@@ -285,29 +114,29 @@ test("_configureAndValidatePaths: Default path mapping", async (t) => {
 });
 
 test("_configureAndValidatePaths: Configured path mapping", async (t) => {
-	const {projectInput} = t.context;
+	const projectInput = clone(basicProjectInput);
 	const project = await Specification.create(projectInput);
 
 	t.is(project._paths.length, 2, "Two path mappings");
 	t.is(project._paths[0].virBasePath, "/", "Correct virtual base path for /");
-	t.is(project._paths[0].fsBasePath, path.join(projectInput.modulePath, "dist"), "Correct fs path");
+	t.is(project._paths[0].fsBasePath, projectInput.modulePath + "/dist", "Correct fs path");
 	t.is(project._paths[1].virBasePath, "/dev/", "Correct virtual base path for /dev/");
-	t.is(project._paths[1].fsBasePath, path.join(projectInput.modulePath, "dev"), "Correct fs path");
+	t.is(project._paths[1].fsBasePath, projectInput.modulePath + "/dev", "Correct fs path");
 });
 
 test("_configureAndValidatePaths: Default directory does not exist", async (t) => {
-	const {projectInput} = t.context;
+	const projectInput = clone(basicProjectInput);
 	projectInput.configuration.resources = {};
-	projectInput.modulePath = "/does/not/exist";
+	projectInput.modulePath = "does/not/exist";
 	const err = await t.throwsAsync(Specification.create(projectInput));
 
 	t.is(err.message, "Unable to find root directory of module project module.a");
 });
 
 test("_configureAndValidatePaths: Directory does not exist", async (t) => {
-	const {projectInput} = t.context;
+	const projectInput = clone(basicProjectInput);
 	projectInput.configuration.resources.configuration.paths.doesNotExist = "does/not/exist";
 	const err = await t.throwsAsync(Specification.create(projectInput));
 
-	t.is(err.message, "Unable to find source directory 'does/not/exist' in module project module.a");
+	t.is(err.message, "Unable to find directory 'does/not/exist' in module project module.a");
 });
