95e2622a75a98b2e6fca061353df98a305281e45
Merlin Beutlberger <m.beutlberger@sap.com>
2020-12-04 18:55:42 +0100

    [project][BREAKING] Implement Project Graph, build execution
    * Replace the JSON-object based dependency tree handling with a graph
    representation
        * Projects are now represented by classes with documented APIs
        * Projects can be accessed by extensions defining specVersion >=2.7
        * Speed up resolution of package.json dependencies
            * Make "ui5.dependencies" package.json configuration obsolete
    * Move build execution from ui5-builder to ui5-project
        * ui5-builder scope reduced top provides task implementations only
    * Build: Determine automatically whether a project-build requires
      dependencies to be built and build them
    * Build: Add new option 'createBuildManifest'. This will create a
      manifest file in the target directory that allows reuse of the
      build result of  library and theme-library projects in other project
      builds (RFC0011)
    
    This PR will need additional follow-up to add more test cases,
    cleanup JSDoc and possibly add more features described in the RFCs.
    
    This is a nicer version of
    https://github.com/SAP/ui5-project/pull/394
    
    Implements RFC0009: https://github.com/SAP/ui5-tooling/pull/501
    Implements RFC0011: https://github.com/SAP/ui5-tooling/pull/612
    
    BREAKING CHANGE:
    * normalizer and projectTree APIs have been removed. Use
    generateProjectGraph instead
    * Going forward only specification versions 2.0 and higher are supported
        * In case a legacy specification version is detected, an automatic,
          transparent migration is attempted.
    * Build:
        * The "dev" build mode has been removed
        * The task "generateVersionInfo" is no longer executed for
          application projects by default. You may enable it again using the
          includedTasks parameter

--- packages/project/lib/specifications/types/Application.js
+++ packages/project/lib/specifications/types/Application.js
@@ -1,21 +1,12 @@
-import fsPath from "node:path";
-import ComponentProject from "../ComponentProject.js";
-import {createReader} from "@ui5/fs/resourceFactory";
+const fsPath = require("path");
+const resourceFactory = require("@ui5/fs").resourceFactory;
+const ComponentProject = require("../ComponentProject");
 
-/**
- * Application
- *
- * @public
- * @class
- * @alias @ui5/project/specifications/types/Application
- * @extends @ui5/project/specifications/ComponentProject
- * @hideconstructor
- */
 class Application extends ComponentProject {
 	constructor(parameters) {
 		super(parameters);
 
-		this._pManifests = Object.create(null);
+		this._pManifests = {};
 
 		this._webappPath = "webapp";
 
@@ -35,30 +26,19 @@ class Application extends ComponentProject {
 			this._config.builder.cachebuster.signatureType || "time";
 	}
 
-	/**
-	 * Get the path of the project's source directory. This might not be POSIX-style on some platforms.
-	 *
-	 * @public
-	 * @returns {string} Absolute path to the source directory of the project
-	 */
-	getSourcePath() {
-		return fsPath.join(this.getRootPath(), this._webappPath);
-	}
-
 	/* === Resource Access === */
 	/**
 	* Get a resource reader for the sources of the project (excluding any test resources)
 	*
-	* @param {string[]} excludes List of glob patterns to exclude
-	* @returns {@ui5/fs/ReaderCollection} Reader collection
+	* @returns {module:@ui5/fs.ReaderCollection} Reader collection
 	*/
-	_getSourceReader(excludes) {
-		return createReader({
-			fsBasePath: this.getSourcePath(),
+	_getSourceReader() {
+		return resourceFactory.createReader({
+			fsBasePath: fsPath.join(this.getPath(), this._webappPath),
 			virBasePath: `/resources/${this._namespace}/`,
 			name: `Source reader for application project ${this.getName()}`,
 			project: this,
-			excludes
+			excludes: this.getBuilderResourcesExcludes()
 		});
 	}
 
@@ -66,17 +46,11 @@ class Application extends ComponentProject {
 		return null; // Applications do not have a dedicated test directory
 	}
 
-	/**
-	 * Get a resource reader for the sources of the project (excluding any test resources)
-	 * without a virtual base path
-	 *
-	 * @returns {@ui5/fs/ReaderCollection} Reader collection
-	*/
 	_getRawSourceReader() {
-		return createReader({
-			fsBasePath: this.getSourcePath(),
+		return resourceFactory.createReader({
+			fsBasePath: fsPath.join(this.getPath(), this._webappPath),
 			virBasePath: "/",
-			name: `Raw source reader for application project ${this.getName()}`,
+			name: `Source reader for application project ${this.getName()}`,
 			project: this
 		});
 	}
@@ -95,12 +69,12 @@ class Application extends ComponentProject {
 		}
 
 		this._log.verbose(`Path mapping for application project ${this.getName()}:`);
-		this._log.verbose(`  Physical root path: ${this.getRootPath()}`);
-		this._log.verbose(`  Mapped to: ${this._webappPath}`);
+		this._log.verbose(`  Physical root path: ${this.getPath()}`);
+		this._log.verbose(`  Mapped to: ${this._srcPath}`);
 
-		if (!(await this._dirExists("/" + this._webappPath))) {
+		if (!await this._dirExists("/" + this._webappPath)) {
 			throw new Error(
-				`Unable to find source directory '${this._webappPath}' in application project ${this.getName()}`);
+				`Unable to find directory '${this._webappPath}' in application project ${this.getName()}`);
 		}
 	}
 
@@ -145,11 +119,7 @@ class Application extends ComponentProject {
 					// 	relevant for the rather "uncommon" App Variants)
 					throw new Error(
 						`Could not find required manifest.json for project ` +
-						`${this.getName()}: ${manifestJsonError.message}\n\n` +
-						`If you are about to start a new project, please refer to:\n` +
-						`https://ui5.github.io/cli/v4/pages/GettingStarted/#starting-a-new-project`, {
-							cause: manifestJsonError
-						});
+						`${this.getName()}: ${manifestJsonError.message}`);
 				}
 				throw appDescVarError;
 			}
@@ -184,7 +154,7 @@ class Application extends ComponentProject {
 		}
 		const namespace = appId.replace(/\./g, "/");
 		this._log.verbose(
-			`Namespace of project ${this.getName()} is ${namespace} (from manifest.json)`);
+			`Namespace of project ${this.getName()} is ${namespace} (from manifest.appdescr_variant)`);
 		return namespace;
 	}
 
@@ -224,16 +194,11 @@ class Application extends ComponentProject {
 		return this._pManifests[filePath] = this._getRawSourceReader().byPath(filePath)
 			.then(async (resource) => {
 				if (!resource) {
-					const error = new Error(
+					throw new Error(
 						`Could not find resource ${filePath} in project ${this.getName()}`);
-					error.code = "ENOENT"; // "File or directory does not exist"
-					throw error;
 				}
 				return JSON.parse(await resource.getString());
 			}).catch((err) => {
-				if (err.code === "ENOENT") {
-					throw err;
-				}
 				throw new Error(
 					`Failed to read ${filePath} for project ` +
 					`${this.getName()}: ${err.message}`);
@@ -241,4 +206,4 @@ class Application extends ComponentProject {
 	}
 }
 
-export default Application;
+module.exports = Application;
