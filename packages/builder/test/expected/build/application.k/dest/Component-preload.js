10354b0ce16d497a84f5851d2c34497585a67076
Matthias Oßwald <1410947+matz3@users.noreply.github.com>
2021-02-09 17:23:22 +0100

    [builder][FEATURE] Add Library/Component preload exclude configuration (https://github.com/SAP/ui5-builder/pull/573)
    JIRA: CPOUI5FOUNDATION-233
    Co-authored-by: Merlin Beutlberger <m.beutlberger@sap.com>

--- packages/builder/test/expected/build/application.k/dest/Component-preload.js
+++ packages/builder/test/expected/build/application.k/dest/Component-preload.js
@@ -1,13 +1,8 @@
 //@ui5-bundle application/k/Component-preload.js
-sap.ui.predefine("application/k/Component", ["sap/ui/core/UIComponent"], function(UIComponent){
-	"use strict";
-	return UIComponent.extend('application.k.Component', {
-		metadata: {
-			manifest: "json"
-		}
-	});
-});
-sap.ui.require.preload({
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"application/k/Component.js":function(){sap.ui.define(["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.k.Component",{metadata:{manifest:"json"}})});
+},
 	"application/k/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"application.k","type":"application","applicationVersion":{"version":"${version}"},"embeds":["embedded"],"title":"{{title}}"},"customCopyrightString":"${copyright}"}'
-});
-//# sourceMappingURL=Component-preload.js.map
+}});
