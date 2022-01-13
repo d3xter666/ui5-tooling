8ab21082d8dd234cc3f9e1f262afca8ee9e37ec9
Merlin Beutlberger <m.beutlberger@sap.com>
2022-01-13 17:21:34 +0100

    [fs][FEATURE] Introduce Readers "Filter" and "Transformer" (https://github.com/SAP/ui5-fs/pull/331)

--- packages/fs/lib/readers/Filter.js
+++ packages/fs/lib/readers/Filter.js
@@ -1,30 +1,28 @@
-import AbstractReader from "../AbstractReader.js";
+const AbstractReader = require("../AbstractReader");
 
 /**
  * A reader that allows dynamic filtering of resources passed through it
  *
  * @public
- * @class
- * @alias @ui5/fs/readers/Filter
- * @extends @ui5/fs/AbstractReader
+ * @memberof module:@ui5/fs
+ * @augments module:@ui5/fs.AbstractReader
  */
 class Filter extends AbstractReader {
 	/**
 	* Filter callback
 	*
 	* @public
-	* @callback @ui5/fs/readers/Filter~callback
-	* @param {@ui5/fs/Resource} resource Resource to test
+	* @callback module:@ui5/fs.readers.Filter~callback
+	* @param {module:@ui5/fs.Resource} resource Resource to test
 	* @returns {boolean} Whether to keep the resource
 	*/
 
 	/**
 	 * Constructor
 	 *
- 	 * @public
 	 * @param {object} parameters Parameters
-	 * @param {@ui5/fs/AbstractReader} parameters.reader The resource reader or collection to wrap
-	 * @param {@ui5/fs/readers/Filter~callback} parameters.callback
+	 * @param {module:@ui5/fs.AbstractReader} parameters.reader The resource reader to wrap
+	 * @param {module:@ui5/fs.readers.Filter~callback} parameters.callback
 	 * 				Filter function. Will be called for every resource read through this reader.
 	 */
 	constructor({reader, callback}) {
@@ -46,8 +44,8 @@ class Filter extends AbstractReader {
 	 * @param {string|string[]} pattern glob pattern as string or an array of
 	 *         glob patterns for virtual directory structure
 	 * @param {object} options glob options
-	 * @param {@ui5/fs/tracing/Trace} trace Trace instance
-	 * @returns {Promise<@ui5/fs/Resource[]>} Promise resolving to list of resources
+	 * @param {module:@ui5/fs.tracing.Trace} trace Trace instance
+	 * @returns {Promise<module:@ui5/fs.Resource[]>} Promise resolving to list of resources
 	 */
 	async _byGlob(pattern, options, trace) {
 		const result = await this._reader._byGlob(pattern, options, trace);
@@ -60,8 +58,8 @@ class Filter extends AbstractReader {
 	 * @private
 	 * @param {string} virPath Virtual path
 	 * @param {object} options Options
-	 * @param {@ui5/fs/tracing/Trace} trace Trace instance
-	 * @returns {Promise<@ui5/fs/Resource>} Promise resolving to a single resource
+	 * @param {module:@ui5/fs.tracing.Trace} trace Trace instance
+	 * @returns {Promise<module:@ui5/fs.Resource>} Promise resolving to a single resource
 	 */
 	async _byPath(virPath, options, trace) {
 		const result = await this._reader._byPath(virPath, options, trace);
@@ -72,4 +70,4 @@ class Filter extends AbstractReader {
 	}
 }
 
-export default Filter;
+module.exports = Filter;
