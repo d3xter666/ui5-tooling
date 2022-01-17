c85cf18d16d2a17e9f917cca3c4e5255ee6cea22
Merlin Beutlberger <m.beutlberger@sap.com>
2022-01-17 10:41:01 +0100

    [builder][FEATURE] Add minify task and processor (https://github.com/SAP/ui5-builder/pull/666)
    Combines debug-file creation, source-map generation and minification.
    Replaces existing tasks/processors 'uglify' and 'createDebugFiles'.
    
    Also introduce new global tags `IsDebugVariant` and `HasDebugVariant`.
    
    BREAKING CHANGE:
    The following tasks have been removed:
    - createDebugFiles
    - uglify
    
    The following processors have been removed:
    - debugFileCreator
    - resourceCopier
    - uglifier
    
    As a replacement, the new 'minify' task and 'minifier' processor can be
    used.
    
    Note: The minify task is executed earlier, before the bundling
    process takes place. Existing 'beforeTask' and 'afterTask' configuration of
    custom tasks might need to be adapted to cater for this change.

--- packages/builder/test/expected/build/application.h/dest/sectionsB/customBundle.js
+++ packages/builder/test/expected/build/application.h/dest/sectionsB/customBundle.js
@@ -2,10 +2,16 @@
 jQuery.sap.registerPreloadedModules({
 "version":"2.0",
 "modules":{
-	"application/h/sectionsB/section1.js":function(){sap.ui.define(["sap/m/Button"],function(n){console.log("Section 1 included")});
+	"application/h/sectionsB/section1.js":function(){sap.ui.define(["sap/m/Button"], function(Button) {
+	console.log("Section 1 included");
+});
 },
-	"application/h/sectionsB/section2.js":function(){sap.ui.define(["sap/m/Button"],function(n){console.log("Section 2 included")});
+	"application/h/sectionsB/section2.js":function(){sap.ui.define(["sap/m/Button"], function(Button) {
+	console.log("Section 2 included");
+});
 },
-	"application/h/sectionsB/section3.js":function(){sap.ui.define(["sap/m/Button"],function(n){console.log("Section 3 included")});
+	"application/h/sectionsB/section3.js":function(){sap.ui.define(["sap/m/Button"], function(Button) {
+	console.log("Section 3 included");
+});
 }
 }});
