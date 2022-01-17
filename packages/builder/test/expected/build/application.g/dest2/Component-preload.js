932b8a52aaf043a6a7241154352402d2a84eda01
Merlin Beutlberger <m.beutlberger@sap.com>
2022-01-17 10:41:01 +0100

    [builder][FEATURE] Add minify task and processor (https://github.com/SAP/ui5-builder/pull/666)
    Combines debug-file creation, source-map generation and minification.
    Replaces existing tasks/processors 'uglify' and 'createDebugFiles'.
    
    Also introduce new global tags `IsDebugVariant` and `HasDebugVariant`.
    
    BREAKING CHANGE:
    The following tasks have been removed:
    - createDebugFiles
    - uglify
    
    The following processors have been removed:
    - debugFileCreator
    - resourceCopier
    - uglifier
    
    As a replacement, the new 'minify' task and 'minifier' processor can be
    used.
    
    Note: The minify task is executed earlier, before the bundling
    process takes place. Existing 'beforeTask' and 'afterTask' configuration of
    custom tasks might need to be adapted to cater for this change.

--- packages/builder/test/expected/build/application.g/dest2/Component-preload.js
+++ packages/builder/test/expected/build/application.g/dest2/Component-preload.js
@@ -1,6 +1,9 @@
 //@ui5-bundle application/g/Component-preload.js
-sap.ui.predefine("application/g/Component", ["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.Component",{metadata:{manifest:"json"}})});
-sap.ui.require.preload({
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"application/g/Component.js":function(){sap.ui.define(["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
+},
 	"application/g/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"application.g","type":"application","applicationVersion":{"version":"1.0.0"},"embeds":["embedded"],"title":"{{title}}"},"customCopyrightString":"Some fancy copyright"}'
-});
-//# sourceMappingURL=Component-preload.js.map
+}});
