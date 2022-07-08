da6cf6401ab85510d54a9fd15e2010ab8e15151e
Merlin Beutlberger <m.beutlberger@sap.com>
2022-07-08 12:18:03 +0200

    [project][INTERNAL] Refactor buildDefinitions and add tests

--- packages/project/lib/build/helpers/BuildContext.js
+++ packages/project/lib/build/helpers/BuildContext.js
@@ -1,80 +1,19 @@
-import ProjectBuildContext from "./ProjectBuildContext.js";
-import OutputStyleEnum from "./ProjectBuilderOutputStyle.js";
+const ProjectBuildContext = require("./ProjectBuildContext");
 
 /**
  * Context of a build process
  *
  * @private
- * @memberof @ui5/project/build/helpers
+ * @memberof module:@ui5/builder.builder
  */
 class BuildContext {
-	constructor(graph, taskRepository, { // buildConfig
-		selfContained = false,
-		cssVariables = false,
-		jsdoc = false,
-		createBuildManifest = false,
-		outputStyle = OutputStyleEnum.Default,
-		includedTasks = [], excludedTasks = [],
-	} = {}) {
+	constructor({graph, options = {}}) {
 		if (!graph) {
 			throw new Error(`Missing parameter 'graph'`);
 		}
-		if (!taskRepository) {
-			throw new Error(`Missing parameter 'taskRepository'`);
-		}
-
-		const rootProjectType = graph.getRoot().getType();
-
-		if (createBuildManifest && !["library", "theme-library"].includes(rootProjectType)) {
-			throw new Error(
-				`Build manifest creation is currently not supported for projects of type ` +
-				rootProjectType);
-		}
-
-		if (createBuildManifest && selfContained) {
-			throw new Error(
-				`Build manifest creation is currently not supported for ` +
-				`self-contained builds`);
-		}
-
-		if (createBuildManifest && outputStyle === OutputStyleEnum.Flat) {
-			throw new Error(
-				`Build manifest creation is not supported in conjunction with flat build output`);
-		}
-		if (outputStyle !== OutputStyleEnum.Default) {
-			if (rootProjectType === "theme-library") {
-				throw new Error(
-					`${outputStyle} build output style is currently not supported for projects of type` +
-						`theme-library since they commonly have more than one namespace. ` +
-						`Currently only the Default output style is supported for this project type.`
-				);
-			}
-			if (rootProjectType === "module") {
-				throw new Error(
-					`${outputStyle} build output style is currently not supported for projects of type` +
-						`module. Their path mappings configuration can't be mapped to any namespace.` +
-						`Currently only the Default output style is supported for this project type.`
-				);
-			}
-		}
-
 		this._graph = graph;
-		this._buildConfig = {
-			selfContained,
-			cssVariables,
-			jsdoc,
-			createBuildManifest,
-			outputStyle,
-			includedTasks,
-			excludedTasks,
-		};
-
-		this._taskRepository = taskRepository;
-
-		this._options = {
-			cssVariables: cssVariables
-		};
 		this._projectBuildContexts = [];
+		this._options = options;
 	}
 
 	getRootProject() {
@@ -85,32 +24,32 @@ class BuildContext {
 		return this._options[key];
 	}
 
-	getBuildConfig() {
-		return this._buildConfig;
-	}
-
-	getTaskRepository() {
-		return this._taskRepository;
-	}
-
-	getGraph() {
-		return this._graph;
-	}
-
-	createProjectContext({project}) {
+	createProjectContext({project, log}) {
 		const projectBuildContext = new ProjectBuildContext({
 			buildContext: this,
-			project
+			project,
+			log
 		});
 		this._projectBuildContexts.push(projectBuildContext);
 		return projectBuildContext;
 	}
 
-	async executeCleanupTasks(force = false) {
+	async executeCleanupTasks() {
 		await Promise.all(this._projectBuildContexts.map((ctx) => {
-			return ctx.executeCleanupTasks(force);
+			return ctx.executeCleanupTasks();
 		}));
+	}
+
+	/**
+	 * Retrieve a single project from the dependency graph
+	 *
+	 * @param {string} projectName Name of the project to retrieve
+	 * @returns {module:@ui5/project.specifications.Project|undefined}
+	 *					project instance or undefined if the project is unknown to the graph
+	 */
+	getProject(projectName) {
+		return this._graph.getProject(projectName);
 	}
 }
 
-export default BuildContext;
+module.exports = BuildContext;
