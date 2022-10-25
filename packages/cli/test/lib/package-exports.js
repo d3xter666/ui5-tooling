9d3b9cdd724d5833cdc7a6c779697f609e64a492
Merlin Beutlberger <m.beutlberger@sap.com>
2022-10-25 06:28:18 +0200

    [cli][BREAKING] Transform to native ESM (https://github.com/SAP/ui5-cli/pull/529)
    BREAKING CHANGE:
    This package has been transformed to native ESM. Therefore it no longer provides a CommonJS export.
    If your project uses CommonJS, it needs to be converted to ESM or use a dynamic import.
    
    For more information see also:
    - https://sap.github.io/ui5-tooling/updates/migrate-v3/
    - https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
    
    Co-authored-by: Matthias Osswald <mat.osswald@sap.com>
    Co-authored-by: Florian Vogt <florian.vogt@sap.com>
    Co-authored-by: Yavor Ivanov <yavor.ivanov@sap.com>

--- packages/cli/test/lib/package-exports.js
+++ packages/cli/test/lib/package-exports.js
@@ -1,7 +1,7 @@
 import test from "ava";
 import {createRequire} from "node:module";
 
-// Using CommonsJS require since JSON module imports are still experimental
+// Using CommonsJS require as importing json files causes an ExperimentalWarning
 const require = createRequire(import.meta.url);
 
 // package.json should be exported to allow reading the version
@@ -9,16 +9,8 @@ test("export of package.json", (t) => {
 	t.truthy(require("@ui5/cli/package.json").version);
 });
 
-test("export of bin/ui5.cjs", (t) => {
-	t.truthy(import.meta.resolve("@ui5/cli/bin/ui5.cjs"));
-});
-
-test("export of bin/ui5.js (for compatibility with CLI v2 that might invoke a newer local installation", (t) => {
-	t.truthy(import.meta.resolve("@ui5/cli/bin/ui5.js"));
-});
-
 // Check number of definied exports
 test("check number of exports", (t) => {
 	const packageJson = require("@ui5/cli/package.json");
-	t.is(Object.keys(packageJson.exports).length, 3);
+	t.is(Object.keys(packageJson.exports).length, 1);
 });
