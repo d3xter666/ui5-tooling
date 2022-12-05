7a1fddde0afb5fae2f1eee9a75bd44162c60afa0
Yavor Ivanov <yavor.ivanov@sap.com>
2022-12-05 10:25:42 +0200

    list dirs 2

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -40,9 +40,9 @@ docker run --rm -v $(pwd):/docs --entrypoint ls \
 	--env GIT_COMMITTER_NAME="${GIT_COMMITTER_NAME}" --env GIT_COMMITTER_EMAIL="${GIT_COMMITTER_EMAIL}"  \
 	$DOCKER_IMAGE -l ./
 
-# docker run --rm -v $(pwd):/docs --entrypoint ls \
-# 	--env GIT_COMMITTER_NAME="${GIT_COMMITTER_NAME}" --env GIT_COMMITTER_EMAIL="${GIT_COMMITTER_EMAIL}"  \
-# 	$DOCKER_IMAGE -l ./src/mike
+docker run --rm -v $(pwd):/docs --entrypoint ls \
+	--env GIT_COMMITTER_NAME="${GIT_COMMITTER_NAME}" --env GIT_COMMITTER_EMAIL="${GIT_COMMITTER_EMAIL}"  \
+	$DOCKER_IMAGE -l ./src
 
 # Build with MkDocs/Mike
 docker run --rm -v $(pwd):/docs --entrypoint mike \
