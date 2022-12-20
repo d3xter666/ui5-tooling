f9bb639dc94c41a9a811a88b1df06527f12d7c2a
Yavor Ivanov <d3xter666@users.noreply.github.com>
2022-12-20 11:11:04 +0200

    [project][FEATURE] specVersion 3.0 (https://github.com/SAP/ui5-project/pull/522)
    Adds schema support for UI5 Tooling v3

--- packages/project/test/lib/validation/schema/__helper__/builder-bundleOptions.js
+++ packages/project/test/lib/validation/schema/__helper__/builder-bundleOptions.js
@@ -1,5 +1,3 @@
-import SpecificationVersion from "../../../../../lib/specifications/SpecificationVersion.js";
-
 /**
  * Common test functionality for builder/bundles/bundleOptions section in config
  */
@@ -9,132 +7,11 @@ export default {
 	 *
 	 * @param {Function} test ava test
 	 * @param {Function} assertValidation assertion function
-	 * @param {string} type one of "application", "library"
+	 * @param {string} type one of "application" and "library"
 	 */
 	defineTests: function(test, assertValidation, type) {
 		// Version specific tests
-		SpecificationVersion.getVersionsForRange(">=4.0").forEach(function(specVersion) {
-			test(`${type} (specVersion ${specVersion}): builder/bundles/bundleOptions`, async (t) => {
-				await assertValidation(t, {
-					"specVersion": specVersion,
-					"kind": "project",
-					"type": type,
-					"metadata": {
-						"name": "com.sap.ui5.test",
-						"copyright": "yes"
-					},
-					"builder": {
-						"bundles": [{
-							"bundleOptions": {
-								"optimize": false,
-								"decorateBootstrapModule": false,
-								"addTryCatchRestartWrapper": true,
-								"numberOfParts": 8,
-								"sourceMap": false
-							}
-						}]
-					}
-				});
-			});
-
-			test(`${type} (specVersion ${specVersion}): builder/bundles/bundleOptions properties removal`,
-				async (t) => {
-					await assertValidation(t, {
-						"specVersion": specVersion,
-						"kind": "project",
-						"type": type,
-						"metadata": {
-							"name": "com.sap.ui5.test",
-							"copyright": "yes"
-						},
-						"builder": {
-							"bundles": [{
-								"bundleOptions": {
-									"usePredefineCalls": true
-								}
-							}]
-						}
-					}, [
-						{
-							keyword: "additionalProperties",
-							dataPath: "/builder/bundles/0/bundleOptions",
-							params: {
-								additionalProperty: "usePredefineCalls",
-							},
-							message: "should NOT have additional properties",
-						},
-					]);
-				});
-
-			test(`${type} invalid (specVersion ${specVersion}): builder/bundles/bundleOptions config`, async (t) => {
-				await assertValidation(t, {
-					"specVersion": specVersion,
-					"kind": "project",
-					"type": type,
-					"metadata": {
-						"name": "com.sap.ui5.test",
-						"copyright": "yes"
-					},
-					"builder": {
-						"bundles": [{
-							"bundleOptions": {
-								"optimize": "invalid value",
-								"decorateBootstrapModule": {"invalid": "value"},
-								"addTryCatchRestartWrapper": ["invalid value"],
-								"numberOfParts": true,
-								"sourceMap": 55
-							}
-						}]
-					}
-				}, [
-					{
-						keyword: "type",
-						dataPath: "/builder/bundles/0/bundleOptions/optimize",
-						params: {
-							type: "boolean",
-						},
-						message: "should be boolean"
-					},
-					{
-						keyword: "type",
-						dataPath:
-							"/builder/bundles/0/bundleOptions/decorateBootstrapModule",
-						params: {
-							type: "boolean",
-						},
-						message: "should be boolean"
-					},
-					{
-						keyword: "type",
-						dataPath:
-							"/builder/bundles/0/bundleOptions/addTryCatchRestartWrapper",
-						params: {
-							type: "boolean",
-						},
-						message: "should be boolean"
-					},
-					{
-						keyword: "type",
-						dataPath:
-							"/builder/bundles/0/bundleOptions/numberOfParts",
-						params: {
-							type: "number",
-						},
-						message: "should be number"
-					},
-					{
-						keyword: "type",
-						dataPath: "/builder/bundles/0/bundleOptions/sourceMap",
-						params: {
-							type: "boolean",
-						},
-						message: "should be boolean"
-					}
-				]);
-			});
-		});
-
-		SpecificationVersion.getVersionsForRange("3.0 - 3.2").forEach(function(specVersion) {
+		["3.0"].forEach(function(specVersion) {
 			test(`${type} (specVersion ${specVersion}): builder/bundles/bundleOptions`, async (t) => {
 				await assertValidation(t, {
 					"specVersion": specVersion,
--- packages/project/test/lib/validation/schema/specVersion/kind/extension/server-middleware.js
+++ packages/project/test/lib/validation/schema/specVersion/kind/extension/server-middleware.js
@@ -1,7 +1,6 @@
 import test from "ava";
 import Ajv from "ajv";
 import ajvErrors from "ajv-errors";
-import SpecificationVersion from "../../../../../../../lib/specifications/SpecificationVersion.js";
 import AjvCoverage from "../../../../../../utils/AjvCoverage.js";
 import {_Validator as Validator} from "../../../../../../../lib/validation/validator.js";
 import ValidationError from "../../../../../../../lib/validation/ValidationError.js";
@@ -16,11 +15,6 @@ async function assertValidation(t, config, expectedErrors = undefined) {
 		});
 		validationError.errors.forEach((error) => {
 			delete error.schemaPath;
-			if (error.params && Array.isArray(error.params.errors)) {
-				error.params.errors.forEach(($) => {
-					delete $.schemaPath;
-				});
-			}
 		});
 		t.deepEqual(validationError.errors, expectedErrors);
 	} else {
@@ -29,7 +23,7 @@ async function assertValidation(t, config, expectedErrors = undefined) {
 }
 
 test.before((t) => {
-	t.context.validator = new Validator({Ajv, ajvErrors, schemaName: "ui5"});
+	t.context.validator = new Validator({Ajv, ajvErrors});
 	t.context.ajvCoverage = new AjvCoverage(t.context.validator.ajv, {
 		includes: ["schema/specVersion/kind/extension/server-middleware.json"]
 	});
@@ -44,86 +38,6 @@ test.after.always((t) => {
 		lines: 70
 	};
 	t.context.ajvCoverage.verify(thresholds);
-});
-
-SpecificationVersion.getVersionsForRange(">=3.0").forEach(function(specVersion) {
-	test(`Invalid extension name (specVersion ${specVersion})`, async (t) => {
-		await assertValidation(t, {
-			"specVersion": specVersion,
-			"kind": "extension",
-			"type": "server-middleware",
-			"metadata": {
-				"name": "illegal-🦜"
-			},
-			"middleware": {
-				"path": "/bar"
-			}
-		}, [{
-			dataPath: "/metadata/name",
-			keyword: "errorMessage",
-			message: `Not a valid extension name. It must consist of lowercase alphanumeric characters, dash, underscore, and period only. Additionally, it may contain an npm-style package scope. For details, see: https://ui5.github.io/cli/stable/pages/Configuration/#name`,
-			params: {
-				errors: [{
-					dataPath: "/metadata/name",
-					keyword: "pattern",
-					message: `should match pattern "^(?:@[0-9a-z-_.]+\\/)?[a-z][0-9a-z-_.]*$"`,
-					params: {
-						pattern: "^(?:@[0-9a-z-_.]+\\/)?[a-z][0-9a-z-_.]*$",
-					}
-				}]
-			},
-		}]);
-		await assertValidation(t, {
-			"specVersion": specVersion,
-			"kind": "extension",
-			"type": "server-middleware",
-			"metadata": {
-				"name": "a"
-			},
-			"middleware": {
-				"path": "/bar"
-			}
-		}, [{
-			dataPath: "/metadata/name",
-			keyword: "errorMessage",
-			message: `Not a valid extension name. It must consist of lowercase alphanumeric characters, dash, underscore, and period only. Additionally, it may contain an npm-style package scope. For details, see: https://ui5.github.io/cli/stable/pages/Configuration/#name`,
-			params: {
-				errors: [{
-					dataPath: "/metadata/name",
-					keyword: "minLength",
-					message: "should NOT be shorter than 3 characters",
-					params: {
-						limit: 3,
-					}
-				}]
-			},
-		}]);
-		await assertValidation(t, {
-			"specVersion": specVersion,
-			"kind": "extension",
-			"type": "server-middleware",
-			"metadata": {
-				"name": "a".repeat(81)
-			},
-			"middleware": {
-				"path": "/bar"
-			}
-		}, [{
-			dataPath: "/metadata/name",
-			keyword: "errorMessage",
-			message: `Not a valid extension name. It must consist of lowercase alphanumeric characters, dash, underscore, and period only. Additionally, it may contain an npm-style package scope. For details, see: https://ui5.github.io/cli/stable/pages/Configuration/#name`,
-			params: {
-				errors: [{
-					dataPath: "/metadata/name",
-					keyword: "maxLength",
-					message: "should NOT be longer than 80 characters",
-					params: {
-						limit: 80,
-					}
-				}]
-			},
-		}]);
-	});
 });
 
 const additionalConfiguration = {
