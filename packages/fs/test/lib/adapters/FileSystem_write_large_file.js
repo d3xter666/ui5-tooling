85f33a5f77a8181d1848ada0b1c8ea6019f9d5ba
Matthias Osswald <mat.osswald@sap.com>
2023-01-16 14:46:11 +0100

    [fs][INTERNAL] FileSystem: Add failing test when writing to same file

--- packages/fs/test/lib/adapters/FileSystem_write_large_file.js
+++ packages/fs/test/lib/adapters/FileSystem_write_large_file.js
@@ -7,11 +7,7 @@ import FileSystem from "../../../lib/adapters/FileSystem.js";
 import Resource from "../../../lib/Resource.js";
 import path from "node:path";
 
-test.serial("Stream a large file from source to target", async (t) => {
-	// This test used to fail. The FileSystem adapter would directly pipe the read-stream into a write stream with the
-	// same path. Leading to only the first chunk of the source file or nothing at all being written into the target
-	// This has been fixed with https://github.com/SAP/ui5-fs/pull/472
-
+test.serial("x", async (t) => {
 	const fsBasePath = fileURLToPath(new URL("../../tmp/adapters/FileSystemWriteLargeFile/", import.meta.url));
 
 	const fileSystem = new FileSystem({
@@ -19,7 +15,8 @@ test.serial("Stream a large file from source to target", async (t) => {
 		virBasePath: "/"
 	});
 
-	const largeBuffer = Buffer.alloc(1048576); // 1MB
+	// const largeBuffer = Buffer.alloc(1048576); // 1MB
+	const largeBuffer = Buffer.alloc(1);
 
 	await fileSystem.write(new Resource({
 		path: "/large-file.txt",
@@ -35,6 +32,6 @@ test.serial("Stream a large file from source to target", async (t) => {
 
 	await fileSystem.write(largeResource);
 
-	t.deepEqual((await readFile(path.join(fsBasePath, "large-file.txt"))).length, largeBuffer.length,
+	t.deepEqual(await readFile(path.join(fsBasePath, "large-file.txt")), largeBuffer,
 		"Large file should be overwritten with exact same contents");
 });
