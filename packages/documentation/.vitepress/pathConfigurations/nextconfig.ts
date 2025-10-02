fcaefa2e00e3f4ee6338d9145e4d7d29945a2d27
d3xter666 <yavor.ivanov@sap.com>
2025-10-02 14:43:26 +0300

    feat: Add vitepress & config

--- packages/documentation/.vitepress/pathConfigurations/nextconfig.ts
+++ packages/documentation/.vitepress/pathConfigurations/nextconfig.ts
@@ -1,16 +1,26 @@
 // using the defineConfig helper will provide TypeScript-powered
 // intellisense for config options
 import { defineConfig } from "vitepress";
+import { join } from "node:path";
 
 // markdown
 import MarkdownItImplicitFigures from "markdown-it-implicit-figures";
 import MarkdownItPlantuml from "markdown-it-plantuml";
 
+// shikiji loading
+import { promises as fs, truncate } from 'node:fs'
+import type { LanguageInput, RawGrammar } from 'shiki'
+const loadSyntax = async (file: string, name: string, alias: string = name): Promise<LanguageInput> => {
+  const src = await fs.readFile(join(__dirname, file))
+  const grammar: RawGrammar = JSON.parse(src.toString())
+  return { name, aliases: [name, alias], ...grammar }
+}
+
 export default defineConfig({
   
-  base: "/ui5-tooling/stable/", // use this for github pages deployment or remove for CF
+  base: "/ui5-tooling/next/", // use this for github pages deployment or remove for CF
   srcDir: "docs",
-  outDir: "dist",
+  outDir: "site",
   lang: "en-US",
   title: "UI5 Tooling",
 
@@ -45,7 +55,7 @@ export default defineConfig({
       dark: "/icons/ui5/O.svg"
     },
     externalLinkIcon: false,
-    outline: [1, 3],
+    outline: [1,3],
 
     nav: nav(),
 
@@ -64,7 +74,7 @@ export default defineConfig({
         &copy; Copyright ${new Date().getFullYear()}, SAP SE and UI5 Tooling Contributors <br/>
           <a style="margin:25px"href="https://www.sap.com/corporate/en/legal/impressum.html">Legal Disclosure</a>
           <a  style="margin:25px" href="https://www.sap.com/corporate/en/legal/terms-of-use.html">Terms of Use</a>
-          <a  style="margin:25px" href="https://ui5.github.io/cli/stable/pages/Privacy/">Privacy</a>
+          <a  style="margin:25px" href="https://sap.github.io/ui25-tooling/stable/pages/Privacy/">Privacy</a>
           <a  style="margin:25px" href="https://www.sap.com/corporate/en/legal/trademark.html">Trademarks</a>
     `,
 
@@ -81,14 +91,24 @@ export default defineConfig({
   },
 
   markdown: {
-    externalLinks: {
-
+    externalLinks:{
+      
     },
-
+    
     //theme: "material-theme-palenight", // pre rc5 default
     // Add support for your own languages.
     // https://github.com/shikijs/shiki/blob/main/languages.md#supporting-your-own-languages-with-shiki
     languages: [
+      // https://github.com/SAP-samples/vscode-abap-cds/blob/main/syntaxes/cds.tmLanguage.json
+      await loadSyntax('syntaxes/abapcds.tmLanguage.json', 'abapcds'),
+      // https://github.com/SAP/cds-textmate-grammar/blob/main/syntaxes/cds.tmLanguage.json
+      await loadSyntax('syntaxes/cds.tmLanguage.json', 'cds'),
+      // https://github.com/mechatroner/vscode_rainbow_csv/blob/master/syntaxes/csv.tmLanguage.json
+      await loadSyntax('syntaxes/csv.tmLanguage.json', 'csv'),
+      // https://github.com/mechatroner/vscode_rainbow_csv/blob/master/syntaxes/csv.tmLanguage.json
+      await loadSyntax('syntaxes/scsv.tmLanguage.json', 'csvs'),
+      // https://github.com/Huachao/vscode-restclient/blob/master/syntaxes/http.tmLanguage.json
+      await loadSyntax('syntaxes/http.tmLanguage.json', 'rest'),
     ],
 
     // Configure the Markdown-it instance
@@ -117,7 +137,7 @@ export default defineConfig({
     },
   },
 
-
+ 
   vite: {
     build: {
       chunkSizeWarningLimit: 4000, // chunk for local search index dominates
@@ -133,16 +153,23 @@ function nav() {
       items: [
         {
           text: 'V3',
-          link: '../v3/',
+          link: 'https://konnraad.github.io/ui5-tooling/v3/',
           target: "_self"
         },
         {
           text: 'V2',
-          link: '../v2/',
+          link: 'https://konnraad.github.io/ui5-tooling/v2/',
           target: "_self"
         }
       ]
     },
+   // {
+   //   text: "Home",
+   //   link: "/Home/",
+   //   activeMatch: "/",
+   // },
+   // { text: "About", link: "/about" },
+
   ];
 }
 
@@ -158,11 +185,11 @@ function guide() {
 
         {
           text: "Home",
-          link: "/pages/index",
+          link: "/HomePage",
         },
         {
           text: "Getting Started",
-          link: "/pages/GettingStarted",
+          link: "/GettingStarted",
         },
 
       ],
@@ -171,7 +198,7 @@ function guide() {
     {
       text: "UI5 CLI",
       collapsed: true,
-      link: "/pages/CLI",
+      link: "/CLI",
 
 
     },
@@ -179,7 +206,7 @@ function guide() {
       text: "Configuration",
       collapsed: true,
 
-      link: "/pages/Configuration",
+      link: "/Configuration",
 
     },
     {
@@ -188,19 +215,19 @@ function guide() {
       items: [
         {
           text: "Overview",
-          link: "/pages/Overview",
+          link: "/Overview",
         },
         {
           text: "OpenUI5",
-          link: "/pages/OpenUI5",
+          link: "/OpenUI5",
         },
         {
           text: "SAPUI5",
-          link: "/pages/SAPUI5",
+          link: "/SAPUI5",
         },
         {
           text: "Workspace",
-          link: "/pages/Workspace",
+          link: "/Workspace",
         },
 
       ],
@@ -212,15 +239,15 @@ function guide() {
       items: [
         {
           text: "Custom Tasks",
-          link: "/pages/extensibility/CustomTasks",
+          link: "/extensibility/CustomTasks",
         },
         {
           text: "Custom Server Middleware",
-          link: "/pages/extensibility/CustomServerMiddleware",
+          link: "/extensibility/CustomServerMiddleware",
         },
         {
           text: "Project Shims",
-          link: "/pages/extensibility/ProjectShims",
+          link: "/extensibility/ProjectShims",
         },
       ],
     },
@@ -230,26 +257,26 @@ function guide() {
       items: [
         {
           text: "Server",
-          link: "/pages/Server",
+          link: "/Server",
         },
         {
           text: "Builder",
-          link: "/pages/Builder",
+          link: "/Builder",
         },
         {
           text: "Project",
-          link: "/pages/Project",
+          link: "/Project",
         },
         {
           text: "File System",
-          link: "/pages/FileSystem",
+          link: "/FileSystem",
         },
       ],
     },
     {
       text: "FAQ",
       collapsed: false,
-      link: "/pages/FAQ",
+      link: "/FAQ",
 
     },
     {
@@ -280,25 +307,25 @@ function guide() {
       items: [
         {
           text: "Troubleshooting",
-          link: "/pages/Troubleshooting",
+          link: "/Troubleshooting",
         },
         {
           text: "Benchmarking",
-          link: "/pages/Benchmarking",
+          link: "/Benchmarking",
         },
         {
           text: "ECMAScript Support",
-          link: "/pages/ESSupport",
+          link: "/ESSupport",
         },
         {
           text: "Code Analysis",
-          link: "/pages/CodeAnalysis",
+          link: "/CodeAnalysis",
         },
       ],
     },
     {
       text: "API Reference",
-      link: "https://ui5.github.io/cli/stable/api/index.html",
+      link: "https://sap.github.io/ui5-tooling/v4/api/index.html",
 
     },
 
