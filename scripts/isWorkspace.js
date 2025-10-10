70e2a4283a9eaa2f7ca74f76c758730ef598fda2
d3xter666 <yavor.ivanov@sap.com>
2025-10-10 14:29:10 +0300

    docs: JSDoc cleanup

--- scripts/isWorkspace.js
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import {fileURLToPath} from "node:url";
-
-const cliPath = path.relative(import.meta.dirname, fileURLToPath(import.meta.resolve("@ui5/cli/package.json")));
-
-// no workspace detected
-if (!cliPath.startsWith(path.join("..", ".."))) {
-	process.exit(1);
-}
