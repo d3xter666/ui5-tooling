87f4bf58a118e9f61625e001ad32545e140fbeb6
d3xter666 <yavor.ivanov@sap.com>
2025-11-13 12:53:58 +0200

    fix: Align and fix scripts with the monorepo

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,8 @@ export default [
 		// to add to common config
 		ignores: [
 			"**/site/",
-			"packages/"
+			"packages/",
+			"internal/"
 		]
 	},
 	...eslintCommonConfig, // Load common ESLint config
